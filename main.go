f9d47f2473fcb6f4a65e9a17b881367cac676b98
Hans Moog <hm@mkjc.net>
2019-03-29 06:24:22 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/iotaledger/goshimmer/packages/node"
+    "github.com/iotaledger/goshimmer/plugins/autopeering"
+    "github.com/iotaledger/goshimmer/plugins/cli"
+    "github.com/iotaledger/goshimmer/plugins/gracefulshutdown"
+    "github.com/iotaledger/goshimmer/plugins/statusscreen"
+)
+
+func main() {
+    node.Run(
+        cli.PLUGIN,
+        autopeering.PLUGIN,
+        statusscreen.PLUGIN,
+        gracefulshutdown.PLUGIN,
+    )
+}
