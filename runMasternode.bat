c57822a3dfddb647c32840b68ca69f4ed4dca462
Hans Moog <hm@mkjc.net>
2019-04-17 04:46:40 +0200

    Feat: test suit

---
+++ runMasternode.bat
@@ -0,0 +1,8 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+Set PEERING_PORT=14000
+
+cd entryNode
+start /b "" "go_build_github_com_iotaledger_goshimmer.exe" -autopeering-port %PEERING_PORT% -autopeering-entry-nodes e83c2e1f0ba552cbb6d08d819b7b2196332f8423@127.0.0.1:14000 -node-log-level 4 -node-disable-plugins statusscreen
+cd ..
