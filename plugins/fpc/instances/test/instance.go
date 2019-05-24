04e77b5fd42c1e8ac447a298eeb63580b881c3d0
capossele <angelo@Angelo.local>
2019-05-24 15:58:49 +0100

    Feat: initial fpc test plugin

---
+++ plugins/fpc/instances/test/instance.go
@@ -0,0 +1,51 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/iotaledger/goshimmer/packages/daemon"
+	"github.com/iotaledger/goshimmer/packages/fpc"
+	"github.com/iotaledger/goshimmer/packages/node"
+
+	"time"
+)
+
+var INSTANCE *fpc.FPC
+
+func Configure(plugin *node.Plugin) {
+	getKnownPeers := func() []int {
+		return []int{1, 2, 3, 4, 5}
+	}
+
+	queryNode := func(txs []fpc.Hash, node int) []fpc.Opinion {
+		output := make([]fpc.Opinion, len(txs))
+		for tx := range txs {
+			output[tx] = true
+		}
+		return output
+	}
+
+	INSTANCE = fpc.New(getKnownPeers, queryNode, fpc.NewParameters())
+
+	INSTANCE.VoteOnTxs()
+	INSTANCE.GetInterimOpinion()
+
+}
+
+func Run(plugin *node.Plugin) {
+	daemon.BackgroundWorker(func() {
+		ticker := time.NewTicker(1000 * time.Millisecond)
+		round := 0
+		for {
+			select {
+			case <-ticker.C:
+				round++
+				INSTANCE.Tick(uint64(round), 0.7)
+			case finalizedTxs := <-INSTANCE.FinalizedTxs:
+				if len(finalizedTxs) > 0 {
+					fmt.Println("Finalized txs", finalizedTxs)
+				}
+			}
+		}
+	})
+}
