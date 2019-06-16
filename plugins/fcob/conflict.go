6a9c31760157eea9df5ebd933d3652b57c8a7e2b
capossele <angelo@Angelo.local>
2019-06-16 09:03:53 +0100

    Opinion unit test added

--- plugins/fcob/conflict.go
+++ plugins/fcob/conflict.go
@@ -7,36 +7,10 @@ import (
 	"github.com/iotaledger/goshimmer/plugins/tangle"
 )
 
-type fcobConflict struct{}
-
-func (fcobConflict) GetConflictSet(target ternary.Trinary) (conflictSet map[ternary.Trinary]bool) {
-	conflictSet = make(map[ternary.Trinary]bool)
-
-	// txObject, err := tangle.GetTransaction(target)
-	// if err != nil {
-	// 	//TODO: handle error
-	// }
-
-	// targetAddress := txObject.GetAddress()
-	// conflict := false
-	// // In real implementation we don't need to iterate the all tangle
-	// // since we can just use the ledger state.
-	// for txHash, txObject := range dummyTangle {
-	// 	if targetAddress == txObject.address {
-	// 		if target != txHash { // filter out the same target tx
-	// 			conflictSet[txHash] = true
-	// 			conflict = true
-	// 		}
-	// 	}
-	// }
-	// if conflict {
-	// 	conflictSet[target] = true
-	// }
-	return conflictSet
-}
-
 type dummyConflict struct{}
 
+// GetConflictSet triggers a (fake) new conflict every 10 received txs
+// including only 1 conflicting tx in the returned conflict set
 func (dummyConflict) GetConflictSet(target ternary.Trinary) (conflictSet map[ternary.Trinary]bool) {
 
 	conflictSet = make(map[ternary.Trinary]bool)
@@ -46,7 +20,7 @@ func (dummyConflict) GetConflictSet(target ternary.Trinary) (conflictSet map[ter
 		//TODO: handle error
 		PLUGIN.LogFailure(fmt.Sprintf("txObject: %v", txObject))
 	}
-	conflict := txObject.GetValue()%10 == 0
+	conflict := txObject.GetValue()%10 == 0 // trigger a new conflict every 10 received txs
 	if conflict {
 		PLUGIN.LogInfo(fmt.Sprintf("(GetConflictSet) NewConflict: %v", target))
 		conflictSet[target] = true
