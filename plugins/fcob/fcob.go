2f4f8369bb4a81aa9bf2c2f2e53e238de9126610
capossele <angelo@Angelo.local>
2019-06-16 09:21:32 +0100

    comments improved

--- plugins/fcob/fcob.go
+++ plugins/fcob/fcob.go
@@ -3,23 +3,21 @@ package fcob
 import (
 	"github.com/iotaledger/goshimmer/packages/errors"
 	"github.com/iotaledger/goshimmer/packages/fpc"
-	"github.com/iotaledger/goshimmer/packages/node"
 	"github.com/iotaledger/goshimmer/packages/ternary"
 )
 
+// beingVoted is the voting map containing all the txs
+// currently being voted
 var beingVoted *VotingnMap
 
-// Fcob struct used to implement
-// the VoterUpdater interface
-type Fcob struct {
-	plugin *node.Plugin
-}
-
-// RunProtocol defines the signature of function
+// RunProtocol defines the signature of the function
 // implementing the FCoB protocol
-type RunProtocol func(txMetadata ternary.Trinary)
+type RunProtocol func(txMetadata ternary.Trinary) error
 
-// Opinioner is the interface for updating an opinion
+// Opinioner is the interface defining the behaviour for
+// - getting the opinion for a given tx
+// - setting the opinion of a given
+// - decide the opinion of a given tx
 type Opinioner interface {
 	GetOpinion(transactionHash ternary.Trinary) (opinion Opinion, err errors.IdentifiableError)
 	SetOpinion(transactionHash ternary.Trinary, opinion Opinion) (err errors.IdentifiableError)
@@ -38,34 +36,36 @@ func makeRunProtocol(voter fpc.Voter, opinioner Opinioner) RunProtocol {
 	beingVoted = NewVotingMap()
 
 	// dummy FCoB logic core
-	return func(txHash ternary.Trinary) {
+	return func(txHash ternary.Trinary) (err error) {
+		// the opinioner decides the initial opinion and the (potential conflict set)
 		initialOpinion, conflictSet := opinioner.Decide(txHash)
-		err := opinioner.SetOpinion(txHash, initialOpinion)
+		err = opinioner.SetOpinion(txHash, initialOpinion)
 		if err != nil {
-			//TODO: handle error
-			//PLUGIN.LogFailure()
+			return err
 		}
+		// the opinioner disliked this tx
 		if !initialOpinion.liked() && !initialOpinion.voted() {
 
-			// converting txHash into fpc TxOpinion
-
-			txsToVote := []fpc.TxOpinion{}
+			txsToSubmit := []fpc.TxOpinion{} // list of potential txs to submit for voting
+			// we loop over the conflict set
 			for tx := range conflictSet {
-				//TODO: add && is not being already in the process of voting
 				txOpinion, err := opinioner.GetOpinion(tx)
 				if err != nil {
-					//TODO: handle error
+					return err
 				}
+				// check not already voted AND not currently being voted
 				if !txOpinion.voted() && !beingVoted.Load(tx) {
+					// converting tx into fpc TxOpinion
 					cTx := fpc.TxOpinion{fpc.ID(tx), txOpinion.like}
-					txsToVote = append(txsToVote, cTx)
-					beingVoted.Store(tx)
+					txsToSubmit = append(txsToSubmit, cTx)
+					beingVoted.Store(tx) // adding it to the voting map
 				}
 			}
 			//TODO: add check that at least one conflicting tx is voted [maybe include like status]
 			// if that is the case, update the voted status for txHash to true and skip voting
 			// else do the voting
-			voter.SubmitTxsForVoting(txsToVote...)
+			voter.SubmitTxsForVoting(txsToSubmit...)
 		}
+		return
 	}
 }
