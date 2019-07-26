9291dc9f521fa7bc248a68285e8a901ef711bc67
capossele <angelocapossele@gmail.com>
2019-07-26 09:44:19 +0100

    :bug: added RWMutex

--- packages/events/event.go
+++ packages/events/event.go
@@ -1,19 +1,28 @@
 package events
 
+import "sync"
+
 type Event struct {
 	triggerFunc func(handler interface{}, params ...interface{})
 	callbacks   map[uintptr]interface{}
+	mutex       sync.RWMutex
 }
 
 func (this *Event) Attach(closure *Closure) {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
 	this.callbacks[closure.Id] = closure.Fnc
 }
 
 func (this *Event) Detach(closure *Closure) {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
 	delete(this.callbacks, closure.Id)
 }
 
 func (this *Event) Trigger(params ...interface{}) {
+	this.mutex.RLock()
+	defer this.mutex.RUnlock()
 	for _, handler := range this.callbacks {
 		this.triggerFunc(handler, params...)
 	}
