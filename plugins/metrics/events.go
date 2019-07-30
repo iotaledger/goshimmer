37061e640696e153e0fa9b8928ffab3df8b46c66
Hans Moog <hm@mkjc.net>
2019-07-30 13:19:46 +0200

    Fix: fixed a few bugs and restructured dashboard

--- plugins/metrics/events.go
+++ plugins/metrics/events.go
@@ -4,17 +4,14 @@ import (
 	"github.com/iotaledger/goshimmer/packages/events"
 )
 
-// define a struct with the available events
+var Events = pluginEvents{
+	ReceivedTPSUpdated: events.NewEvent(uint64EventCaller),
+}
+
 type pluginEvents struct {
 	ReceivedTPSUpdated *events.Event
 }
 
-// define a function that maps our event parameters to the correct type: uint64 (GO has no generics)
 func uint64EventCaller(handler interface{}, params ...interface{}) {
 	handler.(func(uint64))(params[0].(uint64))
-}
-
-// expose our event API
-var Events = pluginEvents{
-	ReceivedTPSUpdated: events.NewEvent(uint64EventCaller),
 }
--- plugins/metrics/plugin.go
+++ plugins/metrics/plugin.go
@@ -1,50 +1,24 @@
 package metrics
 
 import (
-	"encoding/binary"
 	"time"
 
-	"github.com/gorilla/websocket"
 	"github.com/iotaledger/goshimmer/packages/daemon"
 	"github.com/iotaledger/goshimmer/packages/events"
 	"github.com/iotaledger/goshimmer/packages/model/meta_transaction"
 	"github.com/iotaledger/goshimmer/packages/node"
 	"github.com/iotaledger/goshimmer/packages/timeutil"
-	"github.com/iotaledger/goshimmer/plugins/dashboard"
 	"github.com/iotaledger/goshimmer/plugins/gossip"
 )
 
-// create configure handler (get's called when the PLUGIN is "loaded" by the node)
+var PLUGIN = node.NewPlugin("Metrics", configure, run)
+
 func configure(plugin *node.Plugin) {
 	// increase received TPS counter whenever we receive a new transaction
 	gossip.Events.ReceiveTransaction.Attach(events.NewClosure(func(_ *meta_transaction.MetaTransaction) { increaseReceivedTPSCounter() }))
-
-	// send the sampledTPS to client via websocket, use uint32 to save mem
-	Events.ReceivedTPSUpdated.Attach(events.NewClosure(func(sampledTPS uint64) {
-		for client := range dashboard.Clients {
-			p := make([]byte, 4)
-			binary.LittleEndian.PutUint32(p, uint32(sampledTPS))
-			if err := client.WriteMessage(websocket.BinaryMessage, p); err != nil {
-				return
-			}
-			TPSQ = append(TPSQ, uint32(sampledTPS))
-			if len(TPSQ) > MAX_Q_SIZE {
-				TPSQ = TPSQ[1:]
-			}
-			dashboard.TPSQ = TPSQ
-		}
-	}))
 }
 
-// create run handler (get's called when the PLUGIN is "executed" by the node)
 func run(plugin *node.Plugin) {
 	// create a background worker that "measures" the TPS value every second
 	daemon.BackgroundWorker("Metrics TPS Updater", func() { timeutil.Ticker(measureReceivedTPS, 1*time.Second) })
 }
-
-// export plugin
-var PLUGIN = node.NewPlugin("Metrics", configure, run)
-
-// TPS queue
-var TPSQ []uint32
-var MAX_Q_SIZE int = 3600
