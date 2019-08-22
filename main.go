eb8415405723b8903550c1017b080ed47e40008c
Wolfgang Welz <welzwo@gmail.com>
2019-08-22 20:55:46 +0200

    :sparkles: Start server in main

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
 
+	"github.com/wollac/autopeering/discover"
+	"github.com/wollac/autopeering/identity"
+	"github.com/wollac/autopeering/transport"
 	"go.uber.org/zap"
 )
+
+var port = flag.Int("port", 8080, "port of the server")
+
+func init() {
+	flag.Parse()
+}
 
 func initLogger() *zap.Logger {
 	logger, err := zap.NewDevelopment()
@@ -14,7 +28,38 @@ func initLogger() *zap.Logger {
 	return logger
 }
 
+func waitInterrupt() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
+
 func main() {
 	logger := initLogger()
 	defer logger.Sync()
+
+	// create a UDP connection
+	conn, err := net.ListenPacket("udp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("listen failed: %v", err)
+	}
+	defer conn.Close()
+
+	cfg := discover.Config{
+		ID:  identity.GeneratePrivateIdentity(),
+		Log: logger,
+	}
+
+	// start the discovery on that connection
+	disc, err := discover.Listen(transport.Conn(conn), cfg)
+	if err != nil {
+		log.Fatalf("listen failed: %v", err)
+	}
+	defer disc.Close()
+
+	fmt.Println("Discovery protocol started, listening on " + disc.LocalAddr())
+	fmt.Println("Hit Ctrl+C to exit")
+
+	// wait for Ctrl+c
+	waitInterrupt()
 }
