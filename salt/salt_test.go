143a6abad448ee21c2ecef770b3ea2f4a2439119
capossele <angelocapossele@gmail.com>
2019-08-22 17:40:28 +0100

    :white_check_mark: added unit test

---
+++ salt/salt_test.go
@@ -0,0 +1,45 @@
+package salt
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSalt(t *testing.T) {
+	type testCase struct {
+		input time.Duration
+		want  error
+	}
+
+	tests := []testCase{
+		{input: 0, want: nil},
+		{input: 10, want: nil},
+		{input: -1, want: nil},
+	}
+
+	for _, test := range tests {
+		_, err := New(test.input)
+		assert.Equal(t, test.want, err, test)
+	}
+}
+
+func TestSaltExpired(t *testing.T) {
+	type testCase struct {
+		input time.Duration
+		want  bool
+	}
+
+	tests := []testCase{
+		{input: 0, want: true},
+		{input: time.Second * 10, want: false},
+		{input: -1, want: true},
+	}
+
+	for _, test := range tests {
+		salt, _ := New(test.input)
+		got := salt.Expired()
+		assert.Equal(t, test.want, got, test)
+	}
+}
