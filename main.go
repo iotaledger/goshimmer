e91c5fdea83d2d45a692e762e22d9380e9c78f18
Wolfgang Welz <welzwo@gmail.com>
2019-08-27 18:00:02 +0200

    :art: Open UDP connection in main

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/wollac/autopeering/discover"
 	"github.com/wollac/autopeering/id"
 	"github.com/wollac/autopeering/transport"
@@ -20,9 +23,31 @@ func waitInterrupt() {
 	<-c
 }
 
+func parseMaster(s string) (*discover.Peer, error) {
+	if len(s) == 0 {
+		return nil, nil
+	}
+
+	parts := strings.Split(s, "@")
+	if len(parts) != 2 {
+		return nil, errors.New("parseMaster")
+	}
+	pubKey, err := base64.StdEncoding.DecodeString(parts[0])
+	if err != nil {
+		return nil, errors.Wrap(err, "parseMaster")
+	}
+	identity, err := id.NewIdentity(pubKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "parseMaster")
+	}
+
+	return discover.NewPeer(identity, parts[1]), nil
+}
+
 func main() {
 	var (
-		listenAddr = flag.String("addr", ":14626", "listen address")
+		listenAddr = flag.String("addr", "127.0.0.1:14626", "listen address")
+		masterNode = flag.String("master", "", "master node as 'pubKey@address' where pubKey is in Base64")
 
 		err error
 	)
@@ -32,7 +57,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("NewLogger: %v", err)
 	}
-	defer logger.Sync()
+	defer func() { _ = logger.Sync() }()
 
 	addr, err := net.ResolveUDPAddr("udp", *listenAddr)
 	if err != nil {
@@ -49,14 +74,22 @@ func main() {
 		Log: logger,
 	}
 
+	master, err := parseMaster(*masterNode)
+	if err != nil {
+		log.Printf("Ignoring master: %v\n", err)
+	} else if master != nil {
+		cfg.Bootnodes = []*discover.Peer{master}
+	}
+
 	// start the discovery on that connection
-	disc, err := discover.Listen(transport.Conn(conn), cfg)
+	disc, err := discover.Listen(transport.Conn(conn, func(network, address string) (net.Addr, error) { return net.ResolveUDPAddr(network, address) }), cfg)
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 	defer disc.Close()
 
-	fmt.Println("Discovery protocol started, listening on " + disc.LocalAddr())
+	id := base64.StdEncoding.EncodeToString(disc.LocalID().ID())
+	fmt.Println("Discovery protocol started: ID=" + id + ", address=" + disc.LocalAddr())
 	fmt.Println("Hit Ctrl+C to exit")
 
 	// wait for Ctrl+c
