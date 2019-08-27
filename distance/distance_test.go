752bfe3cb621ab0d26bd33e263f2e5f5503c25ab
capossele <angelocapossele@gmail.com>
2019-08-27 11:02:10 +0100

    :white_check_mark: test updated

--- distance/distance_test.go
+++ distance/distance_test.go
@@ -32,7 +32,7 @@ func TestBySalt(t *testing.T) {
 	for _, test := range tests {
 		d := BySalt(test.x, test.y, test.salt)
 		got := (d == 0)
-		assert.Equal(t, test.zeroDistance, got, "Same Expected Zero")
+		assert.Equal(t, test.zeroDistance, got, "Zero Distance")
 	}
 
 }
