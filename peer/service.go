cbe25853b2444d744e15216ddf7f879a2dc40793
Wolfgang Welz <welzwo@gmail.com>
2019-10-21 16:48:51 +0200

    :rotating_light: Remove unused functions

--- peer/service.go
+++ peer/service.go
@@ -1,9 +1,5 @@
 package peer
 
-import (
-	pb "github.com/wollac/autopeering/peer/proto"
-)
-
 // NetworkAddress defines the tuple <Type, Port>, e.g, <TCP, 8000>
 type NetworkAddress struct {
 	Network string
@@ -17,32 +13,4 @@ type ServiceMap = map[string]*NetworkAddress
 // NewServiceMap initializes and returns an empty new ServiceMap
 func NewServiceMap() ServiceMap {
 	return make(ServiceMap)
-}
-
-// encodeService encodes a ServiceMap into a proto buffer ServiceMap message
-func encodeService(s ServiceMap) (result *pb.ServiceMap, err error) {
-	result = &pb.ServiceMap{}
-	result.Map = make(map[string]*pb.NetworkAddress)
-
-	for k, v := range s {
-		result.Map[k] = &pb.NetworkAddress{
-			Network: v.Network,
-			Address: v.Address,
-		}
-	}
-
-	return
-}
-
-// decodeService decodes a proto buffer ServiceMap message (in) into a ServiceMap (out)
-// out MUST NOT be nil
-func decodeService(in *pb.ServiceMap, out ServiceMap) (err error) {
-	for k, v := range in.GetMap() {
-		sp := &NetworkAddress{
-			Network: v.GetNetwork(),
-			Address: v.GetAddress(),
-		}
-		out[k] = sp
-	}
-	return
 }
