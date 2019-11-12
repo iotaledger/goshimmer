be151dd12eeb7137de5fccf02539caf1ac5624df
Wolfgang Welz <welzwo@gmail.com>
2019-11-12 19:41:22 +0100

    :white_check_mark: Add UDP connection transport test

---
+++ transport/conn_test.go
@@ -0,0 +1,42 @@
+package transport
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConnUdpClosed(t *testing.T) {
+	conn := openUDP(t)
+
+	conn.Close()
+	_, _, err := conn.ReadFrom()
+
+	want := "use of closed network connection"
+	assert.Contains(t, err.Error(), want)
+}
+
+func TestConnUdpPacket(t *testing.T) {
+	a := openUDP(t)
+	defer a.Close()
+	b := openUDP(t)
+	defer b.Close()
+
+	err := a.WriteTo(testPacket, b.LocalAddr())
+	require.NoError(t, err)
+
+	pkt, addr, err := b.ReadFrom()
+	require.NoError(t, err)
+
+	assert.Equal(t, pkt.GetData(), testPacket.GetData())
+	assert.Equal(t, addr, a.LocalAddr())
+}
+
+func openUDP(t *testing.T) *TransportConn {
+	c, err := net.ListenPacket("udp", "127.0.0.1:0")
+	require.NoError(t, err)
+
+	return Conn(c, func(network, address string) (net.Addr, error) { return net.ResolveUDPAddr(network, address) })
+}
