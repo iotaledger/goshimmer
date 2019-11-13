1b10d910a23cd151b59786c46bfa09ec08aa9ac8
Wolfgang Welz <welzwo@gmail.com>
2019-11-13 10:38:32 +0100

    Add SetWithTTL to the Database interface

--- packages/database/interfaces.go
+++ packages/database/interfaces.go
@@ -1,7 +1,10 @@
 package database
 
+import "time"
+
 type Database interface {
 	Set(key []byte, value []byte) error
+	SetWithTTL(key []byte, value []byte, ttl time.Duration) error
 	Contains(key []byte) (bool, error)
 	Get(key []byte) ([]byte, error)
 	ForEach(func(key []byte, value []byte)) error
