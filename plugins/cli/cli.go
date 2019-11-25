1ddd1aaf39e3b583ac663404e3cc70d5549c386e
Luca Moser <moser.luca@gmail.com>
2019-11-25 11:50:25 +0100

    fixes typos in CLI cfg options

--- plugins/cli/cli.go
+++ plugins/cli/cli.go
@@ -43,6 +43,6 @@ func printUsage() {
 	flag.PrintDefaults()
 
 	fmt.Fprintf(os.Stderr, "\nThe following plugins are enabled by default and can be disabled with -%s:\n  %s\n", node.CFG_DISABLE_PLUGINS, getList(enabledPlugins))
-	fmt.Fprintf(os.Stderr, "The following plugins are disabled by default and can be enabled with -%s:\n  %s\n", node.CFG_ENABLE_PLGUINS, getList(disabledPlugins))
-	fmt.Fprintf(os.Stderr, "The enabled/disabled plugins can be overriden by altering %s/%s inside config.json\n\n", node.CFG_ENABLE_PLGUINS, node.CFG_DISABLE_PLUGINS)
+	fmt.Fprintf(os.Stderr, "The following plugins are disabled by default and can be enabled with -%s:\n  %s\n", node.CFG_ENABLE_PLUGINS, getList(disabledPlugins))
+	fmt.Fprintf(os.Stderr, "The enabled/disabled plugins can be overriden by altering %s/%s inside config.json\n\n", node.CFG_ENABLE_PLUGINS, node.CFG_DISABLE_PLUGINS)
 }
