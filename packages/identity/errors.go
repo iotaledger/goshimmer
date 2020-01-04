c7c5501fc527cfd56e9d548a1e391afb6831520c
Wolfgang Welz <welzwo@gmail.com>
2020-01-04 15:38:49 +0100

    refactor: remove unused packages

--- packages/identity/errors.go
+++
@@ -1,8 +0,0 @@
-package identity
-
-import "errors"
-
-var (
-	ErrInvalidDataLen   = errors.New("identity: invalid input data length")
-	ErrInvalidSignature = errors.New("identity: invalid signature")
-)
