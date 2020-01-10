d001990eea9e410cdbacef54e7ea2c77848e5332
Wolfgang Welz <welzwo@gmail.com>
2020-01-10 12:04:30 +0100

    fix: remove selection flag and use gossip plugin

--- plugins/autopeering/parameters.go
+++ plugins/autopeering/parameters.go
@@ -6,10 +6,8 @@ import (
 
 const (
 	CFG_ENTRY_NODES = "autopeering.entryNodes"
-	CFG_SELECTION   = "autopeering.selection"
 )
 
 func init() {
 	flag.StringSlice(CFG_ENTRY_NODES, []string{"V8LYtWWcPYYDTTXLeIEFjJEuWlsjDiI0+Pq/Cx9ai6g=@116.202.49.178:14626"}, "list of trusted entry nodes for auto peering")
-	flag.Bool(CFG_SELECTION, true, "enable peer selection")
 }
