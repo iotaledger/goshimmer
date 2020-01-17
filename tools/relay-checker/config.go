3f7db5e097893a27cbb8fbc276ad8896b0173e37
jkrvivian <jkrvivian@gmail.com>
2020-01-17 18:44:43 +0800

    :construction: Rename files and minor tweaks

--- tools/relay-checker/config.go
+++ tools/relay-checker/config.go
@@ -5,8 +5,8 @@ import (
 )
 
 var (
+	nodes    []string
 	target   = ""
-	neighbor = ""
 	txnAddr  = "GOSHIMMER99TEST999999999999999999999999999999999999999999999999999999999999999999"
 	txnData  = "TEST99BROADCAST99DATA"
 	cooldown = 2
@@ -14,27 +14,27 @@ var (
 )
 
 func LoadConfig() {
-	if err := parameter.FetchConfig(true); err != nil {
+	if err := parameter.FetchConfig(false); err != nil {
 		panic(err)
 	}
 }
 
 func SetConfig() {
-	if parameter.NodeConfig.GetString("relaycheck.targetNode") != "" {
-		target = parameter.NodeConfig.GetString("relaycheck.targetNode")
-	} else {
+	if parameter.NodeConfig.GetString("relaycheck.targetNode") == "" {
 		panic("Set the target node address\n")
 	}
-	if parameter.NodeConfig.GetString("relaycheck.neighbor") != "" {
-		neighbor = parameter.NodeConfig.GetString("relaycheck.neighbor")
-	} else {
-		panic("Set the neighbor node address\n")
+	target = parameter.NodeConfig.GetString("relaycheck.targetNode")
+
+	if len(parameter.NodeConfig.GetStringSlice("relaycheck.nodes")) == 0 {
+		panic("Set node addresses\n")
 	}
+	nodes = append(nodes, parameter.NodeConfig.GetStringSlice("relaycheck.nodes")...)
+
 	// optional settings
 	if parameter.NodeConfig.GetString("relaycheck.txnaddress") != "" {
 		txnAddr = parameter.NodeConfig.GetString("relaycheck.txnaddress")
 	}
-	if parameter.NodeConfig.GetString("relaycheck.neighbor") != "" {
+	if parameter.NodeConfig.GetString("relaycheck.data") != "" {
 		txnData = parameter.NodeConfig.GetString("relaycheck.data")
 	}
 	if parameter.NodeConfig.GetInt("relaycheck.cooldowntime") > 0 {
---
+++ tools/relay-checker/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	client "github.com/iotaledger/goshimmer/client"
+	"github.com/iotaledger/goshimmer/packages/errors"
+	"github.com/iotaledger/iota.go/trinary"
+)
+
+func testBroadcastData(api *client.GoShimmerAPI) (trinary.Hash, error) {
+	txnHash, err := api.BroadcastData(txnAddr, txnData)
+	if err != nil {
+		return "", errors.Wrapf(err, "Broadcast failed")
+	}
+	fmt.Printf("txnHash: %s\n", txnHash)
+	return txnHash, nil
+}
+
+func testTargetGetTransactions(api *client.GoShimmerAPI, txnHash trinary.Hash) error {
+	// query target node for broadcasted data
+	_, err := api.GetTransactions([]trinary.Hash{txnHash})
+	if err != nil {
+		return errors.Wrapf(err, "Query target failed")
+	}
+	return nil
+}
+
+func testNodesGetTransactions(txnHash trinary.Hash) error {
+	// query nodes node for broadcasted data
+	for _, n := range nodes {
+		nodesApi := client.NewGoShimmerAPI(n)
+		_, err := nodesApi.GetTransactions([]trinary.Hash{txnHash})
+		if err != nil {
+			return errors.Wrapf(err, "Query %s failed", n)
+		}
+		fmt.Printf("txn found in %s\n", n)
+	}
+	return nil
+}
+
+func main() {
+	LoadConfig()
+	SetConfig()
+
+	api := client.NewGoShimmerAPI(target)
+	for i := 0; i < maxQuery; i++ {
+		txnHash, err := testBroadcastData(api)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			break
+		}
+
+		// cooldown time
+		time.Sleep(time.Duration(cooldown) * time.Second)
+
+		// query target node
+		err = testTargetGetTransactions(api, txnHash)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			break
+		}
+
+		// query nodes node
+		err = testNodesGetTransactions(txnHash)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			break
+		}
+	}
+}
