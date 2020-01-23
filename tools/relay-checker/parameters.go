aec6710ed539e422183de28c1e220364275da4c4
Luca Moser <moser.luca@gmail.com>
2020-01-23 17:08:19 +0100

    Adds relay-checker tool readme (#171)
    * partial readme
    
    * adds readme to the relay-checker and refactors it a little bit

--- tools/relay-checker/parameters.go
+++ tools/relay-checker/parameters.go
@@ -5,19 +5,19 @@ import (
 )
 
 const (
-	CFG_TARGET_NODE    = "relaycheck.targetNode"
-	CFG_TEST_NODES     = "relaycheck.nodes"
-	CFG_TXN_ADDRESS    = "relaycheck.txnAddress"
-	CFG_DATA           = "relaycheck.data"
-	CFG_COOL_DOWN_TIME = "relaycheck.cooldownTime"
-	CFG_MAX_QUERY      = "relaycheck.maxQuery"
+	CFG_TARGET_NODE   = "relayChecker.targetNode"
+	CFG_TEST_NODES    = "relayChecker.testNodes"
+	CFG_TX_ADDRESS    = "relayChecker.txAddress"
+	CFG_DATA          = "relayChecker.data"
+	CFG_COOLDOWN_TIME = "relayChecker.cooldownTime"
+	CFG_REPEAT        = "relayChecker.repeat"
 )
 
 func init() {
-	flag.StringSlice(CFG_TEST_NODES, []string{""}, "list of trusted entry nodes for auto peering")
-	flag.String(CFG_TARGET_NODE, "http://127.0.0.1:8080", "target node to test")
-	flag.String(CFG_TXN_ADDRESS, "SHIMMER99TEST99999999999999999999999999999999999999999999999999999999999999999999", "transaction address")
+	flag.StringSlice(CFG_TEST_NODES, []string{""}, "the list of nodes to check after the cooldown")
+	flag.String(CFG_TARGET_NODE, "http://127.0.0.1:8080", "the target node from the which transaction will be broadcasted from")
+	flag.String(CFG_TX_ADDRESS, "SHIMMER99TEST99999999999999999999999999999999999999999999999999999999999999999999", "the transaction address")
 	flag.String(CFG_DATA, "TEST99BROADCAST99DATA", "data to broadcast")
-	flag.Int(CFG_COOL_DOWN_TIME, 10, "cooldown time after broadcast data")
-	flag.Int(CFG_MAX_QUERY, 1, "the repeat times of relay-checker")
+	flag.Int(CFG_COOLDOWN_TIME, 10, "the cooldown time after broadcasting the data on the specified target node")
+	flag.Int(CFG_REPEAT, 1, "the amount of times to repeat the relay-checker queries")
 }
