c927c50a1f306d3c80b86462518763284573455e
Hans Moog <hm@mkjc.net>
2020-02-04 10:37:51 +0100

    Fix: fixed long locks when replaying events

--- plugins/analysis/webinterface/httpserver/data_stream.go
+++ plugins/analysis/webinterface/httpserver/data_stream.go
@@ -11,47 +11,44 @@ import (
 )
 
 func dataStream(ws *websocket.Conn) {
-	func() {
-		eventHandlers := &types.EventHandlers{
-			AddNode:         func(nodeId string) { fmt.Fprint(ws, "A"+nodeId) },
-			RemoveNode:      func(nodeId string) { fmt.Fprint(ws, "a"+nodeId) },
-			ConnectNodes:    func(sourceId string, targetId string) { fmt.Fprint(ws, "C"+sourceId+targetId) },
-			DisconnectNodes: func(sourceId string, targetId string) { fmt.Fprint(ws, "c"+sourceId+targetId) },
-			NodeOnline:      func(nodeId string) { fmt.Fprint(ws, "O"+nodeId) },
-			NodeOffline:     func(nodeId string) { fmt.Fprint(ws, "o"+nodeId) },
+	eventHandlers := &types.EventHandlers{
+		AddNode:         func(nodeId string) { fmt.Fprint(ws, "A"+nodeId) },
+		RemoveNode:      func(nodeId string) { fmt.Fprint(ws, "a"+nodeId) },
+		ConnectNodes:    func(sourceId string, targetId string) { fmt.Fprint(ws, "C"+sourceId+targetId) },
+		DisconnectNodes: func(sourceId string, targetId string) { fmt.Fprint(ws, "c"+sourceId+targetId) },
+		NodeOnline:      func(nodeId string) { fmt.Fprint(ws, "O"+nodeId) },
+		NodeOffline:     func(nodeId string) { fmt.Fprint(ws, "o"+nodeId) },
+	}
+
+	addNodeClosure := events.NewClosure(eventHandlers.AddNode)
+	removeNodeClosure := events.NewClosure(eventHandlers.RemoveNode)
+	connectNodesClosure := events.NewClosure(eventHandlers.ConnectNodes)
+	disconnectNodesClosure := events.NewClosure(eventHandlers.DisconnectNodes)
+	nodeOnlineClosure := events.NewClosure(eventHandlers.NodeOnline)
+	nodeOfflineClosure := events.NewClosure(eventHandlers.NodeOffline)
+
+	server.Events.AddNode.Attach(addNodeClosure)
+	server.Events.RemoveNode.Attach(removeNodeClosure)
+	server.Events.ConnectNodes.Attach(connectNodesClosure)
+	server.Events.DisconnectNodes.Attach(disconnectNodesClosure)
+	server.Events.NodeOnline.Attach(nodeOnlineClosure)
+	server.Events.NodeOffline.Attach(nodeOfflineClosure)
+
+	go recordedevents.Replay(eventHandlers)
+
+	buf := make([]byte, 1)
+	for {
+		if _, err := ws.Read(buf); err != nil {
+			break
 		}
 
-		addNodeClosure := events.NewClosure(eventHandlers.AddNode)
-		removeNodeClosure := events.NewClosure(eventHandlers.RemoveNode)
-		connectNodesClosure := events.NewClosure(eventHandlers.ConnectNodes)
-		disconnectNodesClosure := events.NewClosure(eventHandlers.DisconnectNodes)
-		nodeOnlineClosure := events.NewClosure(eventHandlers.NodeOnline)
-		nodeOfflineClosure := events.NewClosure(eventHandlers.NodeOffline)
+		fmt.Fprint(ws, "_")
+	}
 
-		server.Events.AddNode.Attach(addNodeClosure)
-		server.Events.RemoveNode.Attach(removeNodeClosure)
-		server.Events.ConnectNodes.Attach(connectNodesClosure)
-		server.Events.DisconnectNodes.Attach(disconnectNodesClosure)
-		server.Events.NodeOnline.Attach(nodeOnlineClosure)
-		server.Events.NodeOffline.Attach(nodeOfflineClosure)
-
-		go recordedevents.Replay(eventHandlers)
-
-		buf := make([]byte, 1)
-	readFromWebsocket:
-		for {
-			if _, err := ws.Read(buf); err != nil {
-				break readFromWebsocket
-			}
-
-			fmt.Fprint(ws, "_")
-		}
-
-		server.Events.AddNode.Detach(addNodeClosure)
-		server.Events.RemoveNode.Detach(removeNodeClosure)
-		server.Events.ConnectNodes.Detach(connectNodesClosure)
-		server.Events.DisconnectNodes.Detach(disconnectNodesClosure)
-		server.Events.NodeOnline.Detach(nodeOnlineClosure)
-		server.Events.NodeOffline.Detach(nodeOfflineClosure)
-	}()
+	server.Events.AddNode.Detach(addNodeClosure)
+	server.Events.RemoveNode.Detach(removeNodeClosure)
+	server.Events.ConnectNodes.Detach(connectNodesClosure)
+	server.Events.DisconnectNodes.Detach(disconnectNodesClosure)
+	server.Events.NodeOnline.Detach(nodeOnlineClosure)
+	server.Events.NodeOffline.Detach(nodeOfflineClosure)
 }
--- plugins/analysis/webinterface/recordedevents/recorded_events.go
+++ plugins/analysis/webinterface/recordedevents/recorded_events.go
@@ -78,9 +78,21 @@ func Configure(plugin *node.Plugin) {
 
 func Replay(handlers *types.EventHandlers) {
 	lock.Lock()
-	defer lock.Unlock()
-
+	copiedNodes := make(map[string]bool)
 	for nodeId, online := range nodes {
+		copiedNodes[nodeId] = online
+	}
+
+	copiedLinks := make(map[string]map[string]bool)
+	for sourceId, targetMap := range links {
+		copiedLinks[sourceId] = make(map[string]bool)
+		for targetId := range targetMap {
+			copiedLinks[sourceId][targetId] = true
+		}
+	}
+	lock.Unlock()
+
+	for nodeId, online := range copiedNodes {
 		handlers.AddNode(nodeId)
 		if online {
 			handlers.NodeOnline(nodeId)
@@ -89,7 +101,7 @@ func Replay(handlers *types.EventHandlers) {
 		}
 	}
 
-	for sourceId, targetMap := range links {
+	for sourceId, targetMap := range copiedLinks {
 		for targetId := range targetMap {
 			handlers.ConnectNodes(sourceId, targetId)
 		}
