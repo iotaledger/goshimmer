acd0da112a3d7f20b1865f0a029d9a80cf9978ab
jkrvivian <jkrvivian@gmail.com>
2020-02-27 16:52:42 +0800

    fix: remove prefix "webapi" in webapi bundles

--- pluginmgr/webapi/plugins.go
+++ pluginmgr/webapi/plugins.go
@@ -2,25 +2,25 @@ package webapi
 
 import (
 	"github.com/iotaledger/goshimmer/plugins/webapi"
-	webapi_broadcastData "github.com/iotaledger/goshimmer/plugins/webapi/broadcastData"
-	webapi_findTransactionHashes "github.com/iotaledger/goshimmer/plugins/webapi/findTransactionHashes"
-	webapi_getNeighbors "github.com/iotaledger/goshimmer/plugins/webapi/getNeighbors"
-	webapi_getTransactionObjectsByHash "github.com/iotaledger/goshimmer/plugins/webapi/getTransactionObjectsByHash"
-	webapi_getTransactionTrytesByHash "github.com/iotaledger/goshimmer/plugins/webapi/getTransactionTrytesByHash"
-	webapi_gtta "github.com/iotaledger/goshimmer/plugins/webapi/gtta"
-	webapi_spammer "github.com/iotaledger/goshimmer/plugins/webapi/spammer"
-	webapi_auth "github.com/iotaledger/goshimmer/plugins/webauth"
+	"github.com/iotaledger/goshimmer/plugins/webapi/broadcastData"
+	"github.com/iotaledger/goshimmer/plugins/webapi/findTransactionHashes"
+	"github.com/iotaledger/goshimmer/plugins/webapi/getNeighbors"
+	"github.com/iotaledger/goshimmer/plugins/webapi/getTransactionObjectsByHash"
+	"github.com/iotaledger/goshimmer/plugins/webapi/getTransactionTrytesByHash"
+	"github.com/iotaledger/goshimmer/plugins/webapi/gtta"
+	"github.com/iotaledger/goshimmer/plugins/webapi/spammer"
+	"github.com/iotaledger/goshimmer/plugins/webauth"
 	"github.com/iotaledger/hive.go/node"
 )
 
 var PLUGINS = node.Plugins(
 	webapi.PLUGIN,
-	webapi_auth.PLUGIN,
-	webapi_gtta.PLUGIN,
-	webapi_spammer.PLUGIN,
-	webapi_broadcastData.PLUGIN,
-	webapi_getTransactionTrytesByHash.PLUGIN,
-	webapi_getTransactionObjectsByHash.PLUGIN,
-	webapi_findTransactionHashes.PLUGIN,
-	webapi_getNeighbors.PLUGIN,
+	webauth.PLUGIN,
+	gtta.PLUGIN,
+	spammer.PLUGIN,
+	broadcastData.PLUGIN,
+	getTransactionTrytesByHash.PLUGIN,
+	getTransactionObjectsByHash.PLUGIN,
+	findTransactionHashes.PLUGIN,
+	getNeighbors.PLUGIN,
 )
