594efbcbbb64dde92ce8d5b06c4877b98f125f23
Hans Moog <hm@mkjc.net>
2020-03-04 00:20:29 +0100

    Fix: fixed wrong variables in Parse of marshalUtil

--- packages/binary/marshalutil/marshalutil.go
+++ packages/binary/marshalutil/marshalutil.go
@@ -38,7 +38,7 @@ func New(args ...interface{}) *MarshalUtil {
 	}
 }
 
-func (util *MarshalUtil) Parse(parser func(data []byte) (result interface{}, err error, consumedBytes int)) (result interface{}, err error) {
+func (util *MarshalUtil) Parse(parser func(data []byte) (interface{}, error, int)) (result interface{}, err error) {
 	result, err, readBytes := parser(util.bytes[util.readOffset:])
 	if err != nil {
 		util.ReadSeek(util.readOffset + readBytes)
