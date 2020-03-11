22c8fb46abbfdf20645b742cdae6a7d4f27e0a33
Hans Moog <hm@mkjc.net>
2020-03-11 01:00:57 +0100

    Docs: added some additional docs

--- packages/binary/marshalutil/marshalutil.time.go
+++ packages/binary/marshalutil/marshalutil.time.go
@@ -4,14 +4,21 @@ import (
 	"time"
 )
 
+// WriteTime marshals the given time into a sequence of bytes, that get appended to the internal buffer.
 func (util *MarshalUtil) WriteTime(timeToWrite time.Time) {
 	marshaledBytes, err := timeToWrite.MarshalBinary()
 	if err != nil {
-		marshaledBytes, _ = time.Time{}.MarshalBinary()
+		// if marshaling fails due to a corrupted time -> we "recover" to the zero value
+		marshaledBytes, err = time.Time{}.MarshalBinary()
+		if err != nil {
+			// if the recovery fails, we panic (sth is really broken)
+			panic(err)
+		}
 	}
 	util.WriteBytes(marshaledBytes)
 }
 
+// ReadTime unmarshals a time object from the internal read buffer.
 func (util *MarshalUtil) ReadTime() (result time.Time, err error) {
 	bytes, err := util.ReadBytes(15)
 	if err != nil {
