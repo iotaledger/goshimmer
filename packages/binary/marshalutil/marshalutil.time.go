9bcd89ba161e79d7031bf251afe78c20f9cf7806
Hans Moog <hm@mkjc.net>
2020-03-16 23:22:52 +0100

    Feat: changed time marshal to use nanoseconds

--- packages/binary/marshalutil/marshalutil.time.go
+++ packages/binary/marshalutil/marshalutil.time.go
@@ -6,28 +6,17 @@ import (
 
 // WriteTime marshals the given time into a sequence of bytes, that get appended to the internal buffer.
 func (util *MarshalUtil) WriteTime(timeToWrite time.Time) {
-	marshaledBytes, err := timeToWrite.MarshalBinary()
-	if err != nil {
-		// if marshaling fails due to a corrupted time -> we "recover" to the zero value
-		marshaledBytes, err = time.Time{}.MarshalBinary()
-		if err != nil {
-			// if the recovery fails, we panic (sth is really broken)
-			panic(err)
-		}
-	}
-	util.WriteBytes(marshaledBytes)
+	util.WriteInt64(timeToWrite.UnixNano())
 }
 
 // ReadTime unmarshals a time object from the internal read buffer.
 func (util *MarshalUtil) ReadTime() (result time.Time, err error) {
-	bytes, err := util.ReadBytes(15)
+	nanoSeconds, err := util.ReadInt64()
 	if err != nil {
 		return
 	}
 
-	if err = result.UnmarshalBinary(bytes); err != nil {
-		util.ReadSeek(util.ReadOffset() - 15)
-	}
+	result = time.Unix(0, nanoSeconds)
 
 	return
 }
