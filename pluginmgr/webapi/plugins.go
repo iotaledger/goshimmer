e90eec906ba588c9d4d4c43cb693aedba7ac9eb2
jonastheis <mail@jonastheis.de>
2020-03-30 17:40:53 +0200

    Add getMessageByHash endpoint

--- pluginmgr/webapi/plugins.go
+++ pluginmgr/webapi/plugins.go
@@ -4,6 +4,7 @@ import (
 	"github.com/iotaledger/goshimmer/plugins/webapi"
 	"github.com/iotaledger/goshimmer/plugins/webapi/broadcastData"
 	"github.com/iotaledger/goshimmer/plugins/webapi/findTransactionHashes"
+	"github.com/iotaledger/goshimmer/plugins/webapi/getMessageByHash"
 	"github.com/iotaledger/goshimmer/plugins/webapi/getNeighbors"
 	"github.com/iotaledger/goshimmer/plugins/webapi/getTransactionObjectsByHash"
 	"github.com/iotaledger/goshimmer/plugins/webapi/getTransactionTrytesByHash"
@@ -23,4 +24,5 @@ var PLUGINS = node.Plugins(
 	getTransactionObjectsByHash.PLUGIN,
 	findTransactionHashes.PLUGIN,
 	getNeighbors.PLUGIN,
+	getMessageByHash.PLUGIN,
 )
