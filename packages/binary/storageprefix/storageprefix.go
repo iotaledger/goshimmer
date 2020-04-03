545abba06702e23cc294eac2ce2284200dcd7c03
Hans Moog <hm@mkjc.net>
2020-04-03 16:34:21 +0200

    Feat: refactored some code

--- packages/binary/storageprefix/storageprefix.go
+++ packages/binary/storageprefix/storageprefix.go
@@ -1,7 +1,10 @@
 package storageprefix
 
 const (
+	// the following values are a list of prefixes defined as an enum
 	_ byte = iota
+
+	// package specific prefixes used for the objectstorage in the corresponding packages
 	MessageLayer
 	ValueTransfers
 )
