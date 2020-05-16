99da890054e33a03e4afe4372ee1bba8961665f7
Hans Moog <hm@mkjc.net>
2020-05-16 15:18:25 +0200

    Feat: Adding the ability to book ValueObjects into their corresponding branch (#414)
    * Feat: added valueobjectbooking model
    
    * Docs: adjusted docs
    
    * Docs: updated another comment
    
    * Feat: added first function to book ValueObjects
    
    * Feat: intermediary commit so nothing gets lost
    
    * Feat: nearly done with the refactor
    
    * Feat: nearly finished the tangle refactor
    
    * Feat: finished refactor
    
    * Fix: fixed unreleased object
    
    * Refactor: removed comment
    
    * Feat: added LedgerState.Balances
    
    * Feat: first successful test for valuetransfer
    
    * Feat: refactor
    
    * Fix: fixed bug in BranchesConflicting
    
    * Fix: fixed annotations from cilint
    
    * Feat: commit so we can merge :D

--- dapps/valuetransfers/packages/tangle/events.go
+++ dapps/valuetransfers/packages/tangle/events.go
@@ -3,7 +3,9 @@ package tangle
 import (
 	"github.com/iotaledger/hive.go/events"
 
+	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/branchmanager"
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/payload"
+	"github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/transaction"
 )
 
 // Events is a container for the different kind of events of the Tangle.
@@ -14,6 +16,18 @@ type Events struct {
 	MissingPayloadReceived *events.Event
 	PayloadMissing         *events.Event
 	PayloadUnsolidifiable  *events.Event
+
+	// TransactionReceived gets triggered whenever a transaction was received for the first time (not solid yet).
+	TransactionReceived *events.Event
+
+	// TransactionBooked gets triggered whenever a transactions becomes solid and gets booked into a particular branch.
+	TransactionBooked *events.Event
+
+	// Fork gets triggered when a previously un-conflicting transaction get's some of its inputs double spend, so that a
+	// new Branch is created.
+	Fork *events.Event
+
+	Error *events.Event
 }
 
 func newEvents() *Events {
@@ -23,6 +37,10 @@ func newEvents() *Events {
 		MissingPayloadReceived: events.NewEvent(cachedPayloadEvent),
 		PayloadMissing:         events.NewEvent(payloadIDEvent),
 		PayloadUnsolidifiable:  events.NewEvent(payloadIDEvent),
+		TransactionReceived:    events.NewEvent(cachedTransactionEvent),
+		TransactionBooked:      events.NewEvent(transactionBookedEvent),
+		Fork:                   events.NewEvent(forkEvent),
+		Error:                  events.NewEvent(events.ErrorCaller),
 	}
 }
 
@@ -34,5 +52,32 @@ func cachedPayloadEvent(handler interface{}, params ...interface{}) {
 	handler.(func(*payload.CachedPayload, *CachedPayloadMetadata))(
 		params[0].(*payload.CachedPayload).Retain(),
 		params[1].(*CachedPayloadMetadata).Retain(),
+	)
+}
+
+func transactionBookedEvent(handler interface{}, params ...interface{}) {
+	handler.(func(*transaction.CachedTransaction, *CachedTransactionMetadata, *branchmanager.CachedBranch, []transaction.OutputID, bool))(
+		params[0].(*transaction.CachedTransaction).Retain(),
+		params[1].(*CachedTransactionMetadata).Retain(),
+		params[2].(*branchmanager.CachedBranch).Retain(),
+		params[3].([]transaction.OutputID),
+		params[4].(bool),
+	)
+}
+
+func forkEvent(handler interface{}, params ...interface{}) {
+	handler.(func(*transaction.CachedTransaction, *CachedTransactionMetadata, *branchmanager.CachedBranch, []transaction.OutputID))(
+		params[0].(*transaction.CachedTransaction).Retain(),
+		params[1].(*CachedTransactionMetadata).Retain(),
+		params[2].(*branchmanager.CachedBranch).Retain(),
+		params[3].([]transaction.OutputID),
+	)
+}
+
+func cachedTransactionEvent(handler interface{}, params ...interface{}) {
+	handler.(func(*transaction.CachedTransaction, *CachedTransactionMetadata, *CachedAttachment))(
+		params[0].(*transaction.CachedTransaction).Retain(),
+		params[1].(*CachedTransactionMetadata).Retain(),
+		params[2].(*CachedAttachment).Retain(),
 	)
 }
