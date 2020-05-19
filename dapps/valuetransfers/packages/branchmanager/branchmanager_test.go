f68c8499e3f00e70b19eae8b78b7fd9cd552b448
Luca Moser <moser.luca@gmail.com>
2020-05-19 10:47:09 +0200

    cleans failing test

--- dapps/valuetransfers/packages/branchmanager/branchmanager_test.go
+++ dapps/valuetransfers/packages/branchmanager/branchmanager_test.go
@@ -33,68 +33,98 @@ func TestConflictDetection(t *testing.T) {
 	defer cachedBranch3.Release()
 	branch3 := cachedBranch3.Unwrap()
 
-	conflicting, err := branchManager.BranchesConflicting(MasterBranchID, branch2.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 2 should not be conflicting with master branch")
+	{
+		conflicting, err := branchManager.BranchesConflicting(MasterBranchID, branch2.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 2 should not be conflicting with master branch")
 
-	conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch2.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 3 should not be conflicting with master branch")
+		conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch2.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 3 should not be conflicting with master branch")
 
-	conflicting, err = branchManager.BranchesConflicting(branch2.ID(), branch3.ID())
-	assert.NoError(t, err)
-	assert.True(t, conflicting, "branch 2 & 3 should be conflicting with each other")
+		conflicting, err = branchManager.BranchesConflicting(branch2.ID(), branch3.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 2 & 3 should be conflicting with each other")
+	}
 
 	// spawn of branch 4 and 5 from branch 2
 	cachedBranch4, _ := branchManager.Fork(BranchID{4}, []BranchID{branch2.ID()}, []ConflictID{{1}})
 	defer cachedBranch4.Release()
 	branch4 := cachedBranch4.Unwrap()
 
-	conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch4.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 4 should not be conflicting with master branch")
-
-	conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch4.ID())
-	assert.NoError(t, err)
-	assert.True(t, conflicting, "branch 3 & 4 should be conflicting with each other")
-
 	cachedBranch5, _ := branchManager.Fork(BranchID{5}, []BranchID{branch2.ID()}, []ConflictID{{1}})
 	defer cachedBranch5.Release()
 	branch5 := cachedBranch5.Unwrap()
 
-	conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch5.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 5 should not be conflicting with master branch")
+	{
+		conflicting, err := branchManager.BranchesConflicting(MasterBranchID, branch4.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 4 should not be conflicting with master branch")
 
-	conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch5.ID())
-	assert.NoError(t, err)
-	assert.True(t, conflicting, "branch 3 & 5 should be conflicting with each other")
+		conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch4.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 3 & 4 should be conflicting with each other")
 
-	// branch 6, 7 is on the same level as 2 and 3 but are not part of that conflict set
+		conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch5.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 5 should not be conflicting with master branch")
+
+		conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch5.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 3 & 5 should be conflicting with each other")
+
+		// since both consume the same output
+		conflicting, err = branchManager.BranchesConflicting(branch4.ID(), branch5.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 4 & 5 should be conflicting with each other")
+	}
+
+	// branch 6, 7 are on the same level as 2 and 3 but are not part of that conflict set
 	cachedBranch6, _ := branchManager.Fork(BranchID{6}, []BranchID{MasterBranchID}, []ConflictID{{2}})
 	defer cachedBranch6.Release()
 	branch6 := cachedBranch6.Unwrap()
 
-	conflicting, err = branchManager.BranchesConflicting(branch2.ID(), branch6.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 6 should not be conflicting with branch 2")
-
-	conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch6.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 6 should not be conflicting with branch 3")
-
-	// branch 6, 7 is on the same level as 2 and 3 but are not part of that conflict set
 	cachedBranch7, _ := branchManager.Fork(BranchID{7}, []BranchID{MasterBranchID}, []ConflictID{{2}})
 	defer cachedBranch7.Release()
 	branch7 := cachedBranch7.Unwrap()
 
-	conflicting, err = branchManager.BranchesConflicting(branch2.ID(), branch7.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 7 should not be conflicting with branch 2")
+	{
+		conflicting, err := branchManager.BranchesConflicting(branch2.ID(), branch6.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 6 should not be conflicting with branch 2")
 
-	conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch7.ID())
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "branch 7 should not be conflicting with branch 3")
+		conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch6.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 6 should not be conflicting with branch 3")
+
+		conflicting, err = branchManager.BranchesConflicting(branch2.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 7 should not be conflicting with branch 2")
+
+		conflicting, err = branchManager.BranchesConflicting(branch3.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 7 should not be conflicting with branch 3")
+
+		conflicting, err = branchManager.BranchesConflicting(branch6.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 6 & 7 should be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(branch4.ID(), branch6.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 6 should not be conflicting with branch 4")
+
+		conflicting, err = branchManager.BranchesConflicting(branch5.ID(), branch6.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 6 should not be conflicting with branch 5")
+
+		conflicting, err = branchManager.BranchesConflicting(branch4.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 7 should not be conflicting with branch 4")
+
+		conflicting, err = branchManager.BranchesConflicting(branch5.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 7 should not be conflicting with branch 5")
+	}
 
 	// aggregated branch out of branch 4 (child of branch 2) and branch 6
 	cachedAggrBranch8, err := branchManager.AggregateBranches(branch4.ID(), branch6.ID())
@@ -102,16 +132,32 @@ func TestConflictDetection(t *testing.T) {
 	defer cachedAggrBranch8.Release()
 	aggrBranch8 := cachedAggrBranch8.Unwrap()
 
-	conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), MasterBranchID)
-	assert.NoError(t, err)
-	assert.False(t, conflicting, "aggr. branch 8 should not be conflicting with master branch")
+	{
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch8.ID(), MasterBranchID)
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 8 should not be conflicting with master branch")
 
-	// conflicting since branch 2 and branch 3 are
-	conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), branch3.ID())
-	assert.NoError(t, err)
-	assert.True(t, conflicting, "aggr. branch 8 & branch 3 should be conflicting with each other")
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), branch2.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 8 should not be conflicting with branch 2")
 
-	// aggregated branch out of aggr. branch 8 and branch 7,
+		// conflicting since branch 2 and branch 3 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), branch3.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 8 & branch 3 should be conflicting with each other")
+
+		// conflicting since branch 4 and branch 5 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), branch5.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 8 & branch 5 should be conflicting with each other")
+
+		// conflicting since branch 6 and branch 7 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 8 & branch 7 should be conflicting with each other")
+	}
+
+	// aggregated branch out of aggr. branch 8 and branch 7:
 	// should fail since branch 6 & 7 are conflicting
 	_, err = branchManager.AggregateBranches(aggrBranch8.ID(), branch7.ID())
 	assert.Error(t, err)
@@ -122,8 +168,11 @@ func TestConflictDetection(t *testing.T) {
 	defer cachedAggrBranch9.Release()
 	aggrBranch9 := cachedAggrBranch9.Unwrap()
 
-	// aggr. branch 8 and 9 should be conflicting, since 4&5 and 6&7 are
-	conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), aggrBranch9.ID())
-	assert.NoError(t, err)
-	assert.True(t, conflicting, "aggr. branch 8 & branch 9 should be conflicting with each other")
+	{
+		// aggr. branch 8 and 9 should be conflicting, since 4&5 and 6&7 are
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch8.ID(), aggrBranch9.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 8 & branch 9 should be conflicting with each other")
+	}
+
 }
