3f3d65099ca537c8fba3a312bcc44d30e3a4a373
Luca Moser <moser.luca@gmail.com>
2020-05-19 13:25:08 +0200

    expands branch conflict detection test

--- dapps/valuetransfers/packages/branchmanager/branchmanager_test.go
+++ dapps/valuetransfers/packages/branchmanager/branchmanager_test.go
@@ -171,10 +171,155 @@ func TestConflictDetection(t *testing.T) {
 	assert.NotEqual(t, aggrBranch8.ID().String(), aggrBranch9.ID().String(), "aggr. branches 8 & 9 should  have different IDs")
 
 	{
-		// aggr. branch 8 and 9 should be conflicting, since 4&5 and 6&7 are
-		conflicting, err := branchManager.BranchesConflicting(aggrBranch8.ID(), aggrBranch9.ID())
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch9.ID(), MasterBranchID)
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 9 should not be conflicting with master branch")
+
+		// aggr. branch 8 and 9 should be conflicting, since 4 & 5 and 6 & 7 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), aggrBranch9.ID())
 		assert.NoError(t, err)
 		assert.True(t, conflicting, "aggr. branch 8 & branch 9 should be conflicting with each other")
+
+		// conflicting since branch 3 & 2 are
+		conflicting, err = branchManager.BranchesConflicting(branch3.ID(), aggrBranch9.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 9 & branch 3 should be conflicting with each other")
+	}
+
+	// aggregated branch out of branch 3 and branch 6
+	cachedAggrBranch10, err := branchManager.AggregateBranches(branch3.ID(), branch6.ID())
+	assert.NoError(t, err)
+	defer cachedAggrBranch10.Release()
+	aggrBranch10 := cachedAggrBranch10.Unwrap()
+
+	{
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch10.ID(), MasterBranchID)
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 10 should not be conflicting with master branch")
+
+		// aggr. branch 8 and 10 should be conflicting, since 2 & 3 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch8.ID(), aggrBranch10.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 8 & branch 10 should be conflicting with each other")
+
+		// aggr. branch 9 and 10 should be conflicting, since 2 & 3 and 6 & 7 are
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch9.ID(), aggrBranch10.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 9 & branch 10 should be conflicting with each other")
+	}
+
+	// branch 11, 12 are on the same level as 2 & 3 and 6 & 7 but are not part of either conflict set
+	cachedBranch11, _ := branchManager.Fork(BranchID{11}, []BranchID{MasterBranchID}, []ConflictID{{3}})
+	defer cachedBranch11.Release()
+	branch11 := cachedBranch11.Unwrap()
+
+	cachedBranch12, _ := branchManager.Fork(BranchID{12}, []BranchID{MasterBranchID}, []ConflictID{{3}})
+	defer cachedBranch12.Release()
+	branch12 := cachedBranch12.Unwrap()
+
+	{
+		conflicting, err := branchManager.BranchesConflicting(MasterBranchID, branch11.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 11 should not be conflicting with master branch")
+
+		conflicting, err = branchManager.BranchesConflicting(MasterBranchID, branch12.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "branch 12 should not be conflicting with master branch")
+
+		conflicting, err = branchManager.BranchesConflicting(branch11.ID(), branch12.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "branch 11 & 12 should be conflicting with each other")
+	}
+
+	// aggr. branch 13 out of branch 6 and 11
+	cachedAggrBranch13, err := branchManager.AggregateBranches(branch6.ID(), branch11.ID())
+	assert.NoError(t, err)
+	defer cachedAggrBranch13.Release()
+	aggrBranch13 := cachedAggrBranch13.Unwrap()
+
+	{
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch13.ID(), aggrBranch9.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 9 & 13 should be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch13.ID(), aggrBranch8.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 8 & 13 should not be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch13.ID(), aggrBranch10.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 10 & 13 should not be conflicting with each other")
+	}
+
+	// aggr. branch 14 out of aggr. branch 10 and 13
+	cachedAggrBranch14, err := branchManager.AggregateBranches(aggrBranch10.ID(), aggrBranch13.ID())
+	assert.NoError(t, err)
+	defer cachedAggrBranch14.Release()
+	aggrBranch14 := cachedAggrBranch14.Unwrap()
+
+	{
+		// aggr. branch 9 has parent branch 7 which conflicts with ancestor branch 6 of aggr. branch 14
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch14.ID(), aggrBranch9.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 14 & 9 should be conflicting with each other")
+
+		// aggr. branch has ancestor branch 2 which conflicts with ancestor branch 3 of aggr. branch 14
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch14.ID(), aggrBranch8.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 14 & 8 should be conflicting with each other")
+	}
+
+	// aggr. branch 15 out of branch 2, 7 and 12
+	cachedAggrBranch15, err := branchManager.AggregateBranches(branch2.ID(), branch7.ID(), branch12.ID())
+	assert.NoError(t, err)
+	defer cachedAggrBranch15.Release()
+	aggrBranch15 := cachedAggrBranch15.Unwrap()
+
+	{
+		// aggr. branch 13 has parent branches 11 & 6 which conflicts which conflicts with ancestor branches 12 & 7 of aggr. branch 15
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch15.ID(), aggrBranch13.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 15 & 13 should be conflicting with each other")
+
+		// aggr. branch 10 has parent branches 3 & 6 which conflicts with ancestor branches 2 & 7 of aggr. branch 15
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch15.ID(), aggrBranch10.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 15 & 10 should be conflicting with each other")
+
+		// aggr. branch 8 has parent branch 6 which conflicts with ancestor branch 7 of aggr. branch 15
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch15.ID(), aggrBranch8.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 15 & 8 should be conflicting with each other")
+	}
+
+	// aggr. branch 16 out of aggr. branches 15 and 9
+	cachedAggrBranch16, err := branchManager.AggregateBranches(aggrBranch15.ID(), aggrBranch9.ID())
+	assert.NoError(t, err)
+	defer cachedAggrBranch16.Release()
+	aggrBranch16 := cachedAggrBranch16.Unwrap()
+
+	{
+		// sanity check
+		conflicting, err := branchManager.BranchesConflicting(aggrBranch16.ID(), aggrBranch9.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 16 & 9 should not be conflicting with each other")
+
+		// sanity check
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch16.ID(), branch7.ID())
+		assert.NoError(t, err)
+		assert.False(t, conflicting, "aggr. branch 16 & 9 should not be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch16.ID(), aggrBranch13.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 16 & 13 should be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch16.ID(), aggrBranch14.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 16 & 14 should be conflicting with each other")
+
+		conflicting, err = branchManager.BranchesConflicting(aggrBranch16.ID(), aggrBranch8.ID())
+		assert.NoError(t, err)
+		assert.True(t, conflicting, "aggr. branch 16 & 8 should be conflicting with each other")
 	}
 
 }
