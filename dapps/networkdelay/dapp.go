bface0d5353ef518f65f9d85075c9297b81307d1
jonastheis <mail@jonastheis.de>
2020-06-15 11:41:46 +0200

    Add check for issuer public key

--- dapps/networkdelay/dapp.go
+++ dapps/networkdelay/dapp.go
@@ -1,24 +1,28 @@
 package networkdelay
 
 import (
-	"encoding/json"
 	"fmt"
-	"net"
 	"time"
 
 	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/message"
 	messageTangle "github.com/iotaledger/goshimmer/packages/binary/messagelayer/tangle"
 	"github.com/iotaledger/goshimmer/plugins/autopeering/local"
+	"github.com/iotaledger/goshimmer/plugins/config"
 	"github.com/iotaledger/goshimmer/plugins/messagelayer"
 	"github.com/iotaledger/goshimmer/plugins/remotelog"
+	"github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/iotaledger/hive.go/events"
 	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/hive.go/node"
+	"github.com/mr-tron/base58"
 )
 
 const (
 	// PluginName contains the human readable name of the plugin.
 	PluginName = "NetworkDelay"
+
+	// CfgNetworkDelayOriginPublicKey defines the config flag of the issuer node public key.
+	CfgNetworkDelayOriginPublicKey = "networkdelay.originPublicKey"
 
 	remoteLogType = "networkdelay"
 )
@@ -30,18 +34,32 @@ var (
 	// log holds a reference to the logger used by this app.
 	log *logger.Logger
 
-	remoteLogConnection net.Conn
-	myID                string
+	remoteLogger *remotelog.RemoteLoggerConn
+
+	myID            string
+	myPublicKey     ed25519.PublicKey
+	originPublicKey ed25519.PublicKey
 )
 
 func configure(_ *node.Plugin) {
 	// configure logger
 	log = logger.NewLogger(PluginName)
 
-	remoteLogConnection = remotelog.Connection()
+	remoteLogger = remotelog.RemoteLogger()
 
 	if local.GetInstance() != nil {
 		myID = local.GetInstance().ID().String()
+		myPublicKey = local.GetInstance().PublicKey()
+	}
+
+	// get origin public key from config
+	bytes, err := base58.Decode(config.Node.GetString(CfgNetworkDelayOriginPublicKey))
+	if err != nil {
+		log.Fatalf("could not parse %s config entry as base58. %v", CfgNetworkDelayOriginPublicKey, err)
+	}
+	originPublicKey, _, err = ed25519.PublicKeyFromBytes(bytes)
+	if err != nil {
+		log.Fatalf("could not parse %s config entry as public key. %v", CfgNetworkDelayOriginPublicKey, err)
 	}
 
 	configureWebAPI()
@@ -63,6 +81,12 @@ func onReceiveMessageFromMessageLayer(cachedMessage *message.CachedMessage, cach
 
 	messagePayload := solidMessage.Payload()
 	if messagePayload.Type() != Type {
+		return
+	}
+
+	// check for node identity
+	issuerPubKey := solidMessage.IssuerPublicKey()
+	if issuerPubKey != originPublicKey || issuerPubKey == myPublicKey {
 		return
 	}
 
@@ -95,9 +119,7 @@ func sendToRemoteLog(networkDelayObject *Object, receiveTime int64) {
 		Delta:       receiveTime - networkDelayObject.sentTime,
 		Type:        remoteLogType,
 	}
-	b, _ := json.Marshal(m)
-	fmt.Fprint(remoteLogConnection, string(b))
-	fmt.Println(string(b))
+	_ = remoteLogger.Send(m)
 }
 
 type networkDelay struct {
