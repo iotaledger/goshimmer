36fddd0fb073457f88ec1f341b602952d5f6ad6c
Luca Moser <moser.luca@gmail.com>
2020-06-16 12:28:08 +0200

    readd all tests again

--- tools/integration-tests/assets/entrypoint.sh
+++ tools/integration-tests/assets/entrypoint.sh
@@ -6,4 +6,4 @@ chmod 777 /assets/*
 echo "assets:"
 ls /assets
 echo "running tests..."
-go test ./tests/"${TEST_NAME}" -run TestConsensusFiftyFiftyOpinionSplit -v -timeout 30m
+go test ./tests/"${TEST_NAME}" -v -timeout 30m
