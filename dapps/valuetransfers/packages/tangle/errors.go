9d16a32cb7a8c80692964bda10adb33d4f21f109
Hans Moog <hm@mkjc.net>
2020-06-18 19:41:47 +0200

    Refactor: added missing comment for error

--- dapps/valuetransfers/packages/tangle/errors.go
+++ dapps/valuetransfers/packages/tangle/errors.go
@@ -6,9 +6,9 @@ var (
 	// ErrFatal represents an error that is not "expected".
 	ErrFatal = errors.New("fatal error")
 
-	// ErrTransactionInvalid represents an error that is triggered when an invalid transaction is issued.
+	// ErrTransactionInvalid represents an error type that is triggered when an invalid transaction is detected.
 	ErrTransactionInvalid = errors.New("transaction invalid")
 
-	//
+	// ErrPayloadInvalid represents an error type that is triggered when an invalid payload is detected.
 	ErrPayloadInvalid = errors.New("payload invalid")
 )
