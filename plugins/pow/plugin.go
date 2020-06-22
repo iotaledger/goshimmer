a983ef88d554784827868b38dec9262fe48be70d
Wolfgang Welz <welzwo@gmail.com>
2020-06-22 11:36:57 +0200

    fix panic when messagelayer is disabled

--- plugins/pow/plugin.go
+++ plugins/pow/plugin.go
@@ -3,6 +3,7 @@ package pow
 import (
 	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/messagefactory"
 	"github.com/iotaledger/goshimmer/plugins/messagelayer"
+	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/hive.go/node"
 )
 
@@ -15,6 +16,14 @@ var (
 )
 
 func run(*node.Plugin) {
+	// assure that the logger is available
+	log := logger.NewLogger(PluginName)
+
+	if node.IsSkipped(messagelayer.Plugin) {
+		log.Infof("%s is disabled; skipping %s\n", messagelayer.PluginName, PluginName)
+		return
+	}
+
 	messagelayer.MessageParser.AddMessageFilter(&powFilter{})
 	messagelayer.MessageFactory.SetWorker(messagefactory.WorkerFunc(DoPOW))
 }
