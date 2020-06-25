7776671e8d85089ce2ed0b55b83290928b3d9f06
Hans Moog <hm@mkjc.net>
2020-06-25 22:58:09 +0200

    Fix: fixed faucet

--- dapps/faucet/packages/faucet.go
+++ dapps/faucet/packages/faucet.go
@@ -8,6 +8,7 @@ import (
 
 	faucetpayload "github.com/iotaledger/goshimmer/dapps/faucet/packages/payload"
 	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/message"
+	"github.com/iotaledger/hive.go/bitmask"
 	"github.com/iotaledger/hive.go/events"
 
 	"github.com/iotaledger/goshimmer/dapps/valuetransfers"
@@ -28,8 +29,11 @@ var (
 // New creates a new faucet using the given seed and tokensPerRequest config.
 func New(seed []byte, tokensPerRequest int64, maxTxBookedAwaitTime time.Duration) *Faucet {
 	return &Faucet{
-		tokensPerRequest:     tokensPerRequest,
-		wallet:               wallet.New(seed),
+		tokensPerRequest: tokensPerRequest,
+		wallet: wallet.New(
+			wallet.Import(wallet.NewSeed(seed), 0, []bitmask.BitMask{}),
+			wallet.ReusableAddress(true),
+		),
 		maxTxBookedAwaitTime: maxTxBookedAwaitTime,
 	}
 }
@@ -132,7 +136,7 @@ func (f *Faucet) collectUTXOsForFunding() (outputIds []transaction.OutputID, rem
 	// get a list of address for inputs
 	for i = 0; total > 0; i++ {
 		addr := f.wallet.Seed().Address(i)
-		valuetransfers.Tangle().OutputsOnAddress(addr).Consume(func(output *tangle.Output) {
+		valuetransfers.Tangle().OutputsOnAddress(addr.Address).Consume(func(output *tangle.Output) {
 			if output.ConsumerCount() > 0 || total == 0 {
 				return
 			}
@@ -161,11 +165,11 @@ func (f *Faucet) nextUnusedAddress() address.Address {
 	var index uint64
 	for index = 0; ; index++ {
 		addr := f.wallet.Seed().Address(index)
-		cachedOutputs := valuetransfers.Tangle().OutputsOnAddress(addr)
+		cachedOutputs := valuetransfers.Tangle().OutputsOnAddress(addr.Address)
 		if len(cachedOutputs) == 0 {
 			// unused address
 			cachedOutputs.Release()
-			return addr
+			return addr.Address
 		}
 		cachedOutputs.Release()
 	}
