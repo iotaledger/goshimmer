3a831669a381ab8314946fba50dd61a76337d2fb
capossele <angelocapossele@gmail.com>
2020-06-26 20:14:54 +0100

    🐛 Fix bug

--- plugins/gracefulshutdown/plugin.go
+++ plugins/gracefulshutdown/plugin.go
@@ -20,54 +20,58 @@ const PluginName = "Graceful Shutdown"
 
 var (
 	// plugin is the plugin instance of the graceful shutdown plugin.
-	plugin       *node.Plugin
-	once         sync.Once
-	log          *logger.Logger
-	gracefulStop chan os.Signal
+	plugin                  *node.Plugin
+	once                    sync.Once
+	log                     *logger.Logger
+	gracefulStop            chan os.Signal
+	waitToKillTimeInSeconds int
 )
+
+func configure(*node.Plugin) {
+	waitToKillTimeInSeconds = config.Node().GetInt(CfgWaitToKillTimeInSeconds)
+
+	log = logger.NewLogger(PluginName)
+	gracefulStop = make(chan os.Signal)
+
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+
+	go func() {
+		<-gracefulStop
+
+		log.Warnf("Received shutdown request - waiting (max %d) to finish processing ...", waitToKillTimeInSeconds)
+
+		go func() {
+			ticker := time.NewTicker(1 * time.Second)
+			defer ticker.Stop()
+
+			start := time.Now()
+			for x := range ticker.C {
+				secondsSinceStart := x.Sub(start).Seconds()
+
+				if secondsSinceStart <= float64(waitToKillTimeInSeconds) {
+					processList := ""
+					runningBackgroundWorkers := daemon.GetRunningBackgroundWorkers()
+					if len(runningBackgroundWorkers) >= 1 {
+						sort.Strings(runningBackgroundWorkers)
+						processList = "(" + strings.Join(runningBackgroundWorkers, ", ") + ") "
+					}
+					log.Warnf("Received shutdown request - waiting (max %d seconds) to finish processing %s...", waitToKillTimeInSeconds-int(secondsSinceStart), processList)
+				} else {
+					log.Error("Background processes did not terminate in time! Forcing shutdown ...")
+					os.Exit(1)
+				}
+			}
+		}()
+
+		daemon.Shutdown()
+	}()
+}
 
 // Plugin gets the plugin instance.
 func Plugin() *node.Plugin {
 	once.Do(func() {
-		WaitToKillTimeInSeconds := config.Node().GetInt(CfgWaitToKillTimeInSeconds)
-		plugin = node.NewPlugin(PluginName, node.Enabled, func(plugin *node.Plugin) {
-			log = logger.NewLogger(PluginName)
-			gracefulStop = make(chan os.Signal)
-
-			signal.Notify(gracefulStop, syscall.SIGTERM)
-			signal.Notify(gracefulStop, syscall.SIGINT)
-
-			go func() {
-				<-gracefulStop
-
-				log.Warnf("Received shutdown request - waiting (max %d) to finish processing ...", WaitToKillTimeInSeconds)
-
-				go func() {
-					ticker := time.NewTicker(1 * time.Second)
-					defer ticker.Stop()
-
-					start := time.Now()
-					for x := range ticker.C {
-						secondsSinceStart := x.Sub(start).Seconds()
-
-						if secondsSinceStart <= float64(WaitToKillTimeInSeconds) {
-							processList := ""
-							runningBackgroundWorkers := daemon.GetRunningBackgroundWorkers()
-							if len(runningBackgroundWorkers) >= 1 {
-								sort.Strings(runningBackgroundWorkers)
-								processList = "(" + strings.Join(runningBackgroundWorkers, ", ") + ") "
-							}
-							log.Warnf("Received shutdown request - waiting (max %d seconds) to finish processing %s...", WaitToKillTimeInSeconds-int(secondsSinceStart), processList)
-						} else {
-							log.Error("Background processes did not terminate in time! Forcing shutdown ...")
-							os.Exit(1)
-						}
-					}
-				}()
-
-				daemon.Shutdown()
-			}()
-		})
+		plugin = node.NewPlugin(PluginName, node.Enabled, configure)
 	})
 	return plugin
 }
