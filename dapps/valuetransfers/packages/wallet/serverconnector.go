6000b415557cd0a72b0cd6348b6634c17071ec3b
Hans Moog <hm@mkjc.net>
2020-06-28 17:31:58 +0200

    Feat: added missing comments

--- dapps/valuetransfers/packages/wallet/serverconnector.go
+++ dapps/valuetransfers/packages/wallet/serverconnector.go
@@ -2,13 +2,17 @@ package wallet
 
 import "github.com/iotaledger/goshimmer/dapps/valuetransfers/packages/transaction"
 
+// ServerConnector is a connector that uses local instance of a node instead of the remote api to implement the required
+// functions for the wallet.
 type ServerConnector struct {
 }
 
+// SendTransaction sends a transaction into the network
 func (s ServerConnector) SendTransaction(tx *transaction.Transaction) {
 	panic("implement me")
 }
 
+// UnspentOutputs returns the unspent outputs on the given addresses.
 func (s ServerConnector) UnspentOutputs(addresses ...Address) map[Address]map[transaction.ID]*Output {
 	panic("implement me")
 }
--- dapps/valuetransfers/packages/wallet/webconnector.go
+++ dapps/valuetransfers/packages/wallet/webconnector.go
@@ -1,3 +1,5 @@
 package wallet
 
+// WebConnector is implement a connector that uses the web API to connect to a node to implement the required functions
+// for the wallet.
 type WebConnector struct{}
