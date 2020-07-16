29651d9a44eadf395dd1123f515a63ea0d8ac0d5
Levente Pap <levente.pap@iota.org>
2020-07-16 10:11:27 +0200

    Restore metrics plugin parameters

--- plugins/metrics/parameters.go
+++ plugins/metrics/parameters.go
@@ -6,15 +6,12 @@ import (
 
 const (
 	// CfgMetricsLocal defines the config flag to enable/disable local metrics.
-	CfgMetricsLocal = "metrics.local.enable"
-	// CfgMetricsLocalDB defines the config flag to enable/disable local database metrics.
-	CfgMetricsLocalDB = "metrics.local.db"
+	CfgMetricsLocal = "metrics.local"
 	// CfgMetricsGlobal defines the config flag to enable/disable global metrics.
-	CfgMetricsGlobal = "metrics.global.enable"
+	CfgMetricsGlobal = "metrics.global"
 )
 
 func init() {
 	flag.Bool(CfgMetricsLocal, true, "include local metrics")
-	flag.Bool(CfgMetricsLocalDB, false, "include local database metrics")
 	flag.Bool(CfgMetricsGlobal, false, "include global metrics")
 }
