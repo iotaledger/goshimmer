2b7af412b4ef281d1871a3bc65caf2fe1ba4e715
Levente Pap <levente.pap@iota.org>
2020-07-20 12:49:59 +0200

    ✨ Introduce tools endpoint for webapi
     - disabled by deafult
     - contains utility api-s, e.g., "tools/pastcone" that given
       a message ID, checks if all messages in the past cone are
       present in DB down to genesis
     - Add PastConeExist() client api method

--- client/tools.go
+++ client/tools.go
@@ -1,59 +1,32 @@
 package client
 
 import (
-	"container/list"
 	"errors"
-	"fmt"
+	"net/http"
+
+	webapi_tools "github.com/iotaledger/goshimmer/plugins/webapi/tools"
 )
 
-// genesisID is the ID of the genesis message
-var genesisID = "1111111111111111111111111111111111111111111111111111111111111111"
+const (
+	routePastCone = "tools/pastcone"
+)
 
 // PastConeExist checks that all of the messages in the the past cone of a message are existing on the node
 // down to the genesis. Returns the number of messages in the past cone as well.
-func (api *GoShimmerAPI) PastConeExist(messageID string) (bool, int, error) {
-	// create a new stack that hold messages to check
-	stack := list.New()
-	stack.PushBack(messageID)
-	// keep track of submitted checks (to not re-add something to the stack that is already in it)
-	// searching in double-linked list is quite expensive, but not in a map
-	submitted := make(map[string]bool)
+func (api *GoShimmerAPI) PastConeExist(base58EncodedMessageID string) (*webapi_tools.PastConeResponse, error) {
+	res := &webapi_tools.PastConeResponse{}
 
-	var checkedMessageCount int
-
-	// process messages, try to request parents until we end up at the genesis
-	for stack.Len() > 0 {
-		checkedMessageCount++
-		// pop the first element from stack
-		currentMsgElement := stack.Front()
-		currentMsgID := currentMsgElement.Value.(string)
-		stack.Remove(currentMsgElement)
-
-		// ask node if it has it
-		response, err := api.FindMessageByID([]string{currentMsgID})
-		if err != nil {
-			return false, checkedMessageCount, errors.New(fmt.Sprintf("error while requesting %s message: %s", currentMsgID, err.Error()))
-		}
-
-		currentMsg := response.Messages[0]
-		// the first element in response.Messages is Messages{}, so its ID is empty if message doesn't exist on node
-		if currentMsg.ID == "" {
-			return false, checkedMessageCount, errors.New(fmt.Sprintf("message %s doesn't exist on node", currentMsgID))
-		}
-
-		// is it the genesis?
-		if currentMsg.BranchID == genesisID && currentMsg.TrunkID == genesisID {
-			continue
-		} else {
-			if !submitted[currentMsg.BranchID] {
-				stack.PushBack(currentMsg.BranchID)
-				submitted[currentMsg.BranchID] = true
-			}
-			if !submitted[currentMsg.TrunkID] {
-				stack.PushBack(currentMsg.TrunkID)
-				submitted[currentMsg.TrunkID] = true
-			}
-		}
+	if err := api.do(
+		http.MethodGet,
+		routePastCone,
+		&webapi_tools.PastConeRequest{ID: base58EncodedMessageID},
+		res,
+	); err != nil {
+		return nil, err
 	}
-	return true, checkedMessageCount, nil
+
+	if res.Error != "" {
+		return res, errors.New(res.Error)
+	}
+	return res, nil
 }
