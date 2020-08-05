848eea4324ef5240c0d7a1575a755f13c3450e6f
Levente Pap <levente.pap@iota.org>
2020-08-05 21:51:56 +0200

    Fix crashing local dashboard with explorer (#682)

--- plugins/dashboard/frontend/src/app/components/SyncBeaconPayload.tsx
+++ plugins/dashboard/frontend/src/app/components/SyncBeaconPayload.tsx
@@ -3,6 +3,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {inject, observer} from "mobx-react";
 import {ExplorerStore} from "app/stores/ExplorerStore";
+import {SyncBeaconPayload as beaconPayload}  from "app/misc/Payload";
 import ListGroup from "react-bootstrap/ListGroup";
 import * as dateformat from 'dateformat';
 
@@ -16,6 +17,12 @@ export class SyncBeaconPayload extends React.Component<Props, any> {
 
     render() {
         let {payload} = this.props.explorerStore;
+        // TODO: this is just a quick fix. other payloads not having `sent_time` field causing a crash
+        // for example, when you open a data message and payload is not a sync beacon anymore, but react
+        // tries to rerender this component...
+        if (!(payload instanceof beaconPayload)) {
+            return []
+        }
         return (
             payload &&
             <React.Fragment>
