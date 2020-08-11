803c9f7d3eb33c8302c54c1a8d7ae282b2a2c271
capossele <angelocapossele@gmail.com>
2020-08-11 15:44:55 +0100

    ♻️ Refactor dRNG API

--- plugins/webapi/drng/info/committee/handler.go
+++ plugins/webapi/drng/info/committee/handler.go
@@ -1,29 +1,34 @@
 package committee
 
 import (
+	"encoding/hex"
 	"net/http"
 
 	"github.com/iotaledger/goshimmer/plugins/drng"
-	"github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/labstack/echo"
+	"github.com/mr-tron/base58"
 )
 
 // Handler returns the current DRNG committee used.
 func Handler(c echo.Context) error {
 	committee := drng.Instance().State.Committee()
+	identities := []string{}
+	for _, pk := range committee.Identities {
+		identities = append(identities, base58.Encode(pk[:]))
+	}
 	return c.JSON(http.StatusOK, Response{
 		InstanceID:    committee.InstanceID,
 		Threshold:     committee.Threshold,
-		Identities:    committee.Identities,
-		DistributedPK: committee.DistributedPK,
+		Identities:    identities,
+		DistributedPK: hex.EncodeToString(committee.DistributedPK),
 	})
 }
 
 // Response is the HTTP message containing the DRNG committee.
 type Response struct {
-	InstanceID    uint32              `json:"instanceID,omitempty"`
-	Threshold     uint8               `json:"threshold,omitempty"`
-	Identities    []ed25519.PublicKey `json:"identities,omitempty"`
-	DistributedPK []byte              `json:"distributedPK,omitempty"`
-	Error         string              `json:"error,omitempty"`
+	InstanceID    uint32   `json:"instanceID,omitempty"`
+	Threshold     uint8    `json:"threshold,omitempty"`
+	Identities    []string `json:"identities,omitempty"`
+	DistributedPK string   `json:"distributedPK,omitempty"`
+	Error         string   `json:"error,omitempty"`
 }
