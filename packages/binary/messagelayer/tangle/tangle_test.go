5ce78f9d428413c9e7f97390c111367216a1927c
Hans Moog <hm@mkjc.net>
2020-08-12 21:03:29 +0200

    Feat: added unit test for race condition

--- packages/binary/messagelayer/tangle/tangle_test.go
+++ packages/binary/messagelayer/tangle/tangle_test.go
@@ -2,14 +2,20 @@ package tangle
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/message"
+	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/messagefactory"
 	"github.com/iotaledger/goshimmer/packages/binary/messagelayer/payload"
 	"github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/iotaledger/hive.go/events"
+	"github.com/iotaledger/hive.go/identity"
 	"github.com/iotaledger/hive.go/kvstore/mapdb"
+	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func BenchmarkTangle_AttachMessage(b *testing.B) {
@@ -81,6 +87,90 @@ func TestTangle_AttachMessage(t *testing.T) {
 	messageTangle.AttachMessage(newMessageOne)
 
 	messageTangle.Shutdown()
+}
+
+func TestTangle_MissingMessages(t *testing.T) {
+	// test parameters
+	messageCount := 10000
+
+	// variables required for the test
+	previousMessageID := message.EmptyID
+	missingMessagesCounter := int32(0)
+	wg := sync.WaitGroup{}
+
+	// setup the message factory
+	msgFactory := messagefactory.New(
+		mapdb.NewMapDB(),
+		[]byte("sequenceKey"),
+		identity.GenerateLocalIdentity(),
+		messagefactory.TipSelectorFunc(func() (message.ID, message.ID) { return previousMessageID, previousMessageID }),
+	)
+	defer msgFactory.Shutdown()
+
+	// create a helper function that creates the messages
+	createNewMessage := func() *message.Message {
+		// issue the payload
+		msg, err := msgFactory.IssuePayload(payload.NewData([]byte("0")))
+		require.NoError(t, err)
+
+		// link the new message to the previous one
+		previousMessageID = msg.ID()
+
+		// return the constructed message
+		return msg
+	}
+
+	// create the tangle
+	tangle := New(mapdb.NewMapDB())
+	if err := tangle.Prune(); err != nil {
+		t.Error(err)
+
+		return
+	}
+
+	// generate the messages we want to solidify
+	preGeneratedMessages := make([]*message.Message, messageCount)
+	for i := 0; i < messageCount; i++ {
+		preGeneratedMessages[i] = createNewMessage()
+	}
+
+	// increase the counter when a missing message was detected
+	tangle.Events.MessageMissing.Attach(events.NewClosure(func(messageId message.ID) {
+		atomic.AddInt32(&missingMessagesCounter, 1)
+	}))
+
+	// decrease the counter when a missing message was received
+	tangle.Events.MissingMessageReceived.Attach(events.NewClosure(func(cachedMessage *message.CachedMessage, cachedMessageMetadata *CachedMessageMetadata) {
+		cachedMessageMetadata.Release()
+		cachedMessage.Consume(func(msg *message.Message) {
+			atomic.AddInt32(&missingMessagesCounter, -1)
+		})
+	}))
+
+	// mark the WaitGroup as done if all messages are solid
+	tangle.Events.MessageSolid.Attach(events.NewClosure(func(cachedMessage *message.CachedMessage, cachedMessageMetadata *CachedMessageMetadata) {
+		cachedMessageMetadata.Release()
+		cachedMessage.Consume(func(msg *message.Message) {
+			if msg.ID() == preGeneratedMessages[messageCount-1].ID() {
+				wg.Done()
+			}
+		})
+	}))
+
+	// issue transactions in reverse order
+	wg.Add(1)
+	for i := len(preGeneratedMessages) - 1; i >= 0; i-- {
+		go tangle.AttachMessage(preGeneratedMessages[i])
+	}
+
+	// wait for all transactions to become solid
+	wg.Wait()
+
+	// make sure that all MessageMissing events also had a corresponding MissingMessageReceived event
+	assert.Equal(t, missingMessagesCounter, int32(0))
+
+	// shutdown the tangle
+	tangle.Shutdown()
 }
 
 func newTestMessage(payloadString string) *message.Message {
