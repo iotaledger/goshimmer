1838c9b26f26a9a2bfa65f5b36638293cf0fcb31
Hans Moog <hm@mkjc.net>
2020-08-24 09:51:24 +0200

    Feat: cleaned up message requester

--- packages/binary/messagelayer/messagerequester/messagerequester.go
+++ packages/binary/messagelayer/messagerequester/messagerequester.go
@@ -8,13 +8,13 @@ import (
 	"github.com/iotaledger/hive.go/events"
 )
 
-const messageExistCheckThreshold = 10
+// the maximum amount of requests before we abort
+const maxRequestThreshold = 500
 
 // MessageRequester takes care of requesting messages.
 type MessageRequester struct {
 	scheduledRequests map[message.ID]*time.Timer
 	options           *Options
-	messageExistsFunc MessageExistsFunc
 	Events            Events
 
 	scheduledRequestsMutex sync.RWMutex
@@ -23,16 +23,11 @@ type MessageRequester struct {
 // MessageExistsFunc is a function that tells if a message exists.
 type MessageExistsFunc func(messageId message.ID) bool
 
-func createReRequest(requester *MessageRequester, msgID message.ID, count int) func() {
-	return func() { requester.reRequest(msgID, count) }
-}
-
 // New creates a new message requester.
-func New(messageExists MessageExistsFunc, missingMessages []message.ID, optionalOptions ...Option) *MessageRequester {
+func New(missingMessages []message.ID, optionalOptions ...Option) *MessageRequester {
 	requester := &MessageRequester{
 		scheduledRequests: make(map[message.ID]*time.Timer),
 		options:           newOptions(optionalOptions),
-		messageExistsFunc: messageExists,
 		Events: Events{
 			SendRequest: events.NewEvent(func(handler interface{}, params ...interface{}) {
 				handler.(func(message.ID))(params[0].(message.ID))
@@ -48,7 +43,7 @@ func New(messageExists MessageExistsFunc, missingMessages []message.ID, optional
 	defer requester.scheduledRequestsMutex.Unlock()
 
 	for _, id := range missingMessages {
-		requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, createReRequest(requester, id, 0))
+		requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, requester.createReRequest(id, 0))
 	}
 
 	return requester
@@ -65,7 +60,7 @@ func (requester *MessageRequester) StartRequest(id message.ID) {
 	}
 
 	// schedule the next request and trigger the event
-	requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, createReRequest(requester, id, 0))
+	requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, requester.createReRequest(id, 0))
 	requester.scheduledRequestsMutex.Unlock()
 	requester.Events.SendRequest.Trigger(id)
 }
@@ -84,22 +79,23 @@ func (requester *MessageRequester) StopRequest(id message.ID) {
 func (requester *MessageRequester) reRequest(id message.ID, count int) {
 	requester.Events.SendRequest.Trigger(id)
 
-	count++
-	stopRequest := count > messageExistCheckThreshold && requester.messageExistsFunc(id)
-
 	// as we schedule a request at most once per id we do not need to make the trigger and the re-schedule atomic
 	requester.scheduledRequestsMutex.Lock()
 	defer requester.scheduledRequestsMutex.Unlock()
 
 	// reschedule, if the request has not been stopped in the meantime
 	if _, exists := requester.scheduledRequests[id]; exists {
-		if stopRequest {
-			// if found message tangle: stop request and delete from missingMessageStorage (via event)
+		// increase the request counter
+		count++
+
+		// if we have requested too often => stop the requests
+		if count > maxRequestThreshold {
 			delete(requester.scheduledRequests, id)
-			requester.Events.MissingMessageAppeared.Trigger(id)
+
 			return
 		}
-		requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, createReRequest(requester, id, count))
+
+		requester.scheduledRequests[id] = time.AfterFunc(requester.options.retryInterval, requester.createReRequest(id, count))
 		return
 	}
 }
@@ -109,4 +105,8 @@ func (requester *MessageRequester) RequestQueueSize() int {
 	requester.scheduledRequestsMutex.RLock()
 	defer requester.scheduledRequestsMutex.RUnlock()
 	return len(requester.scheduledRequests)
+}
+
+func (requester *MessageRequester) createReRequest(msgID message.ID, count int) func() {
+	return func() { requester.reRequest(msgID, count) }
 }
