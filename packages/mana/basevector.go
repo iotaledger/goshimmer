30172431bfd38c60febee00fa40749f4a3b439b7
Acha Bill <achabill12@gmail.com>
2020-09-07 03:34:24 +0100

    attach to transactionConfirmed event

--- packages/mana/basevector.go
+++ packages/mana/basevector.go
@@ -4,14 +4,24 @@ import (
 	"github.com/iotaledger/hive.go/identity"
 )
 
+// BaseManaVector represents a base mana vector
 type BaseManaVector struct {
 	vector     map[identity.ID]*BaseMana
 	vectorType Type
 }
 
+// NewBaseManaVector creates and returns a new base mana vector for the specified type
+func NewBaseManaVector(vectorType Type) *BaseManaVector {
+	return &BaseManaVector{
+		vector:     make(map[identity.ID]*BaseMana),
+		vectorType: vectorType,
+	}
+}
+
 // TODO: update
 // TODO: bookmana, refactor methods pass mana type
 
-func (bmv *BaseManaVector) bookMana(txInfo *TxInfo) {
+// BookMana books mana for a certain node
+func (bmv *BaseManaVector) BookMana(txInfo *TxInfo) {
 
 }
