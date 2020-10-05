f6deefbb49b26f0666596c956696e54b56af7dd9
Angelo Capossele <angelocapossele@gmail.com>
2020-10-05 14:08:56 +0100

    Update packages/database/prefix.go
    Co-authored-by: Ching-Hua (Vivian) Lin <jkrvivian@gmail.com>

--- packages/database/prefix.go
+++ packages/database/prefix.go
@@ -5,7 +5,7 @@ const (
 	PrefixAutoPeering byte = iota
 	// PrefixHealth defines the prefix of the health db.
 	PrefixHealth
-	// PrefixMessageLayer defines the storage prefix for the message layer
+	// PrefixMessageLayer defines the storage prefix for the message layer.
 	PrefixMessageLayer
 	// PrefixValueTransfers defines the storage prefix for value transfer.
 	PrefixValueTransfers
