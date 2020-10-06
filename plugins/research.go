99fa5bc2af602faa74bf14f11f59cc657cd163c2
Jonas Theis <mail@jonastheis.de>
2020-10-06 09:27:09 +0200

    Move networkdelay to plugins folder (#759)
    * Move to plugins folder
    
    * Rename dapp.go to plugin.go

--- plugins/research.go
+++ plugins/research.go
@@ -1,10 +1,10 @@
 package plugins
 
 import (
-	"github.com/iotaledger/goshimmer/dapps/networkdelay"
 	analysisclient "github.com/iotaledger/goshimmer/plugins/analysis/client"
 	analysisdashboard "github.com/iotaledger/goshimmer/plugins/analysis/dashboard"
 	analysisserver "github.com/iotaledger/goshimmer/plugins/analysis/server"
+	"github.com/iotaledger/goshimmer/plugins/networkdelay"
 	"github.com/iotaledger/goshimmer/plugins/prometheus"
 	"github.com/iotaledger/goshimmer/plugins/remotelog"
 	"github.com/iotaledger/hive.go/node"
