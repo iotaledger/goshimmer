c4de9c9ec151deb3ce64ac44a3a072792be8ca33
Hans Moog <hm@mkjc.net>
2020-10-16 10:53:26 +0200

    Feat: updated documentation

--- packages/cerrors/cerrors.go
+++ packages/cerrors/cerrors.go
@@ -1,4 +1,10 @@
-// Package cerrors provides sentinel error values for "common errors" that are likely to appear in multiple packages.
+// Package cerrors provides sentinel error values for common errors that are likely to appear in multiple packages.
+//
+// Since wrapping errors using the new error handling API of Go creates anonymous errors it is not possible to wrap an
+// existing sentinel error with another sentinel error without loosing the stack trace.
+//
+// Using shared sentinel errors enables them to be annotated and used across packages borders without the caller having
+// to distinguish between similar errors types of different packages that would otherwise be identical.
 package cerrors
 
 import "errors"
