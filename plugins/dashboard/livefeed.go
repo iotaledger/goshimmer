12162b44fd666272ff310aae64675bfd3a08fa25
Angelo Capossele <angelocapossele@gmail.com>
2020-12-05 16:33:47 +0000

    Remove livefeed rate limiter (#856)
    * ⚡️ Remove livefeed rate limiter
    
    * 🐛 Fix compile issue

--- plugins/dashboard/livefeed.go
+++ plugins/dashboard/livefeed.go
@@ -1,8 +1,6 @@
 package dashboard
 
 import (
-	"time"
-
 	"github.com/iotaledger/goshimmer/packages/shutdown"
 	"github.com/iotaledger/goshimmer/packages/tangle"
 	"github.com/iotaledger/goshimmer/plugins/messagelayer"
@@ -26,17 +24,10 @@ func configureLiveFeed() {
 }
 
 func runLiveFeed() {
-	newMsgRateLimiter := time.NewTicker(time.Second / 10)
 	notifyNewMsg := events.NewClosure(func(cachedMsgEvent *tangle.CachedMessageEvent) {
 		cachedMsgEvent.MessageMetadata.Release()
-
-		select {
-		case <-newMsgRateLimiter.C:
-			_, ok := liveFeedWorkerPool.TrySubmit(cachedMsgEvent.Message)
-			if !ok {
-				cachedMsgEvent.Message.Release()
-			}
-		default:
+		_, ok := liveFeedWorkerPool.TrySubmit(cachedMsgEvent.Message)
+		if !ok {
 			cachedMsgEvent.Message.Release()
 		}
 	})
@@ -47,7 +38,6 @@ func runLiveFeed() {
 		<-shutdownSignal
 		log.Info("Stopping Dashboard[MsgUpdater] ...")
 		messagelayer.Tangle().Events.MessageAttached.Detach(notifyNewMsg)
-		newMsgRateLimiter.Stop()
 		liveFeedWorkerPool.Stop()
 		log.Info("Stopping Dashboard[MsgUpdater] ... done")
 	}, shutdown.PriorityDashboard); err != nil {
