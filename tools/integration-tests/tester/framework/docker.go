eb38b659ce44fc2984f373534892238c0768d258
capossele <angelocapossele@gmail.com>
2020-12-10 10:13:27 +0000

    ⬆️ Update drng image

--- tools/integration-tests/tester/framework/docker.go
+++ tools/integration-tests/tester/framework/docker.go
@@ -148,7 +148,7 @@ func (d *DockerContainer) CreateDrandMember(name string, goShimmerAPI string, le
 	}
 	env = append(env, "GOSHIMMER=http://"+goShimmerAPI)
 	containerConfig := &container.Config{
-		Image: "angelocapossele/drand:1.1.3",
+		Image: "angelocapossele/drng:payloadfix",
 		ExposedPorts: nat.PortSet{
 			nat.Port("8000/tcp"): {},
 		},
