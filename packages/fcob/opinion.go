0878dfd78df83e8b1588b3150d547a9b3fd184ae
capossele <angelocapossele@gmail.com>
2021-02-03 08:30:17 +0000

    🚧

--- packages/fcob/opinion.go
+++ packages/fcob/opinion.go
@@ -21,36 +21,59 @@ const (
 	// One implies that voting is required.
 	One
 
-	// Two implies that we have finalized our opinion but we can still reply to eventual queries.
+	// Two implies that we have locally finalized our opinion but we can still reply to eventual queries.
 	Two
 
-	// Three implies that we have finalized our opinion and we do not reply to eventual queries.
+	// Three implies that we have locally finalized our opinion and we do not reply to eventual queries.
 	Three
 )
 
 // region Opinion //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Opinion defines the FCoB opinion about a transaction.
 type Opinion struct {
-	transactionID    ledgerstate.TransactionID
-	Timestamp        time.Time
-	Liked            bool
+	transactionID ledgerstate.TransactionID
+
+	// Timestamp is the arrrival/solidification time.
+	Timestamp time.Time
+
+	// Liked is the opinion value.
+	Liked bool
+
+	// LevelOfKnowledge is the degree of certainty of the associated opinion.
 	LevelOfKnowledge LevelOfKnowledge
 
 	objectstorage.StorableObjectFlags
 }
 
+// Bytes marshals the Opinion into a sequence of bytes.
 func (o *Opinion) Bytes() []byte {
 	return byteutils.ConcatBytes(o.ObjectStorageKey(), o.ObjectStorageValue())
 }
 
+// String returns a human readable version of the Opinion.
+func (o *Opinion) String() string {
+	return stringify.Struct("Opinion",
+		stringify.StructField("transactionID", o.transactionID),
+		stringify.StructField("timestamp", o.Timestamp),
+		stringify.StructField("liked", o.Liked),
+		stringify.StructField("LevelOfKnowledge", o.LevelOfKnowledge),
+	)
+}
+
+// Update is disabled and panics if it ever gets called - it is required to match the StorableObject interface.
 func (o *Opinion) Update(other objectstorage.StorableObject) {
 	panic("updates disabled")
 }
 
+// ObjectStorageKey returns the key that is used to store the object in the database. It is required to match the
+// StorableObject interface.
 func (o *Opinion) ObjectStorageKey() []byte {
 	return o.transactionID.Bytes()
 }
 
+// ObjectStorageValue marshals the Opinion into a sequence of bytes. The ID is not serialized here as it is
+// only used as a key in the ObjectStorage.
 func (o *Opinion) ObjectStorageValue() []byte {
 	return marshalutil.New().
 		WriteTime(o.Timestamp).
@@ -111,8 +134,10 @@ func (c *CachedOpinion) String() string {
 
 // region ConflictSet //////////////////////////////////////////////////////////////////////////////////////////////////
 
+// ConflictSet is a list of Opinion.
 type ConflictSet []*Opinion
 
+// hasDecidedLike returns true if the conflict set contains at least one LIKE with LoK >= 2.
 func (c ConflictSet) hasDecidedLike() bool {
 	for _, opinion := range c {
 		if opinion.Liked && opinion.LevelOfKnowledge > One {
@@ -122,6 +147,7 @@ func (c ConflictSet) hasDecidedLike() bool {
 	return false
 }
 
+// hasAllDecidedDislike returns true if the conflict set contains only DISLIKE with LoK >= 2.
 func (c ConflictSet) hasAllDecidedDislike() bool {
 	for _, opinion := range c {
 		if opinion.Liked || opinion.LevelOfKnowledge < Two {
@@ -131,11 +157,24 @@ func (c ConflictSet) hasAllDecidedDislike() bool {
 	return true
 }
 
+// // anchor returns the most recent opinion with LoK <= 1.
+// func (c ConflictSet) anchor() (opinion *Opinion) {
+// 	mostRecentTimestamp := time.Time{}
+// 	for _, o := range c {
+// 		if o.LevelOfKnowledge <= One && o.Timestamp.After(mostRecentTimestamp) {
+// 			mostRecentTimestamp = o.Timestamp
+// 			opinion = o
+// 		}
+// 	}
+// 	return opinion
+// }
+
+// anchor returns the oldest opinion with LoK <= 1.
 func (c ConflictSet) anchor() (opinion *Opinion) {
-	mostRecentTimestamp := time.Time{}
+	oldestTimestamp := time.Unix(1<<63-62135596801, 999999999)
 	for _, o := range c {
-		if o.LevelOfKnowledge <= One && o.Timestamp.After(mostRecentTimestamp) {
-			mostRecentTimestamp = o.Timestamp
+		if o.LevelOfKnowledge <= One && o.Timestamp.Before(oldestTimestamp) {
+			oldestTimestamp = o.Timestamp
 			opinion = o
 		}
 	}
