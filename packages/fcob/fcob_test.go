c3b513c92ef405738ac5cb407bc567ef0192506e
capossele <angelocapossele@gmail.com>
2021-02-03 08:09:39 +0000

    ✅ Add deriveOpinion test cases

--- packages/fcob/fcob_test.go
+++ packages/fcob/fcob_test.go
@@ -7,18 +7,94 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFCoB(t *testing.T) {
+func TestDeriveOpinion(t *testing.T) {
 	now := time.Now()
-	conflictSet := ConflictSet{&Opinion{
-		Timestamp:        now,
-		Liked:            false,
-		LevelOfKnowledge: Pending,
-	}}
 
-	opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
-	assert.Equal(t, &Opinion{
-		Timestamp:        now.Add(1 * time.Second),
-		Liked:            false,
-		LevelOfKnowledge: One,
-	}, opinion)
+	// 1 pending -> Dislike, One
+	{
+		conflictSet := ConflictSet{&Opinion{
+			Timestamp:        now,
+			Liked:            false,
+			LevelOfKnowledge: Pending,
+		}}
+
+		opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
+		assert.Equal(t, &Opinion{
+			Timestamp:        now.Add(1 * time.Second),
+			Liked:            false,
+			LevelOfKnowledge: One,
+		}, opinion)
+	}
+
+	// 1 Like, One -> Dislike, One
+	{
+		{
+			conflictSet := ConflictSet{&Opinion{
+				Timestamp:        now,
+				Liked:            true,
+				LevelOfKnowledge: One,
+			}}
+
+			opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
+			assert.Equal(t, &Opinion{
+				Timestamp:        now.Add(1 * time.Second),
+				Liked:            false,
+				LevelOfKnowledge: One,
+			}, opinion)
+		}
+	}
+
+	// 1 Like, Two -> Dislike, Two
+	{
+		{
+			conflictSet := ConflictSet{&Opinion{
+				Timestamp:        now,
+				Liked:            true,
+				LevelOfKnowledge: Two,
+			}}
+
+			opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
+			assert.Equal(t, &Opinion{
+				Timestamp:        now.Add(1 * time.Second),
+				Liked:            false,
+				LevelOfKnowledge: Two,
+			}, opinion)
+		}
+	}
+
+	// 1 Dislike, Two -> Dislike, Pending
+	{
+		{
+			conflictSet := ConflictSet{&Opinion{
+				Timestamp:        now,
+				Liked:            false,
+				LevelOfKnowledge: Two,
+			}}
+
+			opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
+			assert.Equal(t, &Opinion{
+				Timestamp:        now.Add(1 * time.Second),
+				Liked:            false,
+				LevelOfKnowledge: Pending,
+			}, opinion)
+		}
+	}
+
+	// 1 Dislike, One -> Dislike, One
+	{
+		{
+			conflictSet := ConflictSet{&Opinion{
+				Timestamp:        now,
+				Liked:            false,
+				LevelOfKnowledge: One,
+			}}
+
+			opinion := deriveOpinion(now.Add(1*time.Second), conflictSet)
+			assert.Equal(t, &Opinion{
+				Timestamp:        now.Add(1 * time.Second),
+				Liked:            false,
+				LevelOfKnowledge: One,
+			}, opinion)
+		}
+	}
 }
