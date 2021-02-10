5ded28d62554766dc46c2e2817f7f0673f17d336
capossele <angelocapossele@gmail.com>
2021-02-10 16:48:51 +0000

    ✨ Add LoadSnapshot

--- packages/tangle/ledgerstate.go
+++ packages/tangle/ledgerstate.go
@@ -2,6 +2,7 @@ package tangle
 
 import (
 	"github.com/iotaledger/goshimmer/packages/ledgerstate"
+	"github.com/iotaledger/hive.go/types"
 	"golang.org/x/xerrors"
 )
 
@@ -17,8 +18,11 @@ type LedgerState struct {
 
 // NewLedgerState is the constructor of the LedgerState component.
 func NewLedgerState(tangle *Tangle) (ledgerState *LedgerState) {
+	branchDAG := ledgerstate.NewBranchDAG(tangle.Options.Store)
 	return &LedgerState{
-		tangle: tangle,
+		tangle:    tangle,
+		branchDAG: branchDAG,
+		utxoDAG:   ledgerstate.NewUTXODAG(tangle.Options.Store, branchDAG),
 	}
 }
 
@@ -80,6 +84,52 @@ func (l *LedgerState) BookTransaction(transaction *ledgerstate.Transaction, mess
 	}
 
 	return
+}
+
+// ConflictSet returns the list of transactionIDs conflicting with the given transactionID.
+func (l *LedgerState) ConflictSet(transactionID ledgerstate.TransactionID) (conflictSet ledgerstate.TransactionIDs) {
+	conflictIDs := make(ledgerstate.ConflictIDs)
+	conflictSet = make(ledgerstate.TransactionIDs)
+
+	l.branchDAG.Branch(ledgerstate.NewBranchID(transactionID)).Consume(func(branch ledgerstate.Branch) {
+		conflictIDs = branch.(*ledgerstate.ConflictBranch).Conflicts()
+	})
+
+	for conflictID := range conflictIDs {
+		l.branchDAG.ConflictMembers(conflictID).Consume(func(conflictMember *ledgerstate.ConflictMember) {
+			conflictSet[ledgerstate.TransactionID(conflictMember.BranchID())] = types.Void
+		})
+	}
+
+	return
+}
+
+// TransactionInclusionState returns the InclusionState of the Transaction with the given TransactionID which can either be
+// Pending, Confirmed or Rejected.
+func (l *LedgerState) TransactionInclusionState(transactionID ledgerstate.TransactionID) (ledgerstate.InclusionState, error) {
+	return l.utxoDAG.InclusionState(transactionID)
+}
+
+// BranchInclusionState returns the InclusionState of the Branch with the given BranchID which can either be
+// Pending, Confirmed or Rejected.
+func (l *LedgerState) BranchInclusionState(branchID ledgerstate.BranchID) (inclusionState ledgerstate.InclusionState) {
+	l.branchDAG.Branch(branchID).Consume(func(branch ledgerstate.Branch) {
+		inclusionState = branch.InclusionState()
+	})
+	return
+}
+
+// BranchID returns the branchID of the given transactionID.
+func (l *LedgerState) BranchID(transactionID ledgerstate.TransactionID) (branchID ledgerstate.BranchID) {
+	l.utxoDAG.TransactionMetadata(transactionID).Consume(func(transactionMetadata *ledgerstate.TransactionMetadata) {
+		branchID = transactionMetadata.BranchID()
+	})
+	return
+}
+
+// LoadSnapshot creates a set of outputs in the UTXO-DAG, that are forming the genesis for future transactions.
+func (l *LedgerState) LoadSnapshot(snapshot map[ledgerstate.TransactionID]map[ledgerstate.Address]*ledgerstate.ColoredBalances) {
+	l.utxoDAG.LoadSnapshot(snapshot)
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
