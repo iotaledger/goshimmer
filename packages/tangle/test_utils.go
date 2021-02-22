bb259357e3bb223aa8d705873420736927bed7c3
Hans Moog <hm@mkjc.net>
2021-02-22 22:37:50 +0100

    Feat: fixed unit test to use booker

--- packages/tangle/test_utils.go
+++ packages/tangle/test_utils.go
@@ -100,13 +100,10 @@ func addressFromInput(input ledgerstate.Input, outputsByID ledgerstate.OutputsBy
 }
 
 func messageBranchID(tangle *Tangle, messageID MessageID) (branchID ledgerstate.BranchID, err error) {
-	if !tangle.Storage.MessageMetadata(messageID).Consume(func(messageMetadata *MessageMetadata) {
-		branchID = messageMetadata.BranchID()
-		// fmt.Println(messageID)
-		// fmt.Println(messageMetadata.StructureDetails())
-	}) {
+	if branchID = tangle.Booker.branchIDOfMessage(messageID); branchID == ledgerstate.UndefinedBranchID {
 		return branchID, fmt.Errorf("missing message metadata")
 	}
+
 	return
 }
 
