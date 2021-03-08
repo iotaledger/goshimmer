00a53e4acee5af648f321ade3b98863f703ef724
Hans Moog <hm@mkjc.net>
2021-03-08 13:42:41 +0100

    Feat: added doc comments to parameters

--- plugins/messagelayer/parameters.go
+++ plugins/messagelayer/parameters.go
@@ -4,10 +4,16 @@ import "github.com/iotaledger/hive.go/configuration"
 
 // Parameters contains the configuration parameters used by the message layer.
 var Parameters = struct {
+	// TangleWidth can be used to specify the number of tips the Tangle tries to maintain.
 	TangleWidth int `default:"0" usage:"the width of the Tangle"`
-	Snapshot    struct {
+
+	// Snapshot contains snapshots related configuration parameters.
+	Snapshot struct {
+		// File is the path to the snapshot file.
 		File string `default:"./snapshot.bin" usage:"the path to the snapshot file"`
 	}
+
+	// FCOB contains parameters related to the fast consensus of barcelona.
 	FCOB struct {
 		AverageNetworkDelay int `default:"5" usage:"the avg. network delay to use for FCoB rules"`
 	}
@@ -19,9 +25,13 @@ var FPCParameters = struct {
 	BindAddress string `default:"0.0.0.0:10895" usage:"the bind address on which the FPC vote server binds to"`
 
 	// Listen defines if the FPC service should listen.
-	Listen          bool  `default:"true" usage:"if the FPC service should listen"`
-	RoundInterval   int64 `default:"10" usage:"FPC round interval [s]"`
-	QuerySampleSize int   `default:"21" usage:"Size of the voting quorum (k)"`
+	Listen bool `default:"true" usage:"if the FPC service should listen"`
+
+	// RoundInterval defines how long a round lasts (in seconds).
+	RoundInterval int64 `default:"10" usage:"FPC round interval [s]"`
+
+	// QuerySampleSize defines how many nodes will be queried each round.
+	QuerySampleSize int `default:"21" usage:"Size of the voting quorum (k)"`
 }{}
 
 // StatementParameters contains the configuration parameters used by the FPC statements in the tangle.
