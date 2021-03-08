34fa613d855960203d9826459cd2be1988a02c1a
Hans Moog <hm@mkjc.net>
2021-03-08 11:25:16 +0100

    Feat: started adding comments back

--- plugins/messagelayer/parameters.go
+++ plugins/messagelayer/parameters.go
@@ -15,19 +15,31 @@ var Parameters = struct {
 
 // FPCParameters contains the configuration parameters used by the FPC consensus.
 var FPCParameters = struct {
-	BindAddress     string `default:"0.0.0.0:10895" usage:"the bind address on which the FPC vote server binds to"`
-	Listen          bool   `default:"true" usage:"if the FPC service should listen"`
-	RoundInterval   int64  `default:"10" usage:"FPC round interval [s]"`
-	QuerySampleSize int    `default:"21" usage:"Size of the voting quorum (k)"`
+	// BindAddress defines on which address the FPC service should listen.
+	BindAddress string `default:"0.0.0.0:10895" usage:"the bind address on which the FPC vote server binds to"`
+
+	// Listen defines if the FPC service should listen.
+	Listen          bool  `default:"true" usage:"if the FPC service should listen"`
+	RoundInterval   int64 `default:"10" usage:"FPC round interval [s]"`
+	QuerySampleSize int   `default:"21" usage:"Size of the voting quorum (k)"`
 }{}
 
 // StatementParameters contains the configuration parameters used by the FPC statements in the tangle.
 var StatementParameters = struct {
-	WaitForStatement int     `default:"5" usage:"the time in seconds for which the node wait for receiving the new statement"`
-	WriteStatement   bool    `default:"false" usage:"if the node should make statements"`
-	ManaThreshold    float64 `default:"1" usage:"Mana threshold to accept/write a statement"`
-	CleanInterval    int     `default:"5" usage:"the time in minutes after which the node cleans the statement registry"`
-	DeleteAfter      int     `default:"5" usage:"the time in minutes after which older statements are deleted from the registry"`
+	// WaitForStatement is the time in seconds for which the node wait for receiving the new statement.
+	WaitForStatement int `default:"5" usage:"the time in seconds for which the node wait for receiving the new statement"`
+
+	// WriteStatement defines if the node should write statements.
+	WriteStatement bool `default:"false" usage:"if the node should make statements"`
+
+	// ManaThreshold defines the Mana threshold to accept/write a statement.
+	ManaThreshold float64 `default:"1" usage:"Mana threshold to accept/write a statement"`
+
+	// CleanInterval defines the time interval [in minutes] for cleaning the statement registry.
+	CleanInterval int `default:"5" usage:"the time in minutes after which the node cleans the statement registry"`
+
+	// DeleteAfter defines the time [in minutes] after which older statements are deleted from the registry.
+	DeleteAfter int `default:"5" usage:"the time in minutes after which older statements are deleted from the registry"`
 }{}
 
 func init() {
