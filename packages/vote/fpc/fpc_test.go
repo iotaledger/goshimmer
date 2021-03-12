81ac90cc325c89ddeb9aaf8ca623043f2eb2f231
capossele <angelocapossele@gmail.com>
2021-03-12 17:33:20 +0000

    Fix unit tests

--- packages/vote/fpc/fpc_test.go
+++ packages/vote/fpc/fpc_test.go
@@ -96,6 +96,8 @@ func TestFPCFinalizedEvent(t *testing.T) {
 	paras.FinalizationThreshold = 2
 	paras.CoolingOffPeriod = 2
 	paras.QuerySampleSize = 1
+	paras.MinOpinionsReceived = 1
+
 	voter := fpc.New(opinionGiverFunc, paras)
 	var finalizedOpinion *opinion.Opinion
 	voter.Events().Finalized.Attach(events.NewClosure(func(ev *vote.OpinionEvent) {
@@ -124,6 +126,7 @@ func TestFPCFailedEvent(t *testing.T) {
 
 	paras := fpc.DefaultParameters()
 	paras.QuerySampleSize = 1
+	paras.MinOpinionsReceived = 1
 	paras.MaxRoundsPerVoteContext = 3
 	paras.CoolingOffPeriod = 0
 	// since the finalization threshold is over max rounds it will
