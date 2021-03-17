690d55a1af31f001dad6b23ededfc8e8b71d8874
apenzk <andreas.penzkofer@iota.org>
2021-03-17 09:59:38 +0100

    Liked -> ProportionLiked where applicable

--- plugins/analysis/dashboard/fpc_livefeed_test.go
+++ plugins/analysis/dashboard/fpc_livefeed_test.go
@@ -25,10 +25,10 @@ func TestCreateFPCUpdate(t *testing.T) {
 			RandUsed: 0.5,
 			ActiveVoteContexts: map[string]*vote.Context{
 				"one": {
-					ID:       "one",
-					Liked:    1.,
-					Rounds:   3,
-					Opinions: []opinion.Opinion{opinion.Dislike, opinion.Like, opinion.Dislike},
+					ID:              "one",
+					ProportionLiked: 1.,
+					Rounds:          3,
+					Opinions:        []opinion.Opinion{opinion.Dislike, opinion.Like, opinion.Dislike},
 				}},
 		},
 	}
