758f7a706d9a4c5439beb4da3071099612e7c285
capossele <angelocapossele@gmail.com>
2021-03-18 16:11:05 +0000

    move NewErrorResponse

--- plugins/webapi/ledgerstate/branch.go
+++ plugins/webapi/ledgerstate/branch.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/iotaledger/goshimmer/packages/ledgerstate"
 	"github.com/iotaledger/goshimmer/plugins/messagelayer"
+	"github.com/iotaledger/goshimmer/plugins/webapi"
 	"github.com/labstack/echo"
 )
 
@@ -15,7 +16,7 @@ import (
 func GetBranchEndPoint(c echo.Context) (err error) {
 	branchID, err := branchIDFromContext(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, NewErrorResponse(err))
+		return c.JSON(http.StatusBadRequest, webapi.NewErrorResponse(err))
 	}
 
 	if messagelayer.Tangle().LedgerState.Branch(branchID).Consume(func(branch ledgerstate.Branch) {
@@ -24,14 +25,14 @@ func GetBranchEndPoint(c echo.Context) (err error) {
 		return
 	}
 
-	return c.JSON(http.StatusBadRequest, NewErrorResponse(fmt.Errorf("failed to load Branch with %s", branchID)))
+	return c.JSON(http.StatusBadRequest, webapi.NewErrorResponse(fmt.Errorf("failed to load Branch with %s", branchID)))
 }
 
 // GetBranchChildrenEndPoint is the handler for the /ledgerstate/branch/:branchID/childBranches endpoint.
 func GetBranchChildrenEndPoint(c echo.Context) (err error) {
 	branchID, err := branchIDFromContext(c)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, NewErrorResponse(err))
+		return c.JSON(http.StatusBadRequest, webapi.NewErrorResponse(err))
 	}
 
 	cachedChildBranches := messagelayer.Tangle().LedgerState.ChildBranches(branchID)
