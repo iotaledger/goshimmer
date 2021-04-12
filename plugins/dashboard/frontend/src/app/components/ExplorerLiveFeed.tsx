922f364a6a7fbb6bcf4ac91d29167a9d589362eb
jkrvivian <jkrvivian@gmail.com>
2021-04-12 15:02:56 +0800

    fix: mana leaderboard and explorer livefeed scroll view

--- plugins/dashboard/frontend/src/app/components/ExplorerLiveFeed.tsx
+++ plugins/dashboard/frontend/src/app/components/ExplorerLiveFeed.tsx
@@ -25,9 +25,9 @@ export class ExplorerLiveFeed extends React.Component<Props, any> {
                         <Card.Body>
                             <Card.Title>Live Feed</Card.Title>
                             <Row className={"mb-3"}>
-                                <Col xs={12}>
+                                <Col xs={12} style={{'height':'500px', 'overflow':'auto'}}>
                                     <h6>Messages</h6>
-                                    <Table style={{'height':'500px', 'overflow':'scroll', 'display':'block'}}>
+                                    <Table>
                                         <thead>
                                         <tr>
                                             <td>Id</td>
