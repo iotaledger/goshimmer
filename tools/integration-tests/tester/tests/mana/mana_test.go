80f2b274087a851759ef7697a62b85abf6d8fe45
capossele <angelocapossele@gmail.com>
2021-04-27 09:47:26 +0100

    Fix mana

--- tools/integration-tests/tester/tests/mana/mana_test.go
+++ tools/integration-tests/tester/tests/mana/mana_test.go
@@ -226,7 +226,7 @@ func TestApis(t *testing.T) {
 	require.NoError(t, err)
 	resp4.Nodes = stripGenesisNodeID(resp4.Nodes)
 	require.Equal(t, 2, len(resp3.Nodes))
-	require.Equal(t, 2, len(resp4.Nodes))
+	require.Equal(t, 3, len(resp4.Nodes))
 	for i := 0; i < 3; i++ {
 		assert.Equal(t, base58.Encode(peers[i].ID().Bytes()), resp3.Nodes[i].NodeID)
 		// faucet pledged its cons mana to emptyNodeID...
