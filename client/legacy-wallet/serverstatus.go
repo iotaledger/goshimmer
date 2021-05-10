eab429fe5d7746d0ce257a1aefb7f5a9c751b867
Levente Pap <levente.pap@iota.org>
2021-05-10 13:32:51 +0200

    Remove legacy-wallet lib

--- client/legacy-wallet/serverstatus.go
+++
@@ -1,9 +0,0 @@
-package legacy_wallet
-
-// ServerStatus defines the information of connected server
-type ServerStatus struct {
-	ID        string
-	Synced    bool
-	Version   string
-	ManaDecay float64
-}
