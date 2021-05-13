8e4f17ba6eb055a956ec44c55d17a025e63458a1
Angelo Capossele <angelocapossele@gmail.com>
2021-05-13 11:58:13 +0100

    Add dRNG remotelog (#1279)
    * wip
    
    * add dRNG metrics
    
    * Remove unnecessary object creation.
    
    * Add comment and update struct name
    
    * Add transaction remotelog (#1281)
    
    * Add transaction remote log
    
    * Remove unnecessary object creation.
    
    * Add necessary comment and update struct name.
    
    Co-authored-by: Piotr Macek <piotr.macek@ext.iota.org>
    
    Co-authored-by: Piotr Macek <piotr.macek@ext.iota.org>

--- packages/remotelogmetrics/events.go
+++ packages/remotelogmetrics/events.go
@@ -52,3 +52,24 @@ type FPCConflictRecord struct {
 	// ConflictCreationTime points to time when the context has been created
 	ConflictCreationTime time.Time `json:"conflictStart" bson:"conflictStart"`
 }
+
+// TransactionMetrics defines the transaction metrics record to sent be to remote logger.
+type TransactionMetrics struct {
+	Type               string    `json:"type" bson:"type"`
+	MessageID          string    `json:"messageID" bson:"messageID"`
+	TransactionID      string    `json:"transactionID" bson:"transactionID"`
+	IssuedTimestamp    time.Time `json:"issuedTimestamp" bson:"issuedTimestamp"`
+	SolidTimestamp     time.Time `json:"solidTimestamp" bson:"solidTimestamp"`
+	ScheduledTimestamp time.Time `json:"scheduledTimestamp" bson:"scheduledTimestamp"`
+	BookedTimestamp    time.Time `json:"bookedTimestamp" bson:"bookedTimestamp"`
+	ConfirmedTimestamp time.Time `json:"confirmedTimestamp" bson:"confirmedTimestamp"`
+}
+
+// DRNGMetrics defines the DRNG metrics record to sent be to remote logger.
+type DRNGMetrics struct {
+	Type              string    `json:"type" bson:"type"`
+	InstanceID        uint32    `json:"instanceID" bson:"instanceID"`
+	Round             uint64    `json:"round" bson:"round"`
+	IssuedTimestamp   time.Time `json:"issuedTimestamp" bson:"issuedTimestamp"`
+	ReceivedTimestamp time.Time `json:"receivedTimestamp" bson:"receivedTimestamp"`
+}
