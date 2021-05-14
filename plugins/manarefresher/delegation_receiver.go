6f8d08e3e2f622f16591d497f75a550408cfe9be
Levente Pap <levente.pap@iota.org>
2021-05-14 17:49:19 +0200

    ManaRefresher Plugin
    Checks for delegated outputs on the address derived from
    the node's identity and periodically refreshes its mana.

---
+++ plugins/manarefresher/delegation_receiver.go
@@ -0,0 +1,100 @@
+package manarefresher
+
+import (
+	"sync"
+	"time"
+
+	"github.com/iotaledger/goshimmer/packages/clock"
+	"github.com/iotaledger/goshimmer/packages/ledgerstate"
+	"github.com/iotaledger/goshimmer/plugins/messagelayer"
+)
+
+// DelegationReceiver checks for delegation outputs on the wallet address and keeps the most recent delegated balance.
+type DelegationReceiver struct {
+	*wallet
+	delegatedFunds map[ledgerstate.Color]uint64
+	delFundsMutex  sync.RWMutex
+
+	sync.RWMutex
+}
+
+// Scan scans for unspent delegation outputs on the delegation receiver address
+func (d *DelegationReceiver) Scan() []*ledgerstate.AliasOutput {
+	d.Lock()
+	defer d.Unlock()
+	cachedOutputs := messagelayer.Tangle().LedgerState.OutputsOnAddress(d.address)
+	defer cachedOutputs.Release()
+	now := clock.SyncedTime()
+
+	filtered := cachedOutputs.Unwrap().Filter(func(output ledgerstate.Output) bool {
+		// it has to be an alias
+		if output.Type() != ledgerstate.AliasOutputType {
+			return false
+		}
+		// it has to be unspent
+		isUnspent := false
+		isConfirmed := false
+		messagelayer.Tangle().LedgerState.OutputMetadata(output.ID()).Consume(func(outputMetadata *ledgerstate.OutputMetadata) {
+			isUnspent = outputMetadata.ConsumerCount() == 0
+			incState, err := messagelayer.Tangle().LedgerState.TransactionInclusionState(output.ID().TransactionID())
+			if err != nil {
+				return
+			}
+			isConfirmed = incState == ledgerstate.Confirmed
+		})
+		if !isUnspent || !isConfirmed {
+			return false
+		}
+		// has to be a delegation alias that the delegation address owns for at least 1 min into the future
+		alias := output.(*ledgerstate.AliasOutput)
+		if !alias.GetStateAddress().Equals(d.address) {
+			return false
+		}
+		if !alias.IsDelegated() {
+			return false
+		}
+		// when delegation timelock is present, we want to have 1 minute window to prepare the refresh tx, otherwise just drop it
+		// TODO: what is the optimal window?
+		if !alias.DelegationTimelock().IsZero() && !alias.DelegationTimeLockedNow(now.Add(time.Minute)) {
+			return false
+		}
+		return true
+	})
+
+	scanResult := make([]*ledgerstate.AliasOutput, len(filtered))
+	for i, output := range filtered {
+		scanResult[i] = output.Clone().(*ledgerstate.AliasOutput)
+	}
+	d.updateDelegatedFunds(scanResult)
+	return scanResult
+}
+
+// updateDelegatedFunds updates the internal store of the delegated amount.
+func (d *DelegationReceiver) updateDelegatedFunds(delegatedOutputs []*ledgerstate.AliasOutput) {
+	d.delFundsMutex.Lock()
+	defer d.delFundsMutex.Unlock()
+	current := map[ledgerstate.Color]uint64{}
+	for _, alias := range delegatedOutputs {
+		alias.Balances().ForEach(func(color ledgerstate.Color, balance uint64) bool {
+			current[color] += balance
+			return true
+		})
+	}
+	d.delegatedFunds = current
+}
+
+// TotalDelegatedFunds returns the total amount of funds currently delegated to this node.
+func (d *DelegationReceiver) TotalDelegatedFunds() uint64 {
+	d.delFundsMutex.RLock()
+	defer d.delFundsMutex.RUnlock()
+	total := uint64(0)
+	for _, balance := range d.delegatedFunds {
+		total += balance
+	}
+	return total
+}
+
+// Address returns the receive address of the delegation receiver.
+func (d *DelegationReceiver) Address() ledgerstate.Address {
+	return d.address
+}
---
+++ plugins/manarefresher/plugin.go
@@ -0,0 +1,106 @@
+package manarefresher
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	"github.com/iotaledger/hive.go/daemon"
+	"github.com/iotaledger/hive.go/node"
+
+	"github.com/iotaledger/goshimmer/packages/ledgerstate"
+	"github.com/iotaledger/goshimmer/packages/shutdown"
+	"github.com/iotaledger/goshimmer/plugins/autopeering/local"
+)
+
+var (
+	// plugin is the plugin instance of the activity plugin.
+	plugin    *node.Plugin
+	once      sync.Once
+	refresher *Refresher
+)
+
+// minRefreshInterval is the minimum refresh interval allowed for delegated outputs.
+const minRefreshInterval = 1 // minutes
+
+// Plugin gets the plugin instance.
+func Plugin() *node.Plugin {
+	once.Do(func() {
+		plugin = node.NewPlugin("ManaRefresher", node.Enabled, configure, run)
+	})
+	return plugin
+}
+
+// configure events
+func configure(_ *node.Plugin) {
+	plugin.LogInfof("starting node with manarefresher plugin")
+	nodeIDPrivateKey, err := local.GetInstance().Database().LocalPrivateKey()
+	if err != nil {
+		panic(errors.Wrap(err, "couldn't load private key of node identity"))
+	}
+	localWallet := newWalletFromPrivateKey(nodeIDPrivateKey)
+	refresher = NewRefresher(localWallet, &DelegationReceiver{wallet: localWallet})
+
+	if Parameters.RefreshInterval < minRefreshInterval {
+		panic(fmt.Sprintf("manarefresh interval of %d minutes is too small, minimum is %s minutes", Parameters.RefreshInterval, minRefreshInterval))
+	}
+}
+
+func run(_ *node.Plugin) {
+	if err := daemon.BackgroundWorker("ManaRefresher-plugin", func(shutdownSignal <-chan struct{}) {
+		ticker := time.NewTicker(time.Duration(Parameters.RefreshInterval) * time.Minute)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-shutdownSignal:
+				return
+
+			case <-ticker.C:
+				err := refresher.Refresh()
+				if err != nil {
+					plugin.LogErrorf("couldn't refresh mana: %w", err)
+				}
+			}
+		}
+	}, shutdown.PriorityManaRefresher); err != nil {
+		plugin.Panicf("Failed to start as daemon: %s", err)
+	}
+}
+
+// DelegationAddress returns the current mana delegation address of the node.
+func DelegationAddress() (ledgerstate.Address, error) {
+	if refresher != nil {
+		return refresher.receiver.Address(), nil
+	}
+	return nil, errors.Errorf("manarefresher plugin is disabled")
+}
+
+// TotalDelegatedFunds returns the amount of funds delegated to the node.
+func TotalDelegatedFunds() (amount uint64) {
+	if refresher == nil {
+		return 0
+	}
+	// need to scan to get the most recent
+	_ = refresher.receiver.Scan()
+	return refresher.receiver.TotalDelegatedFunds()
+}
+
+// DelegatedOutputs returns all confirmed, unspent outputs that are delegated to the node.
+func DelegatedOutputs() (delegated ledgerstate.Outputs, err error) {
+	if refresher == nil {
+		err = errors.Errorf("manarefresher plugin is not running, node doesn't process delegated outputs")
+		return
+	}
+	// need to scan to get the most recent
+	found := refresher.receiver.Scan()
+	if len(found) == 0 {
+		err = errors.Errorf("no confirmed delegated outputs found")
+		return
+	}
+	delegated = make(ledgerstate.Outputs, len(found))
+	for i, _ := range found {
+		delegated[i] = found[i]
+	}
+	return delegated, nil
+}
