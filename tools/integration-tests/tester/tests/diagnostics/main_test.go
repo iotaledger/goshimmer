991e3fb09366870a29534ea85bde5a944fe9cfca
Gal Rogozinski <galrogogit@gmail.com>
2021-05-18 13:17:29 +0300

    add diagnostics tests

---
+++ tools/integration-tests/tester/tests/diagnostics/main_test.go
@@ -0,0 +1,25 @@
+package diagnostics
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/iotaledger/goshimmer/tools/integration-tests/tester/framework"
+)
+
+var f *framework.Framework
+
+// TestMain gets called by the test utility and is executed before any other test in this package.
+// It is therefore used to initialize the integration testing framework.
+func TestMain(m *testing.M) {
+	var err error
+	f, err = framework.Instance()
+	// call the tests
+	code := m.Run()
+	err = f.DestroyFramework()
+	if err != nil {
+		fmt.Printf("error destroying framework: %v", err)
+	}
+	os.Exit(code)
+}
