dea28b1a32ebb95be365c0d519313d55a2f22d9b
capossele <angelocapossele@gmail.com>
2021-05-21 18:44:23 +0100

    require no error for wait for autopeering

--- tools/integration-tests/tester/tests/value/value_test.go
+++ tools/integration-tests/tester/tests/value/value_test.go
@@ -79,6 +79,7 @@ func TestTransactionPersistence(t *testing.T) {
 
 	// wait for peers to start
 	time.Sleep(20 * time.Second)
+	require.NoError(t, n.WaitForAutopeering(2))
 
 	// check whether all issued transactions are available on all nodes and confirmed
 	tests.CheckTransactions(t, n.Peers(), txIds, false, tests.ExpectedInclusionState{
@@ -148,8 +149,7 @@ func TestValueColoredPersistence(t *testing.T) {
 
 	// wait for peers to start
 	time.Sleep(20 * time.Second)
-
-	n.WaitForAutopeering(2)
+	require.NoError(t, n.WaitForAutopeering(2))
 
 	// check whether all issued transactions are persistently available on all nodes, and confirmed
 	tests.CheckTransactions(t, n.Peers(), txIds, true, tests.ExpectedInclusionState{
@@ -221,7 +221,7 @@ func TestAlias_Persistence(t *testing.T) {
 
 	// wait for peers to start
 	time.Sleep(20 * time.Second)
-	n.WaitForAutopeering(3)
+	require.NoError(t, n.WaitForAutopeering(3))
 
 	// check if nodes still have the outputs and transaction
 	for _, peer := range n.Peers() {
