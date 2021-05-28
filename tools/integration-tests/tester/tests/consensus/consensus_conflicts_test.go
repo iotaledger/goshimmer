97f02eb54aa6af2b1254a3a36ca85499d438c681
apenzk <andreas.penzkofer@iota.org>
2021-05-28 15:47:31 +0200

    correct time.Second for ParaFCoBQuarantineTime

--- tools/integration-tests/tester/tests/consensus/consensus_conflicts_test.go
+++ tools/integration-tests/tester/tests/consensus/consensus_conflicts_test.go
@@ -20,15 +20,7 @@ import (
 )
 
 func TestConsensus(t *testing.T) {
-	// backupParaWaitToKill := framework.ParaWaitToKill
-	// framework.ParaWaitToKill = 2*framework.ParaFCoBQuarantineTime + 10
-
 	const numberOfPeers = 6
-
-	// // reset framework paras
-	// defer func() {
-	// 	framework.ParaWaitToKill = backupParaWaitToKill
-	// }()
 
 	// create two partitions with their own peers
 	n, err := f.CreateNetworkWithMana("conflict", numberOfPeers, framework.CreateNetworkConfig{Faucet: true, Mana: true, StartSynced: true})
@@ -92,7 +84,7 @@ func TestConsensus(t *testing.T) {
 	}
 	// sleep 3* the avg. network delay so both partitions confirm their own pledging transaction
 	// and 1 avg delay more to make sure each node has mana
-	log.Printf("waiting 2 * %d seconds avg. network delay + 5s to make the transactions confirmed", framework.ParaFCoBQuarantineTime)
+	log.Printf("waiting 2 * %d seconds avg. network delay + 5s to make the transactions confirmed", framework.ParaFCoBQuarantineTime/time.Second)
 	time.Sleep(framework.ParaFCoBQuarantineTime*2 + 5*time.Second)
 
 	resp1, err := n.Peers()[0].GoShimmerAPI.GetAllMana()
@@ -121,7 +113,7 @@ func TestConsensus(t *testing.T) {
 		conflictingTxIDs[i] = resp.TransactionID
 
 		// sleep to prefer the first one
-		time.Sleep(framework.ParaFCoBQuarantineTime * time.Second)
+		time.Sleep(framework.ParaFCoBQuarantineTime)
 	}
 
 	log.Println("waiting for transactions to be available on all peers...")
