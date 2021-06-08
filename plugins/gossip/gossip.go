397e5f45d6cce06fe8201fe947fa00eb514c16b8
Georgy Savva <georgy.savva@gmail.com>
2021-06-08 11:50:05 +0300

    Fake commit to trigger cache

--- plugins/gossip/gossip.go
+++ plugins/gossip/gossip.go
@@ -58,6 +58,7 @@ func start(shutdownSignal <-chan struct{}) {
 	// use the port of the gossip service
 	gossipEndpoint := lPeer.Services().Get(service.GossipKey)
 
+
 	// resolve the bind address
 	address := net.JoinHostPort(config.Node().String(local.CfgBind), strconv.Itoa(gossipEndpoint.Port()))
 	localAddr, err := net.ResolveTCPAddr(gossipEndpoint.Network(), address)
