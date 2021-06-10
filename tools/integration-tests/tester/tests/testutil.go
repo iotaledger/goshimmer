37b03410ad442dc7c475391e466f9ec7f82ba0bf
Jonas Theis <4181434+jonastheis@users.noreply.github.com>
2021-06-10 11:46:10 +0200

    Improve common integration test (#1424)
    * Remove message integration test
    
    * Improve common test to wait for messages to become available instead of blindly waiting some time
    
    * Add waitFor parameter
    
    * Address review comments
    
    * Add lock

--- tools/integration-tests/tester/tests/testutil.go
+++ tools/integration-tests/tester/tests/testutil.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"sync"
@@ -25,6 +26,7 @@ import (
 )
 
 var (
+	ErrMessageNotAvailableInTime     = errors.New("message was not available in time")
 	ErrTransactionNotAvailableInTime = errors.New("transaction was not available in time")
 	ErrTransactionStateNotSameInTime = errors.New("transaction state did not materialize in time")
 	ErrNotSynced                     = errors.New("peers not synced")
@@ -126,8 +128,21 @@ func SendFaucetRequest(t *testing.T, peer *framework.Peer, addr ledgerstate.Addr
 	return resp.ID, sent
 }
 
-// CheckForMessageIDs performs checks to make sure that all peers received all given messages defined in ids.
-func CheckForMessageIDs(t *testing.T, peers []*framework.Peer, messageIDs map[string]DataMessageSent, checkSynchronized bool) {
+// CheckForMessageIDs first waits for all messages to be available, and then performs checks to make sure that all peers received all given messages with
+// their correct information.
+func CheckForMessageIDs(t *testing.T, peers []*framework.Peer, messageIDs map[string]DataMessageSent, checkSynchronized bool, waitFor time.Duration) {
+	missing, err := AwaitMessageAvailability(peers, messageIDs, waitFor)
+	if err != nil {
+		assert.NoError(t, err, "messages should have been available")
+		for p, missingOnPeer := range missing {
+			log.Printf("missing on peer %s:", p)
+			for missingMessage := range missingOnPeer {
+				log.Println("message id: ", missingMessage)
+			}
+		}
+		return
+	}
+
 	for _, peer := range peers {
 		if checkSynchronized {
 			// check that the peer sees itself as synchronized
@@ -162,6 +177,66 @@ func CheckForMessageIDs(t *testing.T, peers []*framework.Peer, messageIDs map[st
 		// check that all messages are present in response
 		assert.ElementsMatchf(t, idsSlice, respIDs, "messages do not match sent in %s", peer.String())
 	}
+}
+
+// AwaitMessageAvailability awaits until the given message IDs become available on all given peers or
+// the max duration is reached. Returns a map of missing messages per peer. An error is returned if at least
+// one peer does not have all specified messages available.
+func AwaitMessageAvailability(peers []*framework.Peer, messageIDs map[string]DataMessageSent, waitFor time.Duration) (missing map[identity.ID]map[string]types.Empty, err error) {
+	s := time.Now()
+	var missingMu sync.RWMutex
+	missing = map[identity.ID]map[string]types.Empty{}
+
+	for i := 0; time.Since(s) < waitFor; time.Sleep(500 * time.Millisecond) {
+		var wg sync.WaitGroup
+		wg.Add(len(peers))
+
+		for _, p := range peers {
+			go func(p *framework.Peer) {
+				defer wg.Done()
+
+				missingMu.RLock()
+				m, has := missing[p.ID()]
+				missingMu.RUnlock()
+
+				// do not request messages again for this peer if a previous iteration did not yield any missing messages
+				if i > 0 && !has {
+					return
+				}
+
+				for messageID := range messageIDs {
+					_, err := p.GetMessage(messageID)
+					if err == nil {
+						if has {
+							delete(m, messageID)
+							if len(m) == 0 {
+								missingMu.Lock()
+								delete(missing, p.ID())
+								missingMu.Unlock()
+							}
+						}
+						continue
+					}
+
+					if !has {
+						m = map[string]types.Empty{}
+					}
+					m[messageID] = types.Void
+
+					missingMu.Lock()
+					missing[p.ID()] = m
+					missingMu.Unlock()
+				}
+			}(p)
+		}
+		wg.Wait()
+
+		if len(missing) == 0 {
+			return missing, nil
+		}
+		i++
+	}
+	return missing, ErrMessageNotAvailableInTime
 }
 
 // SendTransactionFromFaucet sends funds to peers from the faucet, sends back the remainder to faucet, and returns the transaction ID.
