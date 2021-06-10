09b7e3e8567cce489934e6c2114f801568a454e9
Gal Rogozinski <galrogogit@gmail.com>
2021-06-10 22:50:59 +0300

    add test mains and test helper

---
+++ packages/testhelper/testhelper.go
@@ -0,0 +1,11 @@
+package testhelper
+
+// TestSetup setups global environment and parameters for any unit test
+func GlobalSetup() {
+	// To be implemented
+}
+
+// TestTeardown resets all the changes the test made
+func GlobalTeardown() {
+	// To be implemented
+}
