d75825eef64f09827ce2cbf0853120c7d770b4bd
Andreas Penzkofer <36140574+apenzk@users.noreply.github.com>
2021-06-11 11:12:25 +0200

    reduce activity message parentCount parameter for experimental setup (#1425)
    * change parameter for experiment
    
    * Add activity flags
    
    * Add activity flags
    
    Co-authored-by: Daria Dziubałtowska <daria.dziubaltowska@iota.org>

--- plugins/activity/parameters.go
+++ plugins/activity/parameters.go
@@ -6,6 +6,10 @@ import "github.com/iotaledger/hive.go/configuration"
 var Parameters = struct {
 	// BroadcastIntervalSec is the interval in seconds at which the node broadcasts its activity message.
 	BroadcastIntervalSec int `default:"3" usage:"the interval at which the node will broadcast its activity message"`
+	// ParentsCount is the number of parents that node will choose for its activity messages.
+	ParentsCount int `default:"8" usage:"the number of parents that node will choose for its activity messages"`
+	// DelayOffset is the maximum for random initial time delay before sending the activity message.
+	DelayOffset int `default:"10" usage:"the maximum for random initial time delay before sending the activity message"`
 }{}
 
 func init() {
