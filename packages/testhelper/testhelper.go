98516c5cf516c436f5283682df9b772a80f785ec
Gal Rogozinski <galrogogit@gmail.com>
2021-06-13 23:57:45 +0300

    fix comments

--- packages/testhelper/testhelper.go
+++ packages/testhelper/testhelper.go
@@ -1,11 +1,11 @@
 package testhelper
 
-// TestSetup setups global environment and parameters for any unit test
+// GlobalSetup setups global environment and parameters for any unit test
 func GlobalSetup() {
 	// To be implemented
 }
 
-// TestTeardown resets all the changes the test made
+// GlobalTeardown resets all the changes the test made
 func GlobalTeardown() {
 	// To be implemented
 }
