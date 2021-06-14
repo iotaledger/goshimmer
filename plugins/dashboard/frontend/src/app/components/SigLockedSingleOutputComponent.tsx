61848b4f6c579af61e2478ce0366ea2a70a63f45
Levente Pap <levente.pap@iota.org>
2021-06-14 12:11:45 +0200

    Fix loading tx from snapshot on dashboard
    A referenced input in a tx loaded from the snapshot comes from the
    genesis transaction. Genesis transaction is not a real transaction
    in a sense that it doesn't have inputs, as this tx creates the funds
    available in the network.
    
    Manually fill out info of outputs coming from the genesis tx on
    local dashboard. Address "GENESIS" is displayed with 0 balance
    for such referenced outputs.

--- plugins/dashboard/frontend/src/app/components/SigLockedSingleOutputComponent.tsx
+++ plugins/dashboard/frontend/src/app/components/SigLockedSingleOutputComponent.tsx
@@ -10,6 +10,8 @@ interface Props {
 
 export class SigLockedSingleOutputComponent extends React.Component<Props, any> {
     render() {
+        let o = this.props.output;
+        let id = this.props.id;
         return (
             <div className={"mb-2"} key={this.props.id.base58}>
                 <ListGroup>
@@ -17,13 +19,13 @@ export class SigLockedSingleOutputComponent extends React.Component<Props, any> 
                     <ListGroup.Item>
                         Balances:
                         <div>
-                            <div><Badge variant="success">{new Intl.NumberFormat().format(this.props.output.balance)} IOTA</Badge></div>
+                            <div><Badge variant="success">{new Intl.NumberFormat().format(o.balance)} IOTA</Badge></div>
                         </div>
                     </ListGroup.Item>
-                    <ListGroup.Item>OutputID: <a href={`/explorer/output/${this.props.id.base58}`}>{this.props.id.base58}</a></ListGroup.Item>
-                    <ListGroup.Item>Address: <a href={`/explorer/address/${this.props.output.address}`}> {this.props.output.address}</a></ListGroup.Item>
-                <ListGroup.Item>Transaction: <a href={`/explorer/transaction/${this.props.id.transactionID}`}> {this.props.id.transactionID}</a></ListGroup.Item>
-                <ListGroup.Item>Output Index: {this.props.id.outputIndex}</ListGroup.Item>
+                    <ListGroup.Item>OutputID: <a href={`/explorer/output/${id.base58}`}>{id.base58}</a></ListGroup.Item>
+                    <ListGroup.Item>Address: <a href={`/explorer/address/${o.address}`}> {o.address}</a></ListGroup.Item>
+                <ListGroup.Item>Transaction: <a href={`/explorer/transaction/${id.transactionID}`}> {id.transactionID}</a></ListGroup.Item>
+                <ListGroup.Item>Output Index: {id.outputIndex}</ListGroup.Item>
                 </ListGroup>
             </div>
         );
