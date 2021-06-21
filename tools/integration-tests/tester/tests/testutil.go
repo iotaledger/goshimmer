6d4b129d7433a290f6c72ae654cecee6e4776f3b
Wolfgang Welz <welzwo@gmail.com>
2021-06-21 19:11:19 +0200

    remove unused functions

--- tools/integration-tests/tester/tests/testutil.go
+++ tools/integration-tests/tester/tests/testutil.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/cockroachdb/errors"
 	"github.com/iotaledger/hive.go/identity"
-	"github.com/iotaledger/hive.go/stringify"
 	"github.com/mr-tron/base58"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -24,11 +22,7 @@ import (
 	"github.com/iotaledger/goshimmer/tools/integration-tests/tester/framework"
 )
 
-var (
-	faucetTokensPerRequest     = framework.PeerConfig.Faucet.TokensPerRequest
-	faucetPoWDifficulty        = framework.PeerConfig.Faucet.PowDifficulty
-	faucetPreparedOutputsCount = framework.PeerConfig.Faucet.PreparedOutputsCount
-)
+var faucetPoWDifficulty = framework.PeerConfig.Faucet.PowDifficulty
 
 const Tick = 500 * time.Millisecond
 
@@ -108,15 +102,6 @@ func SendFaucetRequest(t *testing.T, peer *framework.Node, addr ledgerstate.Addr
 	return resp.ID, sent
 }
 
-func SendFaucetRequests(t *testing.T, nodes []*framework.Node) {
-	addrBalance := make(map[identity.ID]map[ledgerstate.Color]uint64)
-
-	for _, node := range nodes {
-		SendFaucetRequest(t, node, node.Address(0))
-		addrBalance[node.ID()] = map[ledgerstate.Color]uint64{ledgerstate.ColorIOTA: uint64(node.Config().Faucet.TokensPerRequest)}
-	}
-}
-
 // RequireMessagesAvailable asserts that all nodes have received MessageIDs in waitFor time, periodically checking each tick.
 func RequireMessagesAvailable(t *testing.T, nodes []*framework.Node, messageIDs map[string]DataMessageSent, waitFor time.Duration, tick time.Duration) {
 	missing := make(map[identity.ID]map[string]struct{}, len(nodes))
@@ -176,73 +161,6 @@ func AssertMessagesEqual(t *testing.T, nodes []*framework.Node, messageIDs map[s
 		}
 	}
 	log.Println("Validating messages... done")
-}
-
-// SendTransactionFromFaucet sends funds to peers from the faucet, sends back the remainder to faucet, and returns the transaction ID.
-func SendTransactionFromFaucet(t *testing.T, peers []*framework.Node, sentValue int64) (txIDs []string, addrBalance map[string]map[ledgerstate.Color]int64) {
-	// initiate addrBalance map
-	addrBalance = make(map[string]map[ledgerstate.Color]int64)
-	for _, p := range peers {
-		addr := p.Seed.Address(0).Address().Base58()
-		addrBalance[addr] = make(map[ledgerstate.Color]int64)
-	}
-
-	faucetPeer := peers[0]
-
-	// faucet keeps remaining amount on address 0
-	addrBalance[faucetPeer.Seed.Address(0).Address().Base58()][ledgerstate.ColorIOTA] = int64(framework.GenesisTokenAmount - faucetPreparedOutputsCount*faucetTokensPerRequest)
-	var i uint64
-	// faucet has split genesis output into n bits of 1337 each and remainder on 0
-	for i = 1; i < uint64(len(peers)); i++ {
-		faucetAddrStr := faucetPeer.Seed.Address(i).Address().Base58()
-		addrBalance[faucetAddrStr] = make(map[ledgerstate.Color]int64)
-		// get faucet balances
-		unspentOutputs, err := faucetPeer.PostAddressUnspentOutputs([]string{faucetAddrStr})
-		require.NoErrorf(t, err, "could not get unspent outputs on %s", faucetPeer.String())
-		out, err := unspentOutputs.UnspentOutputs[0].Outputs[0].Output.ToLedgerstateOutput()
-		require.NoError(t, err)
-		balanceValue, exist := out.Balances().Get(ledgerstate.ColorIOTA)
-		assert.Equal(t, true, exist)
-		addrBalance[faucetAddrStr][ledgerstate.ColorIOTA] = int64(balanceValue)
-
-		// send funds to other peers
-		fail, txID := SendIotaTransaction(t, faucetPeer, peers[i], addrBalance, sentValue, TransactionConfig{
-			FromAddressIndex:      i,
-			ToAddressIndex:        0,
-			AccessManaPledgeID:    peers[i].ID(),
-			ConsensusManaPledgeID: peers[i].ID(),
-		})
-		require.False(t, fail)
-		txIDs = append(txIDs, txID)
-
-		// let the transaction propagate
-		time.Sleep(3 * time.Second)
-	}
-
-	return
-}
-
-// SendTransactionOnRandomPeer sends sentValue amount of IOTA tokens from/to a random peer, mutates the given balance map and returns the transaction IDs.
-func SendTransactionOnRandomPeer(t *testing.T, peers []*framework.Node, addrBalance map[string]map[ledgerstate.Color]int64, numMessages int, sentValue int64) (txIDs []string) {
-	counter := 0
-	for i := 0; i < numMessages; i++ {
-		from := rand.Intn(len(peers))
-		to := rand.Intn(len(peers))
-		fail, txID := SendIotaTransaction(t, peers[from], peers[to], addrBalance, sentValue, TransactionConfig{})
-		if fail {
-			i--
-			counter++
-			if counter >= 10 {
-				return
-			}
-			continue
-		}
-
-		// attach tx id
-		txIDs = append(txIDs, txID)
-	}
-
-	return
 }
 
 func SendValue(t *testing.T, from *framework.Node, to *framework.Node, color ledgerstate.Color, value uint64, txConfig TransactionConfig, addrBalance ...map[string]map[ledgerstate.Color]uint64) (string, error) {
@@ -310,168 +228,6 @@ func SendValue(t *testing.T, from *framework.Node, to *framework.Node, color led
 		addrBalance[0][outputAddr.Base58()][outputColor] += value
 	}
 	return resp.TransactionID, nil
-}
-
-// SendIotaTransaction sends sentValue amount of IOTA tokens and remainders from and to a given peer and returns the fail flag and the transaction ID.
-// Every peer sends and receives the transaction on the address of index 0.
-// Optionally, the nodes to pledge access and consensus mana can be specified.
-func SendIotaTransaction(t *testing.T, from *framework.Node, to *framework.Node, addrBalance map[string]map[ledgerstate.Color]int64, sentValue int64, txConfig TransactionConfig) (fail bool, txID string) {
-	inputAddr := from.Seed.Address(txConfig.FromAddressIndex).Address()
-	outputAddr := to.Seed.Address(txConfig.ToAddressIndex).Address()
-
-	// prepare inputs
-	resp, err := from.PostAddressUnspentOutputs([]string{inputAddr.Base58()})
-	require.NoErrorf(t, err, "could not get unspent outputs on %s", from.String())
-
-	// abort if no unspent outputs
-	if len(resp.UnspentOutputs[0].Outputs) == 0 {
-		return true, ""
-	}
-	out, err := resp.UnspentOutputs[0].Outputs[0].Output.ToLedgerstateOutput()
-	require.NoError(t, err)
-	balanceValue, exist := out.Balances().Get(ledgerstate.ColorIOTA)
-	assert.Equal(t, true, exist)
-	availableValue := int64(balanceValue)
-
-	// abort if the balance is not enough
-	if availableValue < sentValue {
-		return true, ""
-	}
-
-	out, err = resp.UnspentOutputs[0].Outputs[0].Output.ToLedgerstateOutput()
-	require.NoErrorf(t, err, "invalid unspent outputs ID on %s", from.String())
-	input := ledgerstate.NewUTXOInput(out.ID())
-	if inputAddr == outputAddr {
-		sentValue = availableValue
-	}
-
-	// set balances
-	var outputs ledgerstate.Outputs
-	output := ledgerstate.NewSigLockedColoredOutput(ledgerstate.NewColoredBalances(map[ledgerstate.Color]uint64{
-		ledgerstate.ColorIOTA: uint64(sentValue),
-	}), outputAddr)
-	outputs = append(outputs, output)
-
-	// handle remainder address
-	if availableValue > sentValue {
-		output := ledgerstate.NewSigLockedColoredOutput(ledgerstate.NewColoredBalances(map[ledgerstate.Color]uint64{
-			ledgerstate.ColorIOTA: uint64(availableValue - sentValue),
-		}), inputAddr)
-		outputs = append(outputs, output)
-	}
-	txEssence := ledgerstate.NewTransactionEssence(0, time.Now(), txConfig.AccessManaPledgeID, txConfig.ConsensusManaPledgeID, ledgerstate.NewInputs(input), ledgerstate.NewOutputs(outputs...))
-	sig := ledgerstate.NewED25519Signature(from.KeyPair(txConfig.FromAddressIndex).PublicKey, from.KeyPair(txConfig.FromAddressIndex).PrivateKey.Sign(txEssence.Bytes()))
-	unlockBlock := ledgerstate.NewSignatureUnlockBlock(sig)
-	txn := ledgerstate.NewTransaction(txEssence, ledgerstate.UnlockBlocks{unlockBlock})
-
-	// send transaction
-	respTx, err := from.PostTransaction(txn.Bytes())
-	if err != nil {
-		fmt.Println(fmt.Errorf("could not send transaction on %s: %w", from.String(), err).Error())
-		return true, ""
-	}
-	txID = respTx.TransactionID
-	addrBalance[inputAddr.Base58()][ledgerstate.ColorIOTA] -= sentValue
-	addrBalance[outputAddr.Base58()][ledgerstate.ColorIOTA] += sentValue
-	return false, txID
-}
-
-// SendColoredTransaction sends IOTA and colored tokens from and to a given peer and returns the ok flag and transaction ID.
-// 1. Get the first unspent outputs of `from`
-// 2. Send 50 IOTA and 50 ColorMint to `to`
-func SendColoredTransaction(t *testing.T, from *framework.Node, to *framework.Node, addrBalance map[string]map[ledgerstate.Color]int64, txConfig TransactionConfig) (fail bool, txId string) {
-	var sentIOTAValue int64 = 50
-	var sentMintValue int64 = 50
-	var balanceList []coloredBalance
-	inputAddr := from.Seed.Address(txConfig.FromAddressIndex).Address()
-	outputAddr := to.Seed.Address(txConfig.ToAddressIndex).Address()
-
-	// prepare inputs
-	resp, err := from.PostAddressUnspentOutputs([]string{inputAddr.Base58()})
-	require.NoErrorf(t, err, "could not get unspent outputs on %s", from.String())
-
-	// abort if no unspent outputs
-	if len(resp.UnspentOutputs[0].Outputs) == 0 {
-		return false, ""
-	}
-
-	out, err := resp.UnspentOutputs[0].Outputs[0].Output.ToLedgerstateOutput()
-	require.NoError(t, err)
-	input := ledgerstate.NewUTXOInput(out.ID())
-
-	// prepare output
-	output := ledgerstate.NewSigLockedColoredOutput(ledgerstate.NewColoredBalances(map[ledgerstate.Color]uint64{
-		ledgerstate.ColorIOTA: uint64(sentIOTAValue),
-		ledgerstate.ColorMint: uint64(sentMintValue),
-	}), outputAddr)
-
-	txEssence := ledgerstate.NewTransactionEssence(0, time.Now(), identity.ID{}, identity.ID{}, ledgerstate.NewInputs(input), ledgerstate.NewOutputs(output))
-
-	// sign transaction
-	sig := ledgerstate.NewED25519Signature(from.KeyPair(txConfig.FromAddressIndex).PublicKey, from.KeyPair(txConfig.FromAddressIndex).PrivateKey.Sign(txEssence.Bytes()))
-	unlockBlock := ledgerstate.NewSignatureUnlockBlock(sig)
-	txn := ledgerstate.NewTransaction(txEssence, ledgerstate.UnlockBlocks{unlockBlock})
-
-	// set expected balances for test, credit from inputAddr
-	balanceList = append(balanceList, coloredBalance{
-		Color:   ledgerstate.ColorIOTA,
-		Balance: -(sentIOTAValue + sentMintValue),
-	})
-	// set expected balances for test, debit to outputAddr
-	balanceList = append(balanceList, coloredBalance{
-		Color:   ledgerstate.ColorIOTA,
-		Balance: sentIOTAValue,
-	})
-	balanceList = append(balanceList, coloredBalance{
-		Color:   ledgerstate.ColorMint,
-		Balance: sentMintValue,
-	})
-
-	// send transaction
-	respTx, err := from.PostTransaction(txn.Bytes())
-	if err != nil {
-		fmt.Println(fmt.Errorf("could not send transaction on %s: %w", from.String(), err).Error())
-		return true, ""
-	}
-	txId = respTx.TransactionID
-
-	// update balance list
-	updateBalanceList(addrBalance, balanceList, inputAddr.Base58(), outputAddr.Base58(), txn.Essence().Outputs()[0])
-
-	return false, txId
-}
-
-// updateBalanceList updates the token amount map with given peers and balances.
-// If the value of balance is negative, it is the balance to be deducted from peer from, else it is deposited to peer to.
-// If the color is ledgerstate.ColorMint, it is recolored / tokens are minted.
-func updateBalanceList(addrBalance map[string]map[ledgerstate.Color]int64, balances []coloredBalance, from, to string, output ledgerstate.Output) {
-	for _, b := range balances {
-		color := b.Color
-		value := b.Balance
-		if value < 0 {
-			// deduct
-			addrBalance[from][color] += value
-			continue
-		}
-		// deposit
-		if color == ledgerstate.ColorMint {
-			color = blake2b.Sum256(output.ID().Bytes())
-			addrBalance[to][color] = value
-			continue
-		}
-		addrBalance[to][color] += value
-	}
-	return
-}
-
-func getColorFromString(colorStr string) (color ledgerstate.Color) {
-	if colorStr == "IOTA" {
-		color = ledgerstate.ColorIOTA
-	} else {
-		t, _ := ledgerstate.TransactionIDFromBase58(colorStr)
-		color, _, _ = ledgerstate.ColorFromBytes(t.Bytes())
-	}
-	return
 }
 
 func RequireBalancesEqual(t *testing.T, nodes []*framework.Node, addrBalance map[string]map[ledgerstate.Color]uint64) {
@@ -660,20 +416,6 @@ func RequireInclusionStateEqual(t *testing.T, nodes []*framework.Node, transacti
 	log.Println("Waiting for inclusion state... done")
 }
 
-// AwaitSync waits until the given peers are in synced.
-func AwaitSync(t *testing.T, peers []*framework.Node, waitFor time.Duration, tick time.Duration) {
-	condition := func() bool {
-		for _, peer := range peers {
-			if !Synced(t, peer) {
-				return false
-			}
-		}
-		return true
-	}
-
-	require.Eventually(t, condition, waitFor, tick)
-}
-
 // ShutdownNetwork shuts down the network and reports errors.
 func ShutdownNetwork(ctx context.Context, t *testing.T, n Shutdowner) {
 	log.Println("Shutting down network...")
@@ -727,18 +469,6 @@ func Mana(t *testing.T, node *framework.Node) jsonmodels.Mana {
 	return info.Mana
 }
 
-type coloredBalance struct {
-	Color   ledgerstate.Color
-	Balance int64
-}
-
-func (b coloredBalance) String() string {
-	return stringify.Struct("coloredBalance",
-		stringify.StructField("Color", b.Color),
-		stringify.StructField("Balance", b.Balance),
-	)
-}
-
 func SelectIndex(transaction *ledgerstate.Transaction, address ledgerstate.Address) (index uint16) {
 	for i, output := range transaction.Essence().Outputs() {
 		if address.Base58() == output.(*ledgerstate.SigLockedSingleOutput).Address().Base58() {
@@ -746,16 +476,4 @@ func SelectIndex(transaction *ledgerstate.Transaction, address ledgerstate.Addre
 		}
 	}
 	return
-}
-
-func GetSnapshot() *ledgerstate.Snapshot {
-	snapshot := &ledgerstate.Snapshot{}
-	f, err := os.Open("/tmp/assets/7R1itJx5hVuo9w9hjg5cwKFmek4HMSoBDgJZN8hKGxih.bin")
-	if err != nil {
-		panic(fmt.Sprintln("can not open snapshot file: ", err))
-	}
-	if _, err := snapshot.ReadFrom(f); err != nil {
-		panic(fmt.Sprintln("could not read snapshot file: ", err))
-	}
-	return snapshot
 }
