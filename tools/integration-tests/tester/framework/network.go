25bb8e772c0566a448da083bf40289a4a0862337
Andrea Villa <andreakarimodm@gmail.com>
2021-06-25 17:53:54 +0200

    Export Netword.id, DockerContainer.id and DockerContainer.name

--- tools/integration-tests/tester/framework/network.go
+++ tools/integration-tests/tester/framework/network.go
@@ -25,7 +25,7 @@ import (
 // Network represents a complete GoShimmer network within Docker.
 // Including an entry node and arbitrary many peers.
 type Network struct {
-	id   string
+	Id   string
 	name string
 
 	docker *client.Client
@@ -52,7 +52,7 @@ func NewNetwork(ctx context.Context, dockerClient *client.Client, name string, t
 	}
 
 	return &Network{
-		id:     resp.ID,
+		Id:     resp.ID,
 		name:   name,
 		tester: tester,
 		docker: dockerClient,
@@ -231,12 +231,12 @@ func (n *Network) Shutdown(ctx context.Context) error {
 	}
 
 	// disconnect tester from network otherwise the network can't be removed
-	if err := n.tester.DisconnectFromNetwork(ctx, n.id); err != nil {
+	if err := n.tester.DisconnectFromNetwork(ctx, n.Id); err != nil {
 		return err
 	}
 
 	// remove network
-	if err := n.docker.NetworkRemove(ctx, n.id); err != nil {
+	if err := n.docker.NetworkRemove(ctx, n.Id); err != nil {
 		return err
 	}
 
@@ -268,7 +268,7 @@ func (n *Network) createNode(ctx context.Context, name string, conf config.GoShi
 	if err := container.CreateNode(ctx, conf); err != nil {
 		return nil, err
 	}
-	if err := container.ConnectToNetwork(ctx, n.id); err != nil {
+	if err := container.ConnectToNetwork(ctx, n.Id); err != nil {
 		return nil, err
 	}
 
