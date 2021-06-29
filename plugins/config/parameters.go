84b00fb424bfa3d1b310f810be74f06eb9438fe1
jonastheis <4181434+jonastheis@users.noreply.github.com>
2021-06-29 16:48:44 +0200

    Fix issues from prior plugins refactor

--- plugins/config/parameters.go
+++ plugins/config/parameters.go
@@ -4,6 +4,11 @@ import (
 	"github.com/iotaledger/hive.go/configuration"
 )
 
+const (
+	CfgDisablePlugins = "node.disablePlugins"
+	CfgEnablePlugins  = "node.enablePlugins"
+)
+
 // ParametersDefinition contains the definition of configuration parameters used by the config plugin.
 type ParametersDefinition struct {
 	// EnablePlugins is the flag to manually enable node plugins.
