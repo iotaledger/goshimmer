f5c5061f2d64b0213867c1ee27ae4ae15a75e501
Andrea Villa <andreakarimodm@gmail.com>
2021-06-30 10:08:26 +0200

    manualpeering.KnownPeers is a map

--- plugins/manualpeering/parameters.go
+++ plugins/manualpeering/parameters.go
@@ -4,8 +4,8 @@ import "github.com/iotaledger/hive.go/configuration"
 
 // ParametersDefinition contains the definition of the parameters used by the manualpeering plugin.
 type ParametersDefinition struct {
-	// KnownPeers defines the list of peers to be used as known peers.
-	KnownPeers string `default:"" usage:"list of peers that will be used as known peers"`
+	// KnownPeers defines the map of peers to be used as known peers.
+	KnownPeers string `default:"[]" usage:"map of peers that will be used as known peers"`
 }
 
 // Parameters contains the configuration used by the manualpeering plugin.
