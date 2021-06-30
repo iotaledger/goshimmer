66bda9752fe6fc2a2af432aa27a3ed06462a769c
Andrea Villa <andreakarimodm@gmail.com>
2021-06-30 10:43:08 +0200

    remotelog uses logger.Params.DisableEvents

--- plugins/remotelog/parameters.go
+++ plugins/remotelog/parameters.go
@@ -6,15 +6,16 @@ import (
 
 // ParametersDefinition contains the definition of the parameters used by the remotelog plugin.
 type ParametersDefinition struct {
-	// ServerAddress defines the server address that will receive the logs.
-	ServerAddress string `default:"ressims.iota.cafe:5213" usage:"RemoteLog server address"`
-	// DisableEvents defines whether to disable logger events.
-	DisableEvents bool `default:"false" usage:"disable events capturing"`
+	// RemoteLog defines the parameters to reach the remote logging server.
+	RemoteLog struct {
+		// ServerAddress defines the server address that will receive the logs.
+		ServerAddress string `default:"ressims.iota.cafe:5213" usage:"RemoteLog server address"`
+	} `name:"remotelog"`
 }
 
 // Parameters contains the configuration used by the remotelog plugin.
 var Parameters = ParametersDefinition{}
 
 func init() {
-	configuration.BindParameters(&Parameters, "remotelog")
+	configuration.BindParameters(&Parameters, "logger")
 }
