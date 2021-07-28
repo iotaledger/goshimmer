9d3be31370d42cc41b35e6504e25e75300cfaf8c
Piotr Macek <piotrm50@gmail.com>
2021-07-28 15:22:49 +0200

    Add new options to testing framework.

--- packages/tangle/testutils.go
+++ packages/tangle/testutils.go
@@ -70,12 +70,26 @@ func NewMessageTestFramework(tangle *Tangle, options ...MessageTestFrameworkOpti
 func (m *MessageTestFramework) CreateMessage(messageAlias string, messageOptions ...MessageOption) (message *Message) {
 	options := NewMessageTestFrameworkMessageOptions(messageOptions...)
 
-	if transaction := m.buildTransaction(options); transaction != nil {
-		m.messagesByAlias[messageAlias] = newTestParentsPayloadMessageIssuer(transaction, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer)
+	if options.reattachmentMessageAlias != "" {
+		reattachmentPayload := m.Message(options.reattachmentMessageAlias).Payload()
+		if options.issuingTime.IsZero() {
+			m.messagesByAlias[messageAlias] = newTestParentsPayloadMessageIssuer(reattachmentPayload, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer)
+		} else {
+			m.messagesByAlias[messageAlias] = newTestParentsPayloadMessageTimestampIssuer(reattachmentPayload, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer, options.issuingTime)
+		}
 	} else {
-		m.messagesByAlias[messageAlias] = newTestParentsDataMessageIssuer(messageAlias, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer)
-	}
+		transaction := m.buildTransaction(options)
 
+		if transaction != nil && options.issuingTime.IsZero() {
+			m.messagesByAlias[messageAlias] = newTestParentsPayloadMessageIssuer(transaction, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer)
+		} else if transaction != nil && !options.issuingTime.IsZero() {
+			m.messagesByAlias[messageAlias] = newTestParentsPayloadMessageTimestampIssuer(transaction, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer, options.issuingTime)
+		} else if options.issuingTime.IsZero() {
+			m.messagesByAlias[messageAlias] = newTestParentsDataMessageIssuer(messageAlias, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer)
+		} else {
+			m.messagesByAlias[messageAlias] = newTestParentsDataMessageTimestampIssuer(messageAlias, m.strongParentIDs(options), m.weakParentIDs(options), options.issuer, options.issuingTime)
+		}
+	}
 	RegisterMessageIDAlias(m.messagesByAlias[messageAlias].ID(), messageAlias)
 
 	return m.messagesByAlias[messageAlias]
@@ -394,12 +408,14 @@ func WithColoredGenesisOutput(alias string, balances map[ledgerstate.Color]uint6
 // MessageTestFrameworkMessageOptions is a struct that represents a collection of options that can be set when creating
 // a Message with the MessageTestFramework.
 type MessageTestFrameworkMessageOptions struct {
-	inputs         map[string]types.Empty
-	outputs        map[string]uint64
-	coloredOutputs map[string]map[ledgerstate.Color]uint64
-	strongParents  map[string]types.Empty
-	weakParents    map[string]types.Empty
-	issuer         ed25519.PublicKey
+	inputs                   map[string]types.Empty
+	outputs                  map[string]uint64
+	coloredOutputs           map[string]map[ledgerstate.Color]uint64
+	strongParents            map[string]types.Empty
+	weakParents              map[string]types.Empty
+	issuer                   ed25519.PublicKey
+	issuingTime              time.Time
+	reattachmentMessageAlias string
 }
 
 // NewMessageTestFrameworkMessageOptions is the constructor for the MessageTestFrameworkMessageOptions.
@@ -470,6 +486,20 @@ func WithIssuer(issuer ed25519.PublicKey) MessageOption {
 	}
 }
 
+// WithIssuingTime returns a MessageOption that is used to set issuing time of the Message.
+func WithIssuingTime(issuingTime time.Time) MessageOption {
+	return func(options *MessageTestFrameworkMessageOptions) {
+		options.issuingTime = issuingTime
+	}
+}
+
+// ReattachmentOf returns a MessageOption that is used to select payload of which Message should be reattached.
+func ReattachmentOf(messageAlias string) MessageOption {
+	return func(options *MessageTestFrameworkMessageOptions) {
+		options.reattachmentMessageAlias = messageAlias
+	}
+}
+
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // region Utility functions ////////////////////////////////////////////////////////////////////////////////////////////
@@ -500,6 +530,10 @@ func newTestParentsDataMessageIssuer(payloadString string, strongParents, weakPa
 	return NewMessage(strongParents, weakParents, nil, nil, time.Now(), issuer, nextSequenceNumber(), payload.NewGenericDataPayload([]byte(payloadString)), 0, ed25519.Signature{})
 }
 
+func newTestParentsDataMessageTimestampIssuer(payloadString string, strongParents, weakParents []MessageID, issuer ed25519.PublicKey, timestamp time.Time) *Message {
+	return NewMessage(strongParents, weakParents, nil, nil, timestamp, issuer, nextSequenceNumber(), payload.NewGenericDataPayload([]byte(payloadString)), 0, ed25519.Signature{})
+}
+
 func newTestParentsDataWithTimestamp(payloadString string, strongParents, weakParents []MessageID, timestamp time.Time) *Message {
 	return NewMessage(strongParents, weakParents, nil, nil, timestamp, ed25519.PublicKey{}, nextSequenceNumber(), payload.NewGenericDataPayload([]byte(payloadString)), 0, ed25519.Signature{})
 }
@@ -511,7 +545,9 @@ func newTestParentsPayloadMessage(p payload.Payload, strongParents, weakParents 
 func newTestParentsPayloadMessageIssuer(p payload.Payload, strongParents, weakParents []MessageID, issuer ed25519.PublicKey) *Message {
 	return NewMessage(strongParents, weakParents, nil, nil, time.Now(), issuer, nextSequenceNumber(), p, 0, ed25519.Signature{})
 }
-
+func newTestParentsPayloadMessageTimestampIssuer(p payload.Payload, strongParents, weakParents []MessageID, issuer ed25519.PublicKey, timestamp time.Time) *Message {
+	return NewMessage(strongParents, weakParents, nil, nil, timestamp, issuer, nextSequenceNumber(), p, 0, ed25519.Signature{})
+}
 func newTestParentsPayloadWithTimestamp(p payload.Payload, strongParents, weakParents []MessageID, timestamp time.Time) *Message {
 	return NewMessage(strongParents, weakParents, nil, nil, timestamp, ed25519.PublicKey{}, nextSequenceNumber(), p, 0, ed25519.Signature{})
 }
@@ -652,7 +688,7 @@ type SimpleMockOnTangleVoting struct {
 func (o *SimpleMockOnTangleVoting) Opinion(branchIDs ledgerstate.BranchIDs) (liked, disliked ledgerstate.BranchIDs, err error) {
 	liked = ledgerstate.BranchIDs{}
 	disliked = ledgerstate.BranchIDs{}
-	for branchID, _ := range branchIDs {
+	for branchID := range branchIDs {
 		if o.disliked.Contains(branchID) {
 			disliked.Add(branchID)
 		} else {
