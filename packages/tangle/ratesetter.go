1fcf8bce4e2573069686743bb1f6b9f6ce93134c
Acha Bill <achabill12@gmail.com>
2021-07-31 00:34:21 +0100

    fix goimports

--- packages/tangle/ratesetter.go
+++ packages/tangle/ratesetter.go
@@ -136,7 +136,7 @@ func (r *RateSetter) Size() int {
 	return r.issuingQueue.Size()
 }
 
-//Estimate estimates the issuing time of new message.
+// Estimate estimates the issuing time of new message.
 func (r *RateSetter) Estimate() time.Duration {
 	// TODO: https://github.com/iotaledger/goshimmer/issues/1483
 
