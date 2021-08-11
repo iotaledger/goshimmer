6a53a0e60046ad72871ab0316e6394510b302242
Luke Thorne <the.thorne48@gmail.com>
2021-08-11 15:10:56 -0400

    add comments

--- tools/docker-network/run.sh
+++ tools/docker-network/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Create a function to join an array by a character
+# Create a function to join an array of strings by a given character
 function join { local IFS="$1"; shift; echo "$*"; }
 
-if [[ $# -gt 4 ]] ; then
+# All parameters can be optional now, just make sure we don't have too many
+if [[ $# -gt 3 ]] ; then
     echo 'Call with ./run [replicas=1|2|3|...] [grafana=0|1] [drng=0|1]'
     exit 0
 fi
@@ -15,6 +16,7 @@ DRNG=${3:-0}
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 echo "Build GoShimmer"
+# Allow docker compose to build and cache an image
 docker-compose build
 
 # check exit code of builder
@@ -25,7 +27,10 @@ then
 fi
 
 echo "Run GoShimmer network"
+# SHIMMER_PEER_REPLICAS is used in docker-compose.yml to determine how many replicas to create
 export SHIMMER_PEER_REPLICAS=$REPLICAS
+# Profiles is created to set which docker profiles to run
+# https://docs.docker.com/compose/profiles/
 PROFILES=()
 if [ $GRAFANA -ne 0 ]
 then
