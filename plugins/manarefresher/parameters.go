dfefd134c8318b5333ed6d88a3a788e201020f39
apenzk <andreas.penzkofer@iota.org>
2021-08-11 14:37:31 +0100

    time.Duration manarefresher

--- plugins/manarefresher/parameters.go
+++ plugins/manarefresher/parameters.go
@@ -1,11 +1,15 @@
 package manarefresher
 
-import "github.com/iotaledger/hive.go/configuration"
+import (
+	"time"
+
+	"github.com/iotaledger/hive.go/configuration"
+)
 
 // ParametersDefinition contains the definition of the parameters used by the manarefresher plugin.
 type ParametersDefinition struct {
 	// RefreshInterval defines the interval for refreshing delegated mana.
-	RefreshInterval uint `default:"25" usage:"interval for refreshing delegated mana (minutes)"`
+	RefreshInterval time.Duration `default:"25m" usage:"interval for refreshing delegated mana (minutes)"`
 }
 
 // Parameters contains the configuration used by the manarefresher plugin.
