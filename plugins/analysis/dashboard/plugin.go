62de988c67ff74a2525d172196f43a8b4ac202f5
apenzk <andreas.penzkofer@iota.org>
2021-08-16 17:06:07 +0100

    bugfix with parameters

--- plugins/analysis/dashboard/plugin.go
+++ plugins/analysis/dashboard/plugin.go
@@ -13,7 +13,6 @@ import (
 	"github.com/labstack/echo/middleware"
 
 	"github.com/iotaledger/goshimmer/packages/shutdown"
-	"github.com/iotaledger/goshimmer/plugins/config"
 )
 
 // PluginName is the name of the dashboard plugin.
@@ -45,10 +44,10 @@ func configureServer() {
 	server.HidePort = true
 	server.Use(middleware.Recover())
 
-	if config.Node().Bool(CfgBasicAuthEnabled) {
+	if Parameters.CfgBasicAuthEnabled {
 		server.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-			if username == config.Node().String(CfgBasicAuthUsername) &&
-				password == config.Node().String(CfgBasicAuthPassword) {
+			if username == Parameters.CfgBasicAuthUsername &&
+				password == Parameters.CfgBasicAuthPassword {
 				return true, nil
 			}
 			return false, nil
@@ -74,7 +73,7 @@ func worker(shutdownSignal <-chan struct{}) {
 	defer log.Infof("Stopping %s ... done", PluginName)
 
 	stopped := make(chan struct{})
-	bindAddr := config.Node().String(CfgBindAddress)
+	bindAddr := Parameters.CfgBindAddress
 	go func() {
 		log.Infof("%s started, bind-address=%s", PluginName, bindAddr)
 		if err := server.Start(bindAddr); err != nil {
--- plugins/analysis/dashboard/ws.go
+++ plugins/analysis/dashboard/ws.go
@@ -9,7 +9,6 @@ import (
 	"github.com/labstack/echo"
 
 	analysisserver "github.com/iotaledger/goshimmer/plugins/analysis/server"
-	"github.com/iotaledger/goshimmer/plugins/config"
 	"github.com/iotaledger/goshimmer/plugins/dashboard"
 )
 
@@ -104,7 +103,7 @@ func websocketRoute(c echo.Context) error {
 	defer removeWsClient(clientID)
 
 	// send mana dashboard address info
-	manaDashboardHostAddress := config.Node().String(CfgManaDashboardAddress)
+	manaDashboardHostAddress := Parameters.CfgManaDashboardAddress
 	err = sendJSON(ws, &wsmsg{
 		Type: dashboard.MsgManaDashboardAddress,
 		Data: manaDashboardHostAddress,
