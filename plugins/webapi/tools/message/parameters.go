04a9aebf37bfcd2c5a9d985c68737ea8bb1fe482
apenzk <andreas.penzkofer@iota.org>
2021-08-16 14:57:40 +0100

    Revert "disovery. and webapi.tools.message"
    This reverts commit 0c153b2d0778ca136065b393613fbf6ebd7f3800.

--- plugins/webapi/tools/message/parameters.go
+++ plugins/webapi/tools/message/parameters.go
@@ -1,21 +1,12 @@
 package message
 
-import "github.com/iotaledger/hive.go/configuration"
+import flag "github.com/spf13/pflag"
 
 const (
 	// CfgExportPath the directory where exported files sit.
 	CfgExportPath = "webapi.exportPath"
 )
 
-// ParametersDefinition contains the definition of configuration parameters.
-type ParametersDefinition struct {
-	// Export path
-	CfgExportPath string `default:"." usage:"default export path"`
-}
-
-// Parameters contains the configuration parameters of the clock plugin.
-var Parameters = &ParametersDefinition{}
-
 func init() {
-	configuration.BindParameters(Parameters, "webapi.tools.message")
+	flag.String(CfgExportPath, ".", "default export path")
 }
