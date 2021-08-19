087a5f238f471c012891c6ba6ee9ec0110560373
apenzk <andreas.penzkofer@iota.org>
2021-08-19 17:09:54 +0100

    parameter update relay checker

--- tools/relay-checker/config.go
+++ tools/relay-checker/config.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"time"
-
-	"github.com/iotaledger/goshimmer/plugins/config"
 )
 
 var (
@@ -15,24 +13,24 @@ var (
 )
 
 func initConfig() {
-	if config.Node().String(CfgTargetNode) == "" {
+	if Parameters.TargetNode == "" {
 		panic("Set the target node address\n")
 	}
-	target = config.Node().String(CfgTargetNode)
+	target = Parameters.CfgTargetNode
 
-	if len(config.Node().Strings(CfgTestNodes)) == 0 {
+	if len(Parameters.CfgTestNodes) == 0 {
 		panic("Set node addresses\n")
 	}
-	nodes = append(nodes, config.Node().Strings(CfgTestNodes)...)
+	nodes = append(nodes, Parameters.CfgTestNodes...)
 
 	// optional settings
-	if config.Node().String(CfgData) != "" {
-		msgData = config.Node().String(CfgData)
+	if Parameters.CfgData != "" {
+		msgData = Parameters.CfgData
 	}
-	if config.Node().Int(CfgCooldownTime) > 0 {
-		cooldownTime = time.Duration(config.Node().Int(CfgCooldownTime)) * time.Second
+	if Parameters.CfgCooldownTime > 0 {
+		cooldownTime = time.Duration(Parameters.CfgCooldownTime) * time.Second
 	}
-	if config.Node().Int(CfgRepeat) > 0 {
-		repeat = config.Node().Int(CfgRepeat)
+	if Parameters.CfgRepeat > 0 {
+		repeat = Parameters.CfgRepeat
 	}
 }
