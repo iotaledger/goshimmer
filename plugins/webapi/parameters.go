8f86eced3f37f4d3d6a59706c47e49615154773f
apenzk <andreas.penzkofer@iota.org>
2021-08-20 09:06:04 +0100

    webAPI

--- plugins/webapi/parameters.go
+++ plugins/webapi/parameters.go
@@ -24,5 +24,5 @@ type ParametersDefinition struct {
 var Parameters = &ParametersDefinition{}
 
 func init() {
-	configuration.BindParameters(Parameters, "webapi")
+	configuration.BindParameters(Parameters, "webAPI")
 }
