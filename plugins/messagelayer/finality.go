244078b2dce0c8ce1e14a9ac5524dc08877e7b79
Hans Moog <3293976+hmoog@users.noreply.github.com>
2021-09-06 16:01:26 +0200

    Fix: fixed old code in plugin

--- plugins/messagelayer/finality.go
+++ plugins/messagelayer/finality.go
@@ -1,8 +1,6 @@
 package messagelayer
 
 import (
-	"sync"
-
 	"github.com/iotaledger/hive.go/events"
 	"github.com/iotaledger/hive.go/identity"
 
@@ -10,30 +8,24 @@ import (
 	"github.com/iotaledger/goshimmer/packages/tangle"
 )
 
-var finalityGadget finality.Gadget
-var finalityOnce sync.Once
-
 func FinalityGadget() finality.Gadget {
-	finalityOnce.Do(func() {
-		Tangle()
-	})
-	return finalityGadget
+	return Tangle().ConfirmationOracle.(finality.Gadget)
 }
 
 func configureFinality() {
 	Tangle().ApprovalWeightManager.Events.MarkerWeightChanged.Attach(events.NewClosure(func(e *tangle.MarkerWeightChangedEvent) {
-		if err := finalityGadget.HandleMarker(e.Marker, e.Weight); err != nil {
+		if err := FinalityGadget().HandleMarker(e.Marker, e.Weight); err != nil {
 			plugin.LogError(err)
 		}
 	}))
 	Tangle().ApprovalWeightManager.Events.BranchWeightChanged.Attach(events.NewClosure(func(e *tangle.BranchWeightChangedEvent) {
-		if err := finalityGadget.HandleBranch(e.BranchID, e.Weight); err != nil {
+		if err := FinalityGadget().HandleBranch(e.BranchID, e.Weight); err != nil {
 			plugin.LogError(err)
 		}
 	}))
 
 	// we need to update the WeightProvider on confirmation
-	finalityGadget.Events().MessageConfirmed.Attach(events.NewClosure(func(messageID tangle.MessageID) {
+	FinalityGadget().Events().MessageConfirmed.Attach(events.NewClosure(func(messageID tangle.MessageID) {
 		Tangle().Storage.Message(messageID).Consume(func(message *tangle.Message) {
 			Tangle().WeightProvider.Update(message.IssuingTime(), identity.NewID(message.IssuerPublicKey()))
 		})
