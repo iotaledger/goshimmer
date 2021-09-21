2d6df4c37fae8494664b79d795d553a7f07d2418
jonastheis <4181434+jonastheis@users.noreply.github.com>
2021-09-21 11:39:31 +0000

    Add unit test

--- packages/tangle/cmanaweightprovider_test.go
+++ packages/tangle/cmanaweightprovider_test.go
@@ -36,3 +36,80 @@ func TestActiveNodesMarshalling(t *testing.T) {
 		assert.EqualValues(t, a.Bytes(), activeNodes2[nodeID].Bytes())
 	}
 }
+
+func TestCManaWeightProvider(t *testing.T) {
+	nodes := map[string]identity.ID{
+		"1": identity.GenerateIdentity().ID(),
+		"2": identity.GenerateIdentity().ID(),
+		"3": identity.GenerateIdentity().ID(),
+	}
+
+	manaRetrieverFunc := func() map[identity.ID]float64 {
+		return map[identity.ID]float64{
+			nodes["1"]: 20,
+			nodes["2"]: 50,
+			nodes["3"]: 30,
+		}
+	}
+
+	tangleTime := time.Unix(DefaultGenesisTime, 0)
+	timeRetrieverFunc := func() time.Time { return tangleTime }
+
+	weightProvider := NewCManaWeightProvider(manaRetrieverFunc, timeRetrieverFunc)
+
+	// Add node1 as active in the genesis.
+	{
+		weightProvider.Update(tangleTime, nodes["1"])
+		assertWeightsOfRelevantSupporters(t, weightProvider, nodes, map[string]float64{
+			"1": 20,
+		})
+	}
+
+	// Add node2 and node3 activity at tangleTime+20 -> only node1 is active.
+	{
+		weightProvider.Update(tangleTime.Add(20*time.Minute), nodes["2"])
+		weightProvider.Update(tangleTime.Add(20*time.Minute), nodes["3"])
+		assertWeightsOfRelevantSupporters(t, weightProvider, nodes, map[string]float64{
+			"1": 20,
+		})
+	}
+
+	// Advance TangleTime by 25min -> all nodes are active.
+	{
+		tangleTime = tangleTime.Add(25 * time.Minute)
+		assertWeightsOfRelevantSupporters(t, weightProvider, nodes, map[string]float64{
+			"1": 20,
+			"2": 50,
+			"3": 30,
+		})
+	}
+
+	// Advance TangleTime by 10min -> node1 and node2 are active.
+	{
+		tangleTime = tangleTime.Add(25 * time.Minute)
+		assertWeightsOfRelevantSupporters(t, weightProvider, nodes, map[string]float64{
+			"2": 50,
+			"3": 30,
+		})
+	}
+
+	// Advance tangleTime by 25min -> no node is active anymore.
+	{
+		tangleTime = tangleTime.Add(25 * time.Minute)
+		assertWeightsOfRelevantSupporters(t, weightProvider, nodes, map[string]float64{})
+	}
+}
+
+func assertWeightsOfRelevantSupporters(t *testing.T, weightProvider WeightProvider, nodes map[string]identity.ID, expectedAlias map[string]float64) {
+	expected := make(map[identity.ID]float64)
+	var expectedTotalWeight float64
+
+	for alias, weight := range expectedAlias {
+		expected[nodes[alias]] = weight
+		expectedTotalWeight += weight
+	}
+
+	weights, totalWeight := weightProvider.WeightsOfRelevantSupporters()
+	assert.EqualValues(t, expected, weights)
+	assert.Equal(t, expectedTotalWeight, totalWeight)
+}
