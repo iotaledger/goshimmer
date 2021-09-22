29dd737b46e726804d783640604b2c95df730d84
jonastheis <4181434+jonastheis@users.noreply.github.com>
2021-09-22 09:10:52 +0000

    Set permissions of files in bind mount

--- tools/integration-tests/assets/entrypoint.sh
+++ tools/integration-tests/assets/entrypoint.sh
@@ -6,4 +6,11 @@ chmod 777 /assets/*
 echo "assets:"
 ls /assets
 echo "running tests..."
-go test ./tests/"${TEST_NAME}" -v -timeout 30m
+go test ./tests/"${TEST_NAME}" -v -timeout 10m
+
+# if running in CI we need to set right permissions on the Go folder (within container)
+echo "CURRENT_UID ${CURRENT_UID}"
+if [ ! -z ${CURRENT_UID} ]; then
+  echo "setting permissions on Go folder... '${CURRENT_UID}'"
+  chown -R "${CURRENT_UID}" /go /root/.cache/go-build
+fi
