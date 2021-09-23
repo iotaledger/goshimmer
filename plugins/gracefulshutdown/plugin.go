e63db0f2bb6fdc7ac4aed44fe944d42253c895f0
Luca Moser <moser.luca@gmail.com>
2021-09-23 11:29:51 +0200

    Adds peerdb mount in docker-compose.yml/docs + fixes graceful shutdown kill time format (#1711)
    * add peerdb to volumes
    
    * fixes format of remaining time until kill

--- plugins/gracefulshutdown/plugin.go
+++ plugins/gracefulshutdown/plugin.go
@@ -52,7 +52,7 @@ func configure(plugin *node.Plugin) {
 						sort.Strings(runningBackgroundWorkers)
 						processList = "(" + strings.Join(runningBackgroundWorkers, ", ") + ") "
 					}
-					plugin.LogWarnf("Received shutdown request - waiting (max %d seconds) to finish processing %s...", Parameters.WaitToKillTime-timeSinceStart, processList)
+					plugin.LogWarnf("Received shutdown request - waiting (max %v) to finish processing %s...", (Parameters.WaitToKillTime - timeSinceStart).Truncate(time.Second), processList)
 				} else {
 					plugin.LogError("Background processes did not terminate in time! Forcing shutdown ...")
 					pprof.Lookup("goroutine").WriteTo(os.Stdout, 2)
