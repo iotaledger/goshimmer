e8eca6e5740692c60cfcbf09edd7d7d56b3ae8a0
jkrvivian <jkrvivian@gmail.com>
2021-10-04 14:15:56 +0800

    feat: Implement UTXOStore

--- plugins/dagsvisualizer/frontend/src/index.tsx
+++ plugins/dagsvisualizer/frontend/src/index.tsx
@@ -2,11 +2,18 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Provider} from 'mobx-react';
 import TangleStore from 'stores/TangleStore';
+import UTXOStore from 'stores/UTXOStore';
 
 const tangleStore = new TangleStore();
+const utxoStore = new UTXOStore();
+
+const stores = {
+  "tangleStore": tangleStore,
+  "utxoStore": utxoStore,
+}
 
 ReactDOM.render(
-  <Provider tanglestore={tangleStore}>
+  <Provider {...stores}>
     <div>
       <h1>Hello</h1>
     </div>
---
+++ plugins/dagsvisualizer/frontend/src/stores/UTXOStore.tsx
@@ -0,0 +1,60 @@
+import { action, observable, ObservableMap } from 'mobx';
+import {connectWebSocket, registerHandler, unregisterHandler, WSMsgType} from 'WS';
+
+export class utxoVertex {
+    msgID:          string;   
+	ID:             string;
+	inputs:         Array<string>;
+    outputs:        Array<string>;
+	approvalWeight: number;
+	confirmedTime:  number;
+}
+
+export class utxoConfirmed {
+    ID: string;
+    approvalWeight: number;
+    confirmedTime: number;
+}
+
+
+export class UTXOStore {
+    @observable maxUTXOVertices: number = 500;
+    @observable transactions = new ObservableMap<string, utxoVertex>();
+    txOrder: Array<any> = [];
+
+    constructor() {        
+        registerHandler(WSMsgType.Transaction, this.addTransaction);
+        registerHandler(WSMsgType.TransactionConfirmed, this.setTXConfirmedTime);
+    }
+
+    unregisterHandlers() {
+        unregisterHandler(WSMsgType.Transaction);
+        unregisterHandler(WSMsgType.TransactionConfirmed);
+    }
+
+    @action
+    addTransaction = (tx: utxoVertex) => {
+        if (this.txOrder.length >= this.maxUTXOVertices) {
+            let removed = this.txOrder.shift();
+            this.transactions.delete(removed);
+        }
+        console.log(tx.ID)
+
+        this.txOrder.push(tx.ID);
+        this.transactions.set(tx.ID, tx);
+    }
+
+    @action
+    setTXConfirmedTime = (txConfirmed: utxoConfirmed) => {
+        let tx = this.transactions.get(txConfirmed.ID);
+        if (!tx) {
+            return;
+        }
+
+        tx.confirmedTime = txConfirmed.confirmedTime;
+        tx.approvalWeight = txConfirmed.approvalWeight;
+        this.transactions.set(txConfirmed.ID, tx);
+    }
+}
+
+export default UTXOStore;
