feded23187e925c4fe652708b89d8f6db7f92931
Andrea Villa <1577639+karimodm@users.noreply.github.com>
2021-10-19 15:20:00 +0200

    Please doggo

--- packages/tangle/approvalweightmanager.go
+++ packages/tangle/approvalweightmanager.go
@@ -1063,9 +1063,8 @@ func (b *BranchSupporters) AddSupporter(supporter Supporter) (added bool) {
 	return
 }
 
-// AddSupporter adds a new Supporter to the tracked ledgerstate.BranchID.
+// AddSupporters adds the supporters set to the tracked ledgerstate.BranchID.
 func (b *BranchSupporters) AddSupporters(supporters *Supporters) (added bool) {
-
 	modified := false
 	supporters.ForEach(func(supporter Supporter) {
 		if added = b.supporters.Add(supporter); added {
