7d9d85296d24e90d071f3edd7a7c083152f025a7
Hans Moog <3293976+hmoog@users.noreply.github.com>
2021-10-23 18:56:07 +0200

    Fix: fix broken unit test

--- packages/tangle/tipmanager_test.go
+++ packages/tangle/tipmanager_test.go
@@ -388,6 +388,14 @@ func TestTipManager_TransactionTips(t *testing.T) {
 
 	// Message 6
 	{
+		// with the invalidity check in the Booker, the tests would now fail as the parents are invalid
+		tangle.Storage.MessageMetadata(messages["3"].ID()).Consume(func(messageMetadata *MessageMetadata) {
+			messageMetadata.SetInvalid(false)
+		})
+		tangle.Storage.MessageMetadata(messages["5"].ID()).Consume(func(messageMetadata *MessageMetadata) {
+			messageMetadata.SetInvalid(false)
+		})
+
 		createScenarioMessageWith1Input1Output("6", "5", "3", "H", "Q", []MessageID{messages["3"].ID(), messages["5"].ID()})
 		tipManager.AddTip(messages["6"])
 		assert.Equal(t, 2, tipManager.TipCount())
