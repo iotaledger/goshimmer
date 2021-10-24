2710c4ec479caa638192ff3c5e66fd95dca1b791
Hans Moog <3293976+hmoog@users.noreply.github.com>
2021-10-24 18:25:31 +0200

    Feat: made merging thread safe

--- packages/ledgerstate/ledgerstate.go
+++ packages/ledgerstate/ledgerstate.go
@@ -49,14 +49,47 @@ func (l *Ledgerstate) Configure(options ...Option) {
 	}
 }
 
-// MergeToMaster merges a confirmed Branch back into the MasterBranch.
+// StoreTransaction adds a new Transaction to the ledger state. It returns a boolean that indicates whether the
+// Transaction was stored, its SolidityType and an error value that contains the cause for possibly exceptions.
+func (l *Ledgerstate) StoreTransaction(transaction *Transaction) (stored bool, solidityType SolidityType, err error) {
+	l.RLock()
+	defer l.RUnlock()
+
+	return l.UTXODAG.StoreTransaction(transaction)
+}
+
+// MergeToMaster merges a Branch (and all of its ancestors) back into the MasterBranch. It reorganizes the BranchDAG on
+// top of this Branch and reassigns the related transactions to these new Branches.
 func (l *Ledgerstate) MergeToMaster(branchID BranchID) (err error) {
 	l.Lock()
 	defer l.Unlock()
 
+	branchesToMergeInOrder, err := l.branchesToMergeInOrder(branchID)
+	if err != nil {
+		return errors.Errorf("failed to determine Branches to merge in order: %w", err)
+	}
+
+	for _, currentBranchID := range branchesToMergeInOrder {
+		if err = l.mergeSingleBranchToMaster(currentBranchID); err != nil {
+			return errors.Errorf("failed to merge %s to MasterBranch: %w", currentBranchID, err)
+		}
+	}
+
+	return nil
+}
+
+// Shutdown marks the Ledgerstate as stopped, so it will not accept any new Transactions.
+func (l *Ledgerstate) Shutdown() {
+	l.BranchDAG.Shutdown()
+	l.UTXODAG.Shutdown()
+}
+
+// branchesToMergeInOrder returns the given Branch and all of its ancestors in their correct order for merging (leafs
+// last).
+func (l *Ledgerstate) branchesToMergeInOrder(branchID BranchID) (branchesInMergeOrder []BranchID, err error) {
 	conflictBranchIDs, err := l.BranchDAG.ResolveConflictBranchIDs(NewBranchIDs(branchID))
 	if err != nil {
-		return errors.Errorf("failed to resolve ConflictBranchIDs of Branch with %s: %w", branchID, err)
+		return nil, errors.Errorf("failed to resolve ConflictBranchIDs of Branch with %s: %w", branchID, err)
 	}
 
 	branchWalker := walker.New(false)
@@ -64,10 +97,9 @@ func (l *Ledgerstate) MergeToMaster(branchID BranchID) (err error) {
 		branchWalker.Push(conflictBranchID)
 	}
 
-	mergeStack := make([]BranchID, 0)
 	for branchWalker.HasNext() {
 		currentBranchID := branchWalker.Next().(BranchID)
-		mergeStack = append(mergeStack, currentBranchID)
+		branchesInMergeOrder = append(branchesInMergeOrder, currentBranchID)
 
 		l.BranchDAG.Branch(currentBranchID).Consume(func(branch Branch) {
 			for parentBranchID := range branch.Parents() {
@@ -80,55 +112,57 @@ func (l *Ledgerstate) MergeToMaster(branchID BranchID) (err error) {
 		})
 	}
 
-	for i := len(mergeStack) - 1; i >= 0; i-- {
-		currentBranchID := mergeStack[i]
-
-		updatedBranches, err := l.BranchDAG.MergeToMaster(currentBranchID)
-		if err != nil {
-			return errors.Errorf("failed to merge Branch with %s to %s: %w", currentBranchID, MasterBranchID, err)
-		}
-
-		transactionWalker := walker.New(false)
-		transactionWalker.Push(currentBranchID.TransactionID())
-
-		for transactionWalker.HasNext() {
-			currentTransactionID := transactionWalker.Next().(TransactionID)
-
-			l.UTXODAG.CachedTransactionMetadata(currentTransactionID).Consume(func(transactionMetadata *TransactionMetadata) {
-				currentBranchID := transactionMetadata.BranchID()
-				if currentBranchID == InvalidBranchID {
-					return
-				}
-
-				updatedBranchID, exists := updatedBranches[currentBranchID]
-				if !exists {
-					return
-				}
-
-				transactionMetadata.SetBranchID(updatedBranchID)
-
-				l.UTXODAG.CachedTransaction(currentTransactionID).Consume(func(transaction *Transaction) {
-					for _, output := range transaction.Essence().Outputs() {
-						l.UTXODAG.CachedOutputMetadata(output.ID()).Consume(func(outputMetadata *OutputMetadata) {
-							outputMetadata.SetBranchID(updatedBranchID)
-						})
-
-						l.UTXODAG.CachedConsumers(output.ID(), Solid).Consume(func(consumer *Consumer) {
-							transactionWalker.Push(consumer.TransactionID())
-						})
-					}
-				})
-			})
-		}
+	for i := len(branchesInMergeOrder)/2 - 1; i >= 0; i-- {
+		opp := len(branchesInMergeOrder) - 1 - i
+		branchesInMergeOrder[i], branchesInMergeOrder[opp] = branchesInMergeOrder[opp], branchesInMergeOrder[i]
 	}
 
-	return nil
+	return
 }
 
-// Shutdown marks the Ledgerstate as stopped, so it will not accept any new Transactions.
-func (l *Ledgerstate) Shutdown() {
-	l.BranchDAG.Shutdown()
-	l.UTXODAG.Shutdown()
+// mergeSingleBranchToMaster merges a single Branch that is at the bottom of the BranchDAG (a child of the root) into
+// the MasterBranch. It reorganizes the BranchDAG on top of this Branch and reassigns the related transactions to these
+// new Branches.
+func (l *Ledgerstate) mergeSingleBranchToMaster(branchID BranchID) (err error) {
+	updatedBranches, err := l.BranchDAG.MergeToMaster(branchID)
+	if err != nil {
+		return errors.Errorf("failed to merge Branch with %s to %s: %w", branchID, MasterBranchID, err)
+	}
+
+	transactionWalker := walker.New(false)
+	transactionWalker.Push(branchID.TransactionID())
+
+	for transactionWalker.HasNext() {
+		currentTransactionID := transactionWalker.Next().(TransactionID)
+
+		l.UTXODAG.CachedTransactionMetadata(currentTransactionID).Consume(func(transactionMetadata *TransactionMetadata) {
+			currentBranchID := transactionMetadata.BranchID()
+			if currentBranchID == InvalidBranchID {
+				return
+			}
+
+			updatedBranchID, exists := updatedBranches[currentBranchID]
+			if !exists {
+				return
+			}
+
+			transactionMetadata.SetBranchID(updatedBranchID)
+
+			l.UTXODAG.CachedTransaction(currentTransactionID).Consume(func(transaction *Transaction) {
+				for _, output := range transaction.Essence().Outputs() {
+					l.UTXODAG.CachedOutputMetadata(output.ID()).Consume(func(outputMetadata *OutputMetadata) {
+						outputMetadata.SetBranchID(updatedBranchID)
+					})
+
+					l.UTXODAG.CachedConsumers(output.ID(), Solid).Consume(func(consumer *Consumer) {
+						transactionWalker.Push(consumer.TransactionID())
+					})
+				}
+			})
+		})
+	}
+
+	return
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
