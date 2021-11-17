73d5eceea61f9f42b00b1e27e3c3072d4bfb213f
Piotr Macek <piotr.macek@iota.org>
2021-11-17 14:32:18 +0100

    Fix integration test.

--- plugins/messagelayer/mana_plugin.go
+++ plugins/messagelayer/mana_plugin.go
@@ -409,15 +409,20 @@ func GetOnlineNodes(manaType mana.Type) (onlineNodesMana []mana.Node, t time.Tim
 	knownPeers = append(knownPeers, deps.Local.Peer)
 	onlineNodesMana = make([]mana.Node, 0)
 	for _, peer := range knownPeers {
-		if baseManaVectors[manaType].Has(peer.ID()) {
-			var peerMana float64
-			peerMana, t, err = baseManaVectors[manaType].GetMana(peer.ID())
-			if err != nil {
-				return nil, t, err
-			}
-			if peerMana > 0 {
-				onlineNodesMana = append(onlineNodesMana, mana.Node{ID: peer.ID(), Mana: peerMana})
-			}
+		if !baseManaVectors[manaType].Has(peer.ID()) {
+			continue
+		}
+		fmt.Println("Has mana", manaType.String(), peer.ID().String())
+		var peerMana float64
+		peerMana, t, err = baseManaVectors[manaType].GetMana(peer.ID())
+		if manaType == mana.AccessMana && peerMana < tangle.MinMana && err == nil {
+			peerMana = tangle.MinMana
+		}
+		if err != nil {
+			return nil, t, err
+		}
+		if peerMana > 0 {
+			onlineNodesMana = append(onlineNodesMana, mana.Node{ID: peer.ID(), Mana: peerMana})
 		}
 	}
 	sort.Slice(onlineNodesMana, func(i, j int) bool {
