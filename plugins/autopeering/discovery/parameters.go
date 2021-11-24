1f8b0af68765e4434559731dd83fb1eaacd8261c
NO8ODY <steffen@emayl.me>
2021-11-24 23:02:12 +0100

    rename existing and add new entry node

--- plugins/autopeering/discovery/parameters.go
+++ plugins/autopeering/discovery/parameters.go
@@ -8,7 +8,7 @@ type ParametersDefinitionDiscovery struct {
 	NetworkVersion uint32 `default:"42" usage:"autopeering network version"`
 
 	// EntryNodes defines the config flag of the entry nodes.
-	EntryNodes []string `default:"2PV5487xMw5rasGBXXWeqSi4hLz7r19YBt8Y1TGAsQbj@analysisentry-01.devnet.shimmer.iota.cafe:15626,5EDH4uY78EA6wrBkHHAVBWBMDt7EcksRq6pjzipoW15B@entryshimmer.tanglebay.com:14646" usage:"list of trusted entry nodes for auto peering"`
+	EntryNodes []string `default:"2PV5487xMw5rasGBXXWeqSi4hLz7r19YBt8Y1TGAsQbj@analysisentry-01.devnet.shimmer.iota.cafe:15626,5EDH4uY78EA6wrBkHHAVBWBMDt7EcksRq6pjzipoW15B@entry-0.devnet.tanglebay.com:14646,CAB87iQZR6BjBrCgEBupQJ4gpEBgvGKKv3uuGVRBKb4n@entry-1.devnet.tanglebay.com:14646" usage:"list of trusted entry nodes for auto peering"`
 }
 
 // Parameters contains the configuration parameters of the autopeering peer discovery.
