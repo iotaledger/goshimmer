c2b4930db17da392eef8187f7213842905b28488
Andrea V <1577639+karimodm@users.noreply.github.com>
2022-01-12 18:39:17 +0100

    Versions bump (#1954)
    * Versions bump
    
    * CHANGELOG

--- plugins/banner/plugin.go
+++ plugins/banner/plugin.go
@@ -15,7 +15,7 @@ var (
 	Plugin = node.NewPlugin(PluginName, nil, node.Enabled, configure, run)
 
 	// AppVersion version number
-	AppVersion = "v0.8.3"
+	AppVersion = "v0.8.4"
 	// SimplifiedAppVersion is the version number without commit hash
 	SimplifiedAppVersion = simplifiedVersion(AppVersion)
 )
