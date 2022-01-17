c6416e2f2d6040c75aeb3bfd77b90a10b35ae803
Acha Bill <57879913+acha-bill@users.noreply.github.com>
2022-01-17 12:15:42 +0100

    [DAGS vizualizer] send initial data on new ws connection (#1952)
    * send initial data on new ws connection
    
    * review
    
    * review

--- plugins/dagsvisualizer/ws.go
+++ plugins/dagsvisualizer/ws.go
@@ -105,6 +105,8 @@ func websocketRoute(c echo.Context) error {
 	clientID, wsClient := registerWSClient()
 	defer removeWsClient(clientID)
 
+	sendInitialData(ws)
+
 	for {
 		msg := <-wsClient.channel
 		if err := ws.SetWriteDeadline(time.Now().Add(webSocketWriteTimeout)); err != nil {
@@ -115,4 +117,22 @@ func websocketRoute(c echo.Context) error {
 		}
 	}
 	return nil
+}
+
+func sendInitialData(ws *websocket.Conn) {
+	bufferMutex.RLock()
+	defer bufferMutex.RUnlock()
+	for _, msg := range buffer {
+		if err := sendJSON(ws, msg); err != nil {
+			log.Errorf("failed to send DAG message to client: %s", err.Error())
+		}
+	}
+}
+
+func sendJSON(ws *websocket.Conn, msg *wsMessage) error {
+	var err error
+	if err = ws.SetWriteDeadline(time.Now().Add(webSocketWriteTimeout)); err == nil {
+		err = ws.WriteJSON(msg)
+	}
+	return err
 }
