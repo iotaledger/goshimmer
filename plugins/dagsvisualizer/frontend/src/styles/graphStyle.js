aa89c4a891f8264fc046e98e77dc45181067d92d
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2022-02-08 09:24:17 +0100

    DAGs Vis: improvements (#2004)
    * Stop animation, increase rank separation
    
    * Add sync buttons for each dag

--- plugins/dagsvisualizer/frontend/src/styles/graphStyle.js
+++ plugins/dagsvisualizer/frontend/src/styles/graphStyle.js
@@ -62,7 +62,7 @@ export var dagreOptions = {
     // dagre algo options, uses default value on undefined
     nodeSep: undefined, // the separation between adjacent nodes in the same rank
     edgeSep: undefined, // the separation between adjacent edges in the same rank
-    rankSep: undefined, // the separation between each rank in the layout
+    rankSep: 200, // the separation between each rank in the layout
     rankDir: 'LR', // 'TB' for top to bottom flow, 'LR' for left to right,
     ranker: undefined, // Type of algorithm to assign a rank to each node in the input graph. Possible values: 'network-simplex', 'tight-tree' or 'longest-path'
     minLen: function () {
@@ -77,7 +77,7 @@ export var dagreOptions = {
     padding: 30, // fit padding
     spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
     nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
-    animate: true, // whether to transition the node positions
+    animate: false, // whether to transition the node positions
     animationDuration: 500, // duration of animation in ms if enabled
     animationEasing: undefined, // easing of animation if enabled
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
