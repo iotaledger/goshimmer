010678df7d5bdff36213066139d0a09ac477365e
jkrvivian <jkrvivian@gmail.com>
2022-03-01 23:35:57 +0800

    Add wallet utility functions

--- client/evilwallet/wallets.go
+++ client/evilwallet/wallets.go
@@ -3,6 +3,7 @@ package evilwallet
 import (
 	"sync"
 
+	"github.com/iotaledger/goshimmer/client/wallet/packages/address"
 	"github.com/iotaledger/goshimmer/client/wallet/packages/seed"
 	"github.com/iotaledger/hive.go/types"
 	"go.uber.org/atomic"
@@ -23,38 +24,22 @@ const (
 type Wallets struct {
 	wallets map[WalletType][]*Wallet
 
-	idCounter    atomic.Int64
 	lastWalletID atomic.Int64
 }
 
 func NewWallets() *Wallets {
 	return &Wallets{
 		wallets:      make(map[WalletType][]*Wallet),
-		idCounter:    *atomic.NewInt64(0),
 		lastWalletID: *atomic.NewInt64(0),
 	}
 }
 
-type Wallet struct {
-	ID         int
-	walletType WalletType
-
-	unspentOutputs    map[string]*Output // maps addr to its unspentOutput
-	indexAddrMap      map[uint64]string
-	inputTransactions map[string]types.Empty
-	seed              *seed.Seed
-
-	lastAddrIdxUsed atomic.Int64 // used during filling in wallet with new outputs
-	lastAddrSpent   atomic.Int64 // used during spamming with outputs one by one
-	spent           bool
-	*sync.RWMutex
-}
-
-func NewWallet(id int) *Wallet {
+func (w *Wallets) NewWallet(wType WalletType) *Wallet {
 	idxSpent := atomic.NewInt64(-1)
 	addrUsed := atomic.NewInt64(-1)
-	return &Wallet{
-		ID:                id,
+	wallet := &Wallet{
+		ID:                int(w.lastWalletID.Load()),
+		walletType:        wType,
 		seed:              seed.NewSeed(),
 		unspentOutputs:    make(map[string]*Output),
 		indexAddrMap:      make(map[uint64]string),
@@ -63,10 +48,30 @@ func NewWallet(id int) *Wallet {
 		lastAddrIdxUsed:   *addrUsed,
 		RWMutex:           &sync.RWMutex{},
 	}
+
+	w.lastWalletID.Add(1)
+	w.addWallet(wallet)
+
+	return wallet
+}
+
+func (w *Wallets) GetWallet(wType WalletType) (wallet *Wallet) {
+	return w.wallets[wType][0]
+}
+
+func (w *Wallets) GetWallets(wType WalletType, num int) (wallets []*Wallet) {
+	allWallets := w.wallets[wType]
+	if num > len(allWallets) {
+		return allWallets
+	}
+
+	for i := 0; i < num; i++ {
+		wallets = append(wallets, allWallets[i])
+	}
+	return wallets
 }
 
 // CreateNFreshFaucet10kWallet creates n new wallets, each wallet is created from one faucet request.
-
 func (w *Wallets) CreateNFreshFaucet10kWallet(numberOf10kWallets int) {
 	// channel to block the number of concurrent goroutines
 	semaphore := make(chan bool, maxGoroutines)
@@ -116,4 +121,28 @@ func requestAndSplitFaucetFunds() (wallet *Wallet, err error) {
 // addWallet stores newly created wallet.
 func (w *Wallets) addWallet(wallet *Wallet) {
 	w.wallets[wallet.walletType] = append(w.wallets[wallet.walletType], wallet)
+}
+
+type Wallet struct {
+	ID         int
+	walletType WalletType
+
+	unspentOutputs    map[string]*Output // maps addr to its unspentOutput
+	indexAddrMap      map[uint64]string
+	inputTransactions map[string]types.Empty
+	seed              *seed.Seed
+
+	lastAddrIdxUsed atomic.Int64 // used during filling in wallet with new outputs
+	lastAddrSpent   atomic.Int64 // used during spamming with outputs one by one
+	spent           bool
+	*sync.RWMutex
+}
+
+func (w *Wallet) Address() address.Address {
+	index := uint64(w.lastAddrIdxUsed.Load())
+	addr := w.seed.Address(index)
+	w.indexAddrMap[index] = addr.Base58()
+	w.lastAddrIdxUsed.Add(1)
+
+	return addr
 }
