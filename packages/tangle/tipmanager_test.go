f94032f2415de98d2156694c1741175f6f1716ff
Piotr Macek <piotr.macek@iota.org>
2022-03-07 12:00:06 +0100

    Turn off linter (dupl). Again

--- packages/tangle/tipmanager_test.go
+++ packages/tangle/tipmanager_test.go
@@ -652,8 +652,7 @@ func createTestTangleTSC(t *testing.T, testFramework *MessageTestFramework) {
 		})
 	}
 	// SEQUENCE 1
-	{
-		//nolint:dupl
+	{ //nolint:dupl
 		testFramework.PreventNewMarkers(true)
 		lastMsgAlias = issueMessages(testFramework, "0/1-preTSCSeq1", 3, []string{"Marker-0/1"}, time.Minute*6)
 		lastMsgAlias = issueMessages(testFramework, "0/1-postTSCSeq1", 6, []string{lastMsgAlias}, time.Minute*4)
@@ -695,8 +694,7 @@ func createTestTangleTSC(t *testing.T, testFramework *MessageTestFramework) {
 	}
 
 	// SEQUENCE 2
-	{
-		//nolint:dupl
+	{ //nolint:dupl
 		testFramework.PreventNewMarkers(true)
 		lastMsgAlias = issueMessages(testFramework, "0/1-preTSCSeq2", 3, []string{"Marker-0/1"}, time.Minute*6)
 		lastMsgAlias = issueMessages(testFramework, "0/1-postTSCSeq2", 6, []string{lastMsgAlias}, time.Minute*4)
