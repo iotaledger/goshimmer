3a725f4027221a63d0aca570e3c9abee9226df27
jkrvivian <jkrvivian@gmail.com>
2022-03-31 15:47:12 +0800

    Add NSpendBatch to scenario

--- client/evilwallet/customscenarios.go
+++ client/evilwallet/customscenarios.go
@@ -24,6 +24,22 @@ func DoubleSpendBatch(spentNum int) EvilBatch {
 	return conflictSlice
 }
 
+func NSpendBatch(nSpent int) EvilBatch {
+	conflictSlice := make(EvilBatch, 0)
+	scenarioAlias := make([]ScenarioAlias, 0)
+	inputStartNum := nSpent + 1
+
+	for i := 1; i <= nSpent; i++ {
+		scenarioAlias = append(scenarioAlias,
+			ScenarioAlias{
+				Inputs:  []string{strconv.Itoa(inputStartNum)},
+				Outputs: []string{strconv.Itoa(i)}},
+		)
+	}
+	conflictSlice = append(conflictSlice, scenarioAlias)
+	return conflictSlice
+}
+
 func Scenario1() EvilBatch {
 	return EvilBatch{
 		[]ScenarioAlias{
