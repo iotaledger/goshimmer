bfda5bb373cc082e57fe6f73b1854e985bfe52de
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-04-06 01:02:43 +0200

    Refactor: started to comment utils

--- packages/ledger/branchdag/utils.go
+++ packages/ledger/branchdag/utils.go
@@ -6,17 +6,19 @@ import (
 	"github.com/iotaledger/hive.go/generics/walker"
 )
 
+// Utils is a BranchDAG component that bundles utility related API to simplify common interactions with the BranchDAG.
 type Utils struct {
 	branchDAG *BranchDAG
 }
 
+// newUtils returns a new Utils instance for the given BranchDAG.
 func newUtils(branchDAG *BranchDAG) (new *Utils) {
 	return &Utils{
 		branchDAG: branchDAG,
 	}
 }
 
-// ForEachBranch iterates over all the branches and executes consumer.
+// ForEachBranch iterates over every existing Branch in the entire Storage.
 func (u *Utils) ForEachBranch(consumer func(branch *Branch)) {
 	u.branchDAG.Storage.branchStorage.ForEach(func(key []byte, cachedObject *objectstorage.CachedObject[*Branch]) bool {
 		cachedObject.Consume(func(branch *Branch) {
@@ -27,33 +29,15 @@ func (u *Utils) ForEachBranch(consumer func(branch *Branch)) {
 	})
 }
 
-// ForEachConflictingBranchID executes the callback for each Branch that is conflicting with the Branch
-// identified by the given BranchID.
+// ForEachConflictingBranchID executes the callback for each Branch that is conflicting with the named Branch.
 func (u *Utils) ForEachConflictingBranchID(branchID BranchID, callback func(conflictingBranchID BranchID) bool) {
 	u.branchDAG.Storage.CachedBranch(branchID).Consume(func(branch *Branch) {
 		u.forEachConflictingBranchID(branch, callback)
 	})
 }
 
-// ForEachConflictingBranchID executes the callback for each Branch that is conflicting with the Branch
-// identified by the given BranchID.
-func (u *Utils) forEachConflictingBranchID(branch *Branch, callback func(conflictingBranchID BranchID) bool) {
-	for it := branch.Conflicts().Iterator(); it.HasNext(); {
-		abort := false
-		u.branchDAG.Storage.CachedConflictMembers(it.Next()).Consume(func(conflictMember *ConflictMember) {
-			if abort || conflictMember.BranchID() == branch.ID() {
-				return
-			}
-
-			if abort = !callback(conflictMember.BranchID()); abort {
-				it.StopWalk()
-			}
-		})
-	}
-}
-
-// ForEachConnectedConflictingBranchID executes the callback for each Branch that is connected through a chain
-// of intersecting ConflictSets.
+// ForEachConnectedConflictingBranchID executes the callback for each Branch that is directly or indirectly connected to
+// the named Branch through a chain of intersecting conflicts.
 func (u *Utils) ForEachConnectedConflictingBranchID(branchID BranchID, callback func(conflictingBranchID BranchID)) {
 	traversedBranches := set.New[BranchID]()
 	conflictSetsWalker := walker.New[ConflictID]()
@@ -79,7 +63,21 @@ func (u *Utils) ForEachConnectedConflictingBranchID(branchID BranchID, callback 
 		})
 	}
 
-	traversedBranches.ForEach(func(element BranchID) {
-		callback(element)
-	})
+	traversedBranches.ForEach(callback)
+}
+
+// forEachConflictingBranchID executes the callback for each Branch that is conflicting with the named Branch.
+func (u *Utils) forEachConflictingBranchID(branch *Branch, callback func(conflictingBranchID BranchID) bool) {
+	for it := branch.Conflicts().Iterator(); it.HasNext(); {
+		abort := false
+		u.branchDAG.Storage.CachedConflictMembers(it.Next()).Consume(func(conflictMember *ConflictMember) {
+			if abort || conflictMember.BranchID() == branch.ID() {
+				return
+			}
+
+			if abort = !callback(conflictMember.BranchID()); abort {
+				it.StopWalk()
+			}
+		})
+	}
 }
