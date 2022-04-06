26892f2fccc172bf16ad28d61e6d6f674c116565
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2022-04-06 10:13:29 +0200

    Quick test

--- tools/evil-spammer/quick-test.go
+++ tools/evil-spammer/quick-test.go
@@ -1,11 +1,9 @@
 package main
 
 import (
+	"github.com/iotaledger/goshimmer/client/evilspammer"
+	"github.com/iotaledger/goshimmer/client/evilwallet"
 	"time"
-
-	"github.com/iotaledger/goshimmer-tools/internal-tools/standardized-tests/tools"
-
-	"github.com/iotaledger/goshimmer-tools/internal-tools/standardized-tests/spammers"
 )
 
 type QuickTestParams struct {
@@ -18,64 +16,48 @@ type QuickTestParams struct {
 }
 
 // QuickTest runs short spamming periods with stable mps
-func QuickTest(params *QuickTestParams) *spammers.Wallets {
-	clients := spammers.NewClientsFromURL(params.ClientUrls, "")
-	counter := spammers.NewErrorCount()
+func QuickTest(params *QuickTestParams) {
+	evilWallet := evilwallet.NewEvilWallet(params.ClientUrls...)
+	counter := evilspammer.NewErrorCount()
 	log.Info("Starting quick test")
 
-	numOfOut := spammers.TotalOutputsNeeded(params.Duration, []int{params.Rate, params.Rate}, params.TimeUnit) // double spent will need two times fewer outputs
+	nWallets := 2 * evilspammer.BigWalletsNeeded(params.Rate, params.TimeUnit, params.Duration)
 
 	log.Info("Start preparing funds")
-	spamWallet, err := spammers.PrepareFunds(clients, numOfOut, 5, counter)
-	if err != nil {
-		log.Error(err)
-		return nil
-	}
-	outWallets := spammers.NewWallets()
+	evilWallet.RequestFreshBigFaucetWallets(nWallets)
 
 	// define spammers
-	baseOptions := []spammers.Options{
-		spammers.WithSpamDetails(params.Rate, params.TimeUnit, params.Duration, 0),
-		spammers.WithClients(clients),
-		spammers.WithErrorCounter(counter),
+	baseOptions := []evilspammer.Options{
+		evilspammer.WithSpamRate(params.Rate, params.TimeUnit),
+		evilspammer.WithSpamDuration(params.Duration),
+		evilspammer.WithErrorCounter(counter),
+		evilspammer.WithSpamWallet(evilWallet),
 	}
 	msgOptions := append(baseOptions,
-		spammers.WithSpammingFunc(spammers.MessageSpammingFunc, false),
+		evilspammer.WithSpammingFunc(evilspammer.DataSpammingFunction),
 	)
-	txOptions := append(baseOptions, []spammers.Options{
-		spammers.WithOutputWallets(outWallets),
-		spammers.WithSpamWallet(spamWallet),
-		spammers.WithSpammingFunc(spammers.TransactionSpammingFunc, true),
-	}...)
-	dsOptions := append(baseOptions, []spammers.Options{
-		spammers.WithOutputWallets(outWallets),
-		spammers.WithSpamWallet(spamWallet),
-		spammers.WithTimeDelayForDoubleSpend(params.DelayBetweenConflicts),
-		spammers.WithSpammingFunc(spammers.DoubleSpendSpammingFunc, true),
-	}...)
 
-	msgSpammer := spammers.NewSpammer(msgOptions...)
-	txSpammer := spammers.NewSpammer(txOptions...)
-	dsSpammer := spammers.NewSpammer(dsOptions...)
+	dsScenario := evilwallet.NewEvilScenario(
+		evilwallet.WithScenarioCustomConflicts(evilwallet.DoubleSpendBatch(2)),
+	)
+
+	dsOptions := append(baseOptions,
+		evilspammer.WithEvilScenario(dsScenario),
+	)
+
+	msgSpammer := evilspammer.NewSpammer(msgOptions...)
+	txSpammer := evilspammer.NewSpammer(baseOptions...)
+	dsSpammer := evilspammer.NewSpammer(dsOptions...)
 
 	// start test
 	txSpammer.Spam()
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
 
 	msgSpammer.Spam()
-	awaitTipPoolSizeIsSmallEnough(clients.GetClients(1)[0], 2*time.Minute, 100)
+	time.Sleep(5 * time.Second)
 
 	dsSpammer.Spam()
 
 	log.Info(counter.GetErrorsSummary())
-
-	if params.VerifyLedger {
-		log.Infof("Ledger verification params.Starts within 10s...")
-		time.Sleep(time.Second * 10)
-		tools.VerifyBranches(clients)
-	}
-
 	log.Info("Quick Test finished")
-
-	return outWallets
 }
