47c7b298b7bc6cc60e9da4a814b49b7d52a3de04
jkrvivian <jkrvivian@gmail.com>
2022-04-27 21:07:29 +0800

    Make EpochManager configurable

--- packages/notarization/epochmanager.go
+++ packages/notarization/epochmanager.go
@@ -19,37 +19,43 @@ const (
 
 // EpochManager is responsible for time/ECI conversions.
 type EpochManager struct {
-	GenesisTime      int64
-	Interval         int64
-	OracleEpochShift ECI
+	options *ManagerOptions
 }
 
 // NewEpochManager is the constructor of the EpochManager that takes a KVStore to persist its state.
-func NewEpochManager() *EpochManager {
-	return &EpochManager{
+func NewEpochManager(opts ...ManagerOption) *EpochManager {
+	options := &ManagerOptions{
 		GenesisTime:      DefaultGenesisTime,
 		Interval:         defaultInterval,
 		OracleEpochShift: defaultOracleEpochShift,
+	}
+
+	for _, option := range opts {
+		option(options)
+	}
+
+	return &EpochManager{
+		options: options,
 	}
 }
 
 // TimeToECI calculates the ECI for the given time.
 func (m *EpochManager) TimeToECI(t time.Time) (eci ECI) {
-	elapsedSeconds := t.Unix() - m.GenesisTime
+	elapsedSeconds := t.Unix() - m.options.GenesisTime
 	if elapsedSeconds <= 0 {
 		return 0
 	}
 
-	return ECI(elapsedSeconds / m.Interval)
+	return ECI(elapsedSeconds / m.options.Interval)
 }
 
 // TimeToOracleECI calculates the oracle ECI for the given time.
 func (m *EpochManager) TimeToOracleECI(t time.Time) (oracleECI ECI) {
 	eci := m.TimeToECI(t)
-	oracleECI = eci - m.OracleEpochShift
+	oracleECI = eci - m.options.OracleEpochShift
 
 	// default to ECI 0 if oracle epoch < shift as it is not defined
-	if eci < m.OracleEpochShift || oracleECI < m.OracleEpochShift {
+	if eci < m.options.OracleEpochShift || oracleECI < m.options.OracleEpochShift {
 		return 0
 	}
 
@@ -58,14 +64,52 @@ func (m *EpochManager) TimeToOracleECI(t time.Time) (oracleECI ECI) {
 
 // ECIToStartTime calculates the start time of the given epoch.
 func (m *EpochManager) ECIToStartTime(eci ECI) time.Time {
-	startUnix := m.GenesisTime + int64(eci)*m.Interval
+	startUnix := m.options.GenesisTime + int64(eci)*m.options.Interval
 	return time.Unix(startUnix, 0)
 }
 
 // ECIToEndTime calculates the end time of the given epoch.
 func (m *EpochManager) ECIToEndTime(eci ECI) time.Time {
-	endUnix := m.GenesisTime + int64(eci)*m.Interval + m.Interval - 1
+	endUnix := m.options.GenesisTime + int64(eci)*m.options.Interval + m.options.Interval - 1
 	return time.Unix(endUnix, 0)
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region ManagerOptions ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// ManagerOption represents the return type of optional parameters that can be handed into the constructor of the
+// Manager to configure its behavior.
+type ManagerOption func(options *ManagerOptions)
+
+// ManagerOptions is a container for all configurable parameters of the EpochManager.
+type ManagerOptions struct {
+	GenesisTime      int64
+	Interval         int64
+	OracleEpochShift ECI
+}
+
+// GenesisTime is a ManagerOption that allows to define the time of the genesis, i.e., the start of the epochs,
+// specified in Unix time (seconds).
+func GenesisTime(genesisTime int64) ManagerOption {
+	return func(options *ManagerOptions) {
+		options.GenesisTime = genesisTime
+	}
+}
+
+// Interval is a ManagerOption that allows to define the epoch interval, i.e., the duration of each Epoch, specified
+// in seconds.
+func Interval(interval int64) ManagerOption {
+	return func(options *ManagerOptions) {
+		options.Interval = interval
+	}
+}
+
+// OracleEpochShift is a ManagerOption that allows to define the shift of the oracle epoch.
+func OracleEpochShift(shift int) ManagerOption {
+	return func(options *ManagerOptions) {
+		options.OracleEpochShift = ECI(shift)
+	}
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
