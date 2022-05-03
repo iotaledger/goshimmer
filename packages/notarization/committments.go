6126a443fe7f4afdff6865ab7e5b11e73ed8a695
jkrvivian <jkrvivian@gmail.com>
2022-05-03 14:45:22 +0800

    Modify EpochCommitment comment

--- packages/notarization/committments.go
+++ packages/notarization/committments.go
@@ -5,7 +5,7 @@ import (
 	"github.com/iotaledger/goshimmer/packages/tangle"
 )
 
-// EpochCommitment is a commitment is a compressed form of all the information (messages and confirmed value payloads) of a certain epoch.
+// EpochCommitment is a compressed form of all the information (messages and confirmed value payloads) of a certain epoch.
 type EpochCommitment struct {
 	ECI ECI
 }
