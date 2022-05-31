3f259391b2f9796ae116321d4850f1a398cf1d83
Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>
2022-05-31 10:24:52 +0200

    Feat/commitment proofs (#2229)
    * Add methods for proof calculation
    
    * Handle all errors in commitment factory and log them
    
    * Add proof verification
    
    * Change to wrap

--- packages/notarization/manager.go
+++ packages/notarization/manager.go
@@ -1,6 +1,7 @@
 package notarization
 
 import (
+	"github.com/iotaledger/hive.go/logger"
 	"sync"
 	"time"
 
@@ -20,12 +21,14 @@ type Manager struct {
 	options                *ManagerOptions
 	pendingConflictsCount  map[EI]uint64
 	pccMutex               sync.RWMutex
+	log                    *logger.Logger
 }
 
 // NewManager creates and returns a new notarization manager.
 func NewManager(epochManager *EpochManager, epochCommitmentFactory *EpochCommitmentFactory, tangle *tangle.Tangle, opts ...ManagerOption) *Manager {
 	options := &ManagerOptions{
 		MinCommitableEpochAge: minEpochCommitableDuration,
+		Log:                   nil,
 	}
 	for _, option := range opts {
 		option(options)
@@ -35,6 +38,7 @@ func NewManager(epochManager *EpochManager, epochCommitmentFactory *EpochCommitm
 		epochManager:           epochManager,
 		epochCommitmentFactory: epochCommitmentFactory,
 		pendingConflictsCount:  make(map[EI]uint64),
+		log:                    options.Log,
 		options:                options,
 	}
 }
@@ -65,27 +69,67 @@ func (m *Manager) GetLatestEC() *EpochCommitment {
 	return m.epochCommitmentFactory.GetEpochCommitment(ei)
 }
 
+// GetBlockInclusionProof gets the proof of the inclusion (acceptance) of a block.
+func (m *Manager) GetBlockInclusionProof(blockID tangle.MessageID) (*CommitmentProof, error) {
+	var ei EI
+	m.tangle.Storage.Message(blockID).Consume(func(block *tangle.Message) {
+		t := block.IssuingTime()
+		ei = m.epochManager.TimeToEI(t)
+	})
+	proof, err := m.epochCommitmentFactory.ProofTangleRoot(ei, blockID)
+	if err != nil {
+		return nil, err
+	}
+	return proof, nil
+}
+
+// GetTransactionInclusionProof gets the proof of the inclusion (acceptance) of a transaction.
+func (m *Manager) GetTransactionInclusionProof(transactionID ledgerstate.TransactionID) (*CommitmentProof, error) {
+	var ei EI
+	m.tangle.LedgerState.Transaction(transactionID).Consume(func(tx *ledgerstate.Transaction) {
+		t := tx.Essence().Timestamp()
+		ei = m.epochManager.TimeToEI(t)
+	})
+	proof, err := m.epochCommitmentFactory.ProofStateMutationRoot(ei, transactionID)
+	if err != nil {
+		return nil, err
+	}
+	return proof, nil
+}
+
 // OnMessageConfirmed is the handler for message confirmed event.
 func (m *Manager) OnMessageConfirmed(message *tangle.Message) {
 	ei := m.epochManager.TimeToEI(message.IssuingTime())
-	m.epochCommitmentFactory.InsertTangleLeaf(ei, message.ID())
+	err := m.epochCommitmentFactory.InsertTangleLeaf(ei, message.ID())
+	if err != nil && m.log != nil {
+		m.log.Error(err)
+	}
 }
 
 // OnTransactionConfirmed is the handler for transaction confirmed event.
 func (m *Manager) OnTransactionConfirmed(tx *ledgerstate.Transaction) {
 	ei := m.epochManager.TimeToEI(tx.Essence().Timestamp())
-	m.epochCommitmentFactory.InsertStateMutationLeaf(ei, tx.ID())
+	err := m.epochCommitmentFactory.InsertStateMutationLeaf(ei, tx.ID())
+	if err != nil && m.log != nil {
+		m.log.Error(err)
+	}
 	m.updateStateSMT(ei, tx)
 }
 
 func (m *Manager) updateStateSMT(ei EI, tx *ledgerstate.Transaction) {
 	for _, o := range tx.Essence().Outputs() {
-		m.epochCommitmentFactory.InsertStateLeaf(ei, o.ID())
+		err := m.epochCommitmentFactory.InsertStateLeaf(ei, o.ID())
+		if err != nil && m.log != nil {
+			m.log.Error(err)
+		}
 	}
 	// remove spent outputs
 	for _, i := range tx.Essence().Inputs() {
 		out, _ := ledgerstate.OutputIDFromBase58(i.Base58())
-		m.epochCommitmentFactory.RemoveStateLeaf(ei, out)
+		err := m.epochCommitmentFactory.RemoveStateLeaf(ei, out)
+		if err != nil && m.log != nil {
+			m.log.Error(err)
+		}
 	}
 }
 
@@ -128,11 +172,19 @@ type ManagerOption func(options *ManagerOptions)
 // ManagerOptions is a container of all the config parameters of the notarization manager.
 type ManagerOptions struct {
 	MinCommitableEpochAge time.Duration
+	Log                   *logger.Logger
 }
 
 // MinCommitableEpochAge specifies how old an epoch has to be for it to be commitable.
 func MinCommitableEpochAge(d time.Duration) ManagerOption {
 	return func(options *ManagerOptions) {
 		options.MinCommitableEpochAge = d
+	}
+}
+
+// Log provides the logger.
+func Log(log *logger.Logger) ManagerOption {
+	return func(options *ManagerOptions) {
+		options.Log = log
 	}
 }
