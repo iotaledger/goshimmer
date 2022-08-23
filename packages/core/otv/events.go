8f56adacbda777148a63831912399982dbc2524b
Piotr Macek <piotr.macek@iota.org>
2022-08-23 09:54:21 +0200

    Fix typo

--- packages/core/otv/events.go
+++ packages/core/otv/events.go
@@ -9,17 +9,17 @@ import (
 
 type Events struct {
 	BlockTracked          *event.Event[*Block]
-	ConflictVoterAdded    *event.Event[*votes.ConflictVoterEvent[utxo.TransactionID]]
-	ConflicteVoterRemoved *event.Event[*votes.ConflictVoterEvent[utxo.TransactionID]]
-	SequenceVoterAdded    *event.Event[*votes.SequenceVoterEvent]
+	ConflictVoterAdded   *event.Event[*votes.ConflictVoterEvent[utxo.TransactionID]]
+	ConflictVoterRemoved *event.Event[*votes.ConflictVoterEvent[utxo.TransactionID]]
+	SequenceVoterAdded   *event.Event[*votes.SequenceVoterEvent]
 }
 
 // newEvents creates a new Events instance.
 func newEvents(conflictTrackerEvents *votes.ConflictTrackerEvents[utxo.TransactionID], sequenceTrackerEvents *votes.SequenceTrackerEvents) *Events {
 	return &Events{
-		BlockTracked:          event.New[*Block](),
-		ConflictVoterAdded:    conflictTrackerEvents.VoterAdded,
-		ConflicteVoterRemoved: conflictTrackerEvents.VoterRemoved,
-		SequenceVoterAdded:    sequenceTrackerEvents.VoterAdded,
+		BlockTracked:         event.New[*Block](),
+		ConflictVoterAdded:   conflictTrackerEvents.VoterAdded,
+		ConflictVoterRemoved: conflictTrackerEvents.VoterRemoved,
+		SequenceVoterAdded:   sequenceTrackerEvents.VoterAdded,
 	}
 }
