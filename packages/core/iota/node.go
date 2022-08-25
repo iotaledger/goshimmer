5000337d529d256d2a0ca7231d6b37fcb95edcf6
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-08-25 17:06:56 +0200

    Feat: Orchestration Framework

---
+++ packages/core/iota/node.go
@@ -0,0 +1,28 @@
+package iota
+
+import (
+	"github.com/iotaledger/hive.go/core/generics/options"
+
+	"github.com/iotaledger/goshimmer/packages/node/database"
+)
+
+type Node struct {
+	Database *database.Manager
+	Protocol *Protocol
+	// Parser
+	// Requester
+	// Gossip
+	// P2P
+	// Auto peering
+	// Warp sync
+
+	optsDatabaseOptions []database.Option
+	optsProtocolOptions []options.Option[Protocol]
+}
+
+func NewNode(dbFolder string, opts ...options.Option[Node]) (newNode *Node) {
+	return options.Apply(&Node{}, opts, func(n *Node) {
+		n.Database = database.NewManager(dbFolder, n.optsDatabaseOptions...)
+		n.Protocol = NewProtocol(n.optsProtocolOptions...)
+	})
+}
