e03bd32b8c248d0b69f75f31554660bff3403830
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-08-31 11:43:05 +0200

    Feat: added more boilerplate code

--- packages/protocol/engine/engine.go
+++ packages/protocol/engine/engine.go
@@ -1,16 +1,37 @@
 package engine
 
 import (
+	"github.com/iotaledger/hive.go/core/generics/options"
+
+	"github.com/iotaledger/goshimmer/packages/core/eviction"
 	"github.com/iotaledger/goshimmer/packages/core/ledger"
+	"github.com/iotaledger/goshimmer/packages/core/validator"
 	"github.com/iotaledger/goshimmer/packages/protocol/engine/tangle"
+	"github.com/iotaledger/goshimmer/packages/protocol/engine/tangle/models"
 )
+
+// region Engine ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type Engine struct {
 	*tangle.Tangle
+
+	optsTangle []options.Option[tangle.Tangle]
 }
 
-func NewEngine(ledger *ledger.Ledger) (engine *Engine) {
-	return &Engine{
-		Tangle: tangle.New(ledger, nil, nil),
+func NewEngine(ledger *ledger.Ledger, evictionManager *eviction.Manager[models.BlockID], validatorSet *validator.Set, opts ...options.Option[Engine]) (engine *Engine) {
+	return options.Apply(new(Engine), opts, func(e *Engine) {
+		e.Tangle = tangle.New(ledger, evictionManager, validatorSet, e.optsTangle...)
+	})
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// region Options //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func WithTangleOptions(tangleOptions ...options.Option[tangle.Tangle]) options.Option[Engine] {
+	return func(e *Engine) {
+		e.optsTangle = tangleOptions
 	}
 }
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
