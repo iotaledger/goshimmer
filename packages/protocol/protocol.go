c064c33ef2f378552da892d80c91c674111daba6
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-09-06 11:32:19 +0200

    Feat: commit evening stuff

--- packages/protocol/protocol.go
+++ packages/protocol/protocol.go
@@ -6,6 +6,7 @@ import (
 	"github.com/iotaledger/hive.go/core/generics/options"
 
 	"github.com/iotaledger/goshimmer/packages/core/notarization"
+	"github.com/iotaledger/goshimmer/packages/network"
 	"github.com/iotaledger/goshimmer/packages/protocol/database"
 	"github.com/iotaledger/goshimmer/packages/protocol/engine"
 	"github.com/iotaledger/goshimmer/packages/protocol/engine/tangle/models"
@@ -26,14 +27,13 @@ type Protocol struct {
 	Engine              *engine.Engine
 	Solidification      *solidification.Solidification
 	SybilProtection     *sybilprotection.SybilProtection
-	Network             bool
 
 	optsEngineOptions         []options.Option[engine.Engine]
 	optsDBManagerOptions      []options.Option[database.Manager]
 	optsSolidificationOptions []options.Option[solidification.Solidification]
 }
 
-func New(opts ...options.Option[Protocol]) (protocol *Protocol) {
+func New(network network.Network, opts ...options.Option[Protocol]) (protocol *Protocol) {
 	return options.Apply(new(Protocol), opts, func(p *Protocol) {
 		p.Events = NewEvents()
 
