eba172b4a27b1f3da83f2bc1f6c39b1b5e3cf6b0
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-09-26 10:20:52 +0200

    Fix: fixed referenceprovider test

--- packages/app/blockissuer/blockfactory/referenceprovider_test.go
+++ packages/app/blockissuer/blockfactory/referenceprovider_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/iotaledger/goshimmer/packages/core/epoch"
 	"github.com/iotaledger/goshimmer/packages/core/validator"
 	"github.com/iotaledger/goshimmer/packages/protocol"
-	"github.com/iotaledger/goshimmer/packages/protocol/engine"
 	"github.com/iotaledger/goshimmer/packages/protocol/engine/tangle"
 	"github.com/iotaledger/goshimmer/packages/protocol/models"
 	"github.com/iotaledger/goshimmer/packages/protocol/models/payload"
@@ -42,101 +41,103 @@ func TestReferenceProvider_References1(t *testing.T) {
 }
 
 func TestBlockFactory_PrepareLikedReferences_2(t *testing.T) {
-	tf := engine.NewTestFramework(t)
+	tf := protocol.NewTestFramework(t)
+	tangleTF := tangle.NewTestFramework(t, tangle.WithTangle(tf.Protocol.Engine().Tangle))
 
-	tf.Tangle.CreateIdentity("V1", validator.WithWeight(10))
-	tf.Tangle.CreateIdentity("V2", validator.WithWeight(20))
+	tangleTF.CreateIdentity("V1", validator.WithWeight(10))
+	tangleTF.CreateIdentity("V2", validator.WithWeight(20))
 
-	tf.Tangle.CreateBlock("Block0", models.WithPayload(tf.Tangle.CreateTransaction("TX0", 2, "Genesis")), models.WithIssuer(tf.Tangle.Identity("V1").PublicKey()))
-	tf.Tangle.CreateBlock("Block1", models.WithPayload(tf.Tangle.CreateTransaction("TX1", 1, "TX0.0")), models.WithIssuer(tf.Tangle.Identity("V2").PublicKey()), models.WithIssuingTime(time.Now().Add(time.Minute)))
-	tf.Tangle.CreateBlock("Block2", models.WithPayload(tf.Tangle.CreateTransaction("TX2", 1, "TX0.1")), models.WithIssuer(tf.Tangle.Identity("V2").PublicKey()))
-	tf.Tangle.CreateBlock("Block3", models.WithPayload(tf.Tangle.CreateTransaction("TX3", 1, "TX0.1")), models.WithIssuer(tf.Tangle.Identity("V1").PublicKey()))
-	tf.Tangle.CreateBlock("Block4", models.WithPayload(tf.Tangle.CreateTransaction("TX4", 1, "TX0.0")), models.WithIssuer(tf.Tangle.Identity("V1").PublicKey()))
-	tf.Tangle.IssueBlocks("Block0", "Block1", "Block2", "Block3", "Block4").WaitUntilAllTasksProcessed()
+	tangleTF.CreateBlock("Block0", models.WithPayload(tangleTF.CreateTransaction("TX0", 2, "Genesis")), models.WithIssuer(tangleTF.Identity("V1").PublicKey()))
+	tangleTF.CreateBlock("Block1", models.WithPayload(tangleTF.CreateTransaction("TX1", 1, "TX0.0")), models.WithIssuer(tangleTF.Identity("V2").PublicKey()), models.WithIssuingTime(time.Now().Add(time.Minute)))
+	tangleTF.CreateBlock("Block2", models.WithPayload(tangleTF.CreateTransaction("TX2", 1, "TX0.1")), models.WithIssuer(tangleTF.Identity("V2").PublicKey()))
+	tangleTF.CreateBlock("Block3", models.WithPayload(tangleTF.CreateTransaction("TX3", 1, "TX0.1")), models.WithIssuer(tangleTF.Identity("V1").PublicKey()))
+	tangleTF.CreateBlock("Block4", models.WithPayload(tangleTF.CreateTransaction("TX4", 1, "TX0.0")), models.WithIssuer(tangleTF.Identity("V1").PublicKey()))
+	tangleTF.IssueBlocks("Block0", "Block1", "Block2", "Block3", "Block4").WaitUntilAllTasksProcessed()
 
-	rp := NewReferenceProvider(func() *engine.Engine { return tf.Engine }, func() (epoch.Index, error) {
+	rp := NewReferenceProvider(tf.Protocol.Engine, func() (epoch.Index, error) {
 		return 0, nil
 	})
 
 	// Verify that like references are set correctly.
 	{
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs(), map[models.ParentsType]models.BlockIDs{}, true)
+		checkReferences(t, rp, nil, tangleTF.BlockIDs(), map[models.ParentsType]models.BlockIDs{}, true)
 
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs("Block2", "Block3"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType:      tf.Tangle.BlockIDs("Block2", "Block3"),
-			models.ShallowLikeParentType: tf.Tangle.BlockIDs("Block2"),
+		checkReferences(t, rp, nil, tangleTF.BlockIDs("Block2", "Block3"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType:      tangleTF.BlockIDs("Block2", "Block3"),
+			models.ShallowLikeParentType: tangleTF.BlockIDs("Block2"),
 		})
 
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs("Block1", "Block2"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block1", "Block2"),
+		checkReferences(t, rp, nil, tangleTF.BlockIDs("Block1", "Block2"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block1", "Block2"),
 		})
 
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs("Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType:      tf.Tangle.BlockIDs("Block3", "Block4"),
-			models.ShallowLikeParentType: tf.Tangle.BlockIDs("Block1", "Block2"),
+		checkReferences(t, rp, nil, tangleTF.BlockIDs("Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType:      tangleTF.BlockIDs("Block3", "Block4"),
+			models.ShallowLikeParentType: tangleTF.BlockIDs("Block1", "Block2"),
 		})
 
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs("Block1", "Block2", "Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType:      tf.Tangle.BlockIDs("Block1", "Block2", "Block3", "Block4"),
-			models.ShallowLikeParentType: tf.Tangle.BlockIDs("Block1", "Block2"),
+		checkReferences(t, rp, nil, tangleTF.BlockIDs("Block1", "Block2", "Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType:      tangleTF.BlockIDs("Block1", "Block2", "Block3", "Block4"),
+			models.ShallowLikeParentType: tangleTF.BlockIDs("Block1", "Block2"),
 		})
 	}
 
 	// Add reattachment that is older than the original block and verify that it is selected with a like reference.
 	{
-		tf.Tangle.CreateBlock("Block5", models.WithPayload(tf.Tangle.Transaction("TX1")))
-		tf.Tangle.IssueBlocks("Block5").WaitUntilAllTasksProcessed()
+		tangleTF.CreateBlock("Block5", models.WithPayload(tangleTF.Transaction("TX1")))
+		tangleTF.IssueBlocks("Block5").WaitUntilAllTasksProcessed()
 
-		checkReferences(t, rp, nil, tf.Tangle.BlockIDs("Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType:      tf.Tangle.BlockIDs("Block3", "Block4"),
-			models.ShallowLikeParentType: tf.Tangle.BlockIDs("Block2", "Block5"),
+		checkReferences(t, rp, nil, tangleTF.BlockIDs("Block3", "Block4"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType:      tangleTF.BlockIDs("Block3", "Block4"),
+			models.ShallowLikeParentType: tangleTF.BlockIDs("Block2", "Block5"),
 		})
 	}
 }
 
 // Tests if weak references are properly constructed from consumed outputs.
 func TestBlockFactory_WeakReferencesConsumed(t *testing.T) {
-	tf := engine.NewTestFramework(t)
+	tf := protocol.NewTestFramework(t)
+	tangleTF := tangle.NewTestFramework(t, tangle.WithTangle(tf.Protocol.Engine().Tangle))
 
-	tf.Tangle.CreateBlock("Block1", models.WithPayload(tf.Tangle.CreateTransaction("TX1", 3, "Genesis")))
-	tf.Tangle.CreateBlock("Block2", models.WithPayload(tf.Tangle.CreateTransaction("TX2", 1, "TX1.0")))
-	tf.Tangle.CreateBlock("Block3", models.WithPayload(tf.Tangle.CreateTransaction("TX3", 1, "TX1.1")))
-	tf.Tangle.CreateBlock("Block4", models.WithPayload(tf.Tangle.CreateTransaction("TX4", 1, "TX2.0", "TX3.0")))
-	tf.Tangle.IssueBlocks("Block1", "Block2", "Block3", "Block4").WaitUntilAllTasksProcessed()
+	tangleTF.CreateBlock("Block1", models.WithPayload(tangleTF.CreateTransaction("TX1", 3, "Genesis")))
+	tangleTF.CreateBlock("Block2", models.WithPayload(tangleTF.CreateTransaction("TX2", 1, "TX1.0")))
+	tangleTF.CreateBlock("Block3", models.WithPayload(tangleTF.CreateTransaction("TX3", 1, "TX1.1")))
+	tangleTF.CreateBlock("Block4", models.WithPayload(tangleTF.CreateTransaction("TX4", 1, "TX2.0", "TX3.0")))
+	tangleTF.IssueBlocks("Block1", "Block2", "Block3", "Block4").WaitUntilAllTasksProcessed()
 
-	rp := NewReferenceProvider(func() *engine.Engine { return tf.Engine }, func() (epoch.Index, error) {
+	rp := NewReferenceProvider(tf.Protocol.Engine, func() (epoch.Index, error) {
 		return 0, nil
 	})
 
 	{
-		checkReferences(t, rp, tf.Tangle.Block("Block2").Payload(), tf.Tangle.BlockIDs("Block1"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block1"),
+		checkReferences(t, rp, tangleTF.Block("Block2").Payload(), tangleTF.BlockIDs("Block1"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block1"),
 		})
 
-		checkReferences(t, rp, tf.Tangle.Block("Block3").Payload(), tf.Tangle.BlockIDs("Block2"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block2"),
-			models.WeakParentType:   tf.Tangle.BlockIDs("Block1"),
+		checkReferences(t, rp, tangleTF.Block("Block3").Payload(), tangleTF.BlockIDs("Block2"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block2"),
+			models.WeakParentType:   tangleTF.BlockIDs("Block1"),
 		})
 
-		checkReferences(t, rp, tf.Tangle.Block("Block4").Payload(), tf.Tangle.BlockIDs("Block4"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block4"),
-			models.WeakParentType:   tf.Tangle.BlockIDs("Block2", "Block3"),
+		checkReferences(t, rp, tangleTF.Block("Block4").Payload(), tangleTF.BlockIDs("Block4"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block4"),
+			models.WeakParentType:   tangleTF.BlockIDs("Block2", "Block3"),
 		})
 
-		checkReferences(t, rp, tf.Tangle.Block("Block4").Payload(), tf.Tangle.BlockIDs("Block4", "Block3"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block4", "Block3"),
-			models.WeakParentType:   tf.Tangle.BlockIDs("Block2"),
+		checkReferences(t, rp, tangleTF.Block("Block4").Payload(), tangleTF.BlockIDs("Block4", "Block3"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block4", "Block3"),
+			models.WeakParentType:   tangleTF.BlockIDs("Block2"),
 		})
 	}
 
 	// IssueBlock reattachment of TX3 (Block5) and make sure it is referenced in favor of Block3 (earliest attachment).
-	tf.Tangle.CreateBlock("Block5", models.WithPayload(tf.Tangle.Transaction("TX3")))
-	tf.Tangle.IssueBlocks("Block5").WaitUntilAllTasksProcessed()
+	tangleTF.CreateBlock("Block5", models.WithPayload(tangleTF.Transaction("TX3")))
+	tangleTF.IssueBlocks("Block5").WaitUntilAllTasksProcessed()
 
 	{
-		checkReferences(t, rp, tf.Tangle.Block("Block4").Payload(), tf.Tangle.BlockIDs("Block1"), map[models.ParentsType]models.BlockIDs{
-			models.StrongParentType: tf.Tangle.BlockIDs("Block1"),
-			models.WeakParentType:   tf.Tangle.BlockIDs("Block5", "Block2"),
+		checkReferences(t, rp, tangleTF.Block("Block4").Payload(), tangleTF.BlockIDs("Block1"), map[models.ParentsType]models.BlockIDs{
+			models.StrongParentType: tangleTF.BlockIDs("Block1"),
+			models.WeakParentType:   tangleTF.BlockIDs("Block5", "Block2"),
 		})
 	}
 }
