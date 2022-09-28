24a2036eddf99183da9dc48fb3311ae1ddf26cfd
Piotr Macek <piotr.macek@iota.org>
2022-09-28 12:42:06 +0200

    Fix value test.

--- tools/integration-tests/tester/tests/testutil.go
+++ tools/integration-tests/tester/tests/testutil.go
@@ -134,7 +134,6 @@ func Context(ctx context.Context, t *testing.T) (context.Context, context.Cancel
 // Synced returns whether node is synchronized.
 func Synced(t *testing.T, node *framework.Node) bool {
 	info, err := node.Info()
-	fmt.Printf("%+v\n", info)
 	require.NoError(t, err)
 	return info.TangleTime.Synced
 }
