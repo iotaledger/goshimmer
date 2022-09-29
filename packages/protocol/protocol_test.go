3773c2155d4762f09e244e0af290880816d7d5b7
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-09-29 09:59:25 +0200

    Feat: cleaned up more code

--- packages/protocol/protocol_test.go
+++ packages/protocol/protocol_test.go
@@ -22,23 +22,23 @@ func TestProtocol(t *testing.T) {
 	testNetwork := network.NewMockedNetwork()
 
 	endpoint1 := testNetwork.Join(identity.GenerateIdentity().ID())
-	endpoint2 := testNetwork.Join(identity.GenerateIdentity().ID())
-
 	diskUtil1 := diskutil.New(t.TempDir())
-	diskUtil2 := diskutil.New(t.TempDir())
 
 	require.NoError(t, creator.CreateSnapshot(diskUtil1.Path("snapshot.bin"), 100, make([]byte, 32, 32), map[identity.ID]uint64{
 		identity.GenerateIdentity().ID(): 100,
 	}))
 
+	protocol1 := New(endpoint1, WithBaseDirectory(diskUtil1.Path()), WithSnapshotPath(diskUtil1.Path("snapshot.bin")))
+	protocol1.Run()
+
+	endpoint2 := testNetwork.Join(identity.GenerateIdentity().ID())
+	diskUtil2 := diskutil.New(t.TempDir())
+
 	require.NoError(t, creator.CreateSnapshot(diskUtil2.Path("snapshot.bin"), 100, make([]byte, 32, 32), map[identity.ID]uint64{
 		identity.GenerateIdentity().ID(): 100,
 	}))
 
-	protocol1 := New(endpoint1, WithBaseDirectory(diskUtil1.Path()), WithSnapshotPath(diskUtil1.Path("snapshot.bin")))
 	protocol2 := New(endpoint2, WithBaseDirectory(diskUtil2.Path()), WithSnapshotPath(diskUtil2.Path("snapshot.bin")))
-
-	protocol1.Run()
 	protocol2.Run()
 
 	tf1 := engine.NewTestFramework(t, engine.WithEngine(protocol1.activeInstance))
