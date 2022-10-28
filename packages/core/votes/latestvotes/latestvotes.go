0a0aaa46df747aa18039b6880e4751d813763c36
Piotr Macek <piotr.macek@iota.org>
2022-10-28 10:30:16 +0200

    Refactor naming

--- packages/core/votes/latestvotes/latestvotes.go
+++ packages/core/votes/latestvotes/latestvotes.go
@@ -9,49 +9,49 @@ import (
 	"github.com/iotaledger/goshimmer/packages/core/validator"
 )
 
-// LatestVotes keeps track of the most up-to-date for a certain Voter casted on a specific Marker SequenceID.
-// Votes can be casted on Markers (SequenceID, Index), but can arrive in any arbitrary order.
+// LatestVotes keeps track of the most up-to-date for a certain Voter casted on a specific Index.
+// Votes can be casted on any Index of Integer type, but can arrive in any arbitrary order.
 // Due to the nature of a Sequence, a vote casted for a certain Index clobbers votes for every lower index.
 // Similarly, if a vote for an Index is casted and an existing vote for an higher Index exists, the operation has no effect.
-type LatestVotes[EntityID constraints.Integer, VotePowerType constraints.Comparable[VotePowerType]] struct {
+type LatestVotes[EntityIndex constraints.Integer, VotePowerType constraints.Comparable[VotePowerType]] struct {
 	voter *validator.Validator
-	t     *thresholdmap.ThresholdMap[EntityID, VotePowerType]
+	t     *thresholdmap.ThresholdMap[EntityIndex, VotePowerType]
 
 	m sync.RWMutex
 }
 
 // NewLatestVotes creates a new NewLatestVotes instance associated with the given details.
-func NewLatestVotes[EntityID constraints.Integer, VotePowerType constraints.Comparable[VotePowerType]](voter *validator.Validator) (newLatestMarkerVotes *LatestVotes[EntityID, VotePowerType]) {
-	return &LatestVotes[EntityID, VotePowerType]{
+func NewLatestVotes[EntityIndex constraints.Integer, VotePowerType constraints.Comparable[VotePowerType]](voter *validator.Validator) (newLatestMarkerVotes *LatestVotes[EntityIndex, VotePowerType]) {
+	return &LatestVotes[EntityIndex, VotePowerType]{
 		voter: voter,
-		t:     thresholdmap.New[EntityID, VotePowerType](thresholdmap.UpperThresholdMode),
+		t:     thresholdmap.New[EntityIndex, VotePowerType](thresholdmap.UpperThresholdMode),
 	}
 }
 
 // ForEach provides a callback based iterator that iterates through all Elements in the map.
-func (l *LatestVotes[EntityID, VotePowerType]) ForEach(iterator func(node *thresholdmap.Element[EntityID, VotePowerType]) bool) {
+func (l *LatestVotes[EntityIndex, VotePowerType]) ForEach(iterator func(node *thresholdmap.Element[EntityIndex, VotePowerType]) bool) {
 	l.t.ForEach(iterator)
 }
 
 // Voter returns the Voter for the LatestVotes.
-func (l *LatestVotes[EntityID, VotePowerType]) Voter() *validator.Validator {
+func (l *LatestVotes[EntityIndex, VotePowerType]) Voter() *validator.Validator {
 	l.m.RLock()
 	defer l.m.RUnlock()
 
 	return l.voter
 }
 
-// Power returns the power of the vote for the given marker Index.
-func (l *LatestVotes[EntityID, VotePowerType]) Power(index EntityID) (power VotePowerType, exists bool) {
+// Power returns the power of the vote for the given Index.
+func (l *LatestVotes[EntityIndex, VotePowerType]) Power(index EntityIndex) (power VotePowerType, exists bool) {
 	l.m.RLock()
 	defer l.m.RUnlock()
 
 	return l.t.Get(index)
 }
 
-// Store stores the vote with the given marker Index and votePower.
+// Store stores the vote with the given Index and votePower.
 // The votePower parameter is used to determine the order of the vote.
-func (l *LatestVotes[EntityID, VotePowerType]) Store(index EntityID, power VotePowerType) (stored bool, previousHighestIndex EntityID) {
+func (l *LatestVotes[EntityIndex, VotePowerType]) Store(index EntityIndex, power VotePowerType) (stored bool, previousHighestIndex EntityIndex) {
 	l.m.Lock()
 	defer l.m.Unlock()
 	if maxElement := l.t.MaxElement(); maxElement != nil {
