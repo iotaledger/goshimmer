709bfc8e516bef9ab6882cae51b615c8a8428d0c
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-11-01 08:24:38 +0100

    Feat: go mod tidy + comments

--- packages/storage/storage.go
+++ packages/storage/storage.go
@@ -7,34 +7,33 @@ import (
 	"github.com/iotaledger/goshimmer/packages/storage/prunable"
 )
 
-// region Storage //////////////////////////////////////////////////////////////////////////////////////////////////////
-
+// Storage is an abstraction around the storage layer of the node.
 type Storage struct {
+	// Permanent is the section of the storage that is maintained forever (holds the current ledger state).
 	*permanent.Permanent
+
+	// Prunable is the section of the storage that is pruned regularly (holds the history of the ledger state).
 	*prunable.Prunable
 
+	// Manager is the database manager that manages the underlying database instances.
 	database *database.Manager
 }
 
-func New(folder string, databaseVersion database.Version) (newStorage *Storage) {
-	database := database.NewManager(databaseVersion,
-		database.WithBaseDir(folder),
-		database.WithGranularity(1),
-		database.WithDBProvider(database.NewDB),
-	)
+// New creates a new storage instance with the named database version in the given directory.
+func New(directory string, version database.Version) (newStorage *Storage) {
+	database := database.NewManager(version, database.WithBaseDir(directory), database.WithGranularity(1), database.WithDBProvider(database.NewDB))
 
 	return &Storage{
-		Permanent: permanent.New(diskutil.New(folder, true), database),
+		Permanent: permanent.New(diskutil.New(directory, true), database),
 		Prunable:  prunable.New(database),
 
 		database: database,
 	}
 }
 
+// Shutdown shuts down the storage.
 func (c *Storage) Shutdown() (err error) {
 	defer c.database.Shutdown()
 
 	return c.Permanent.Shutdown()
 }
-
-// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
