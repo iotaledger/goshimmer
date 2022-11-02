a70aa53b6da9814d7a6e8e3e566c321117c44f8b
Andrew Cullen <45826600+cyberphysic4l@users.noreply.github.com>
2022-11-02 17:39:56 +0000

    clean up merge

---
+++ packages/protocol/engine/notarization/proofs/proofs.go
@@ -0,0 +1,147 @@
+package proofs
+
+/*
+import (
+	"github.com/celestiaorg/smt"
+	"github.com/cockroachdb/errors"
+	"github.com/iotaledger/hive.go/core/generics/lo"
+	"golang.org/x/crypto/blake2b"
+
+	"github.com/iotaledger/goshimmer/packages/core/epoch"
+	"github.com/iotaledger/goshimmer/packages/protocol/ledger"
+	"github.com/iotaledger/goshimmer/packages/protocol/ledger/utxo"
+	"github.com/iotaledger/goshimmer/packages/protocol/models"
+)
+
+// region proofs helpers ///////////////////////////////////////////////////////////////////////////////////////////////
+
+// CommitmentProof represents an inclusion proof for a specific epoch.
+type CommitmentProof struct {
+	EI    epoch.Index
+	proof smt.SparseMerkleProof
+	root  []byte
+}
+
+// GetBlockInclusionProof gets the proof of the inclusion (acceptance) of a block.
+func (m *Manager) GetBlockInclusionProof(blockID models.BlockID) (*CommitmentProof, error) {
+	var ei epoch.Index
+	block, exists := m.tangle.BlockDAG.Block(blockID)
+	if !exists {
+		return nil, errors.Errorf("cannot retrieve block with id %s", blockID)
+	}
+	t := block.IssuingTime()
+	ei = epoch.IndexFromTime(t)
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+	proof, err := m.epochCommitmentFactory.ProofTangleRoot(ei, blockID)
+========
+	proof, err := m.commitmentFactory.ProofTangleRoot(ei, blockID)
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	if err != nil {
+		return nil, err
+	}
+	return proof, nil
+}
+
+// GetTransactionInclusionProof gets the proof of the inclusion (acceptance) of a transaction.
+func (m *Manager) GetTransactionInclusionProof(transactionID utxo.TransactionID) (*CommitmentProof, error) {
+	var ei epoch.Index
+	m.ledger.Storage.CachedTransactionMetadata(transactionID).Consume(func(txMeta *ledger.TransactionMetadata) {
+		ei = epoch.IndexFromTime(txMeta.InclusionTime())
+	})
+	proof, err := m.commitmentFactory.ProofStateMutationRoot(ei, transactionID)
+	if err != nil {
+		return nil, err
+	}
+	return proof, nil
+}
+
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) verifyRoot(proof CommitmentProof, key []byte, value []byte) bool {
+========
+func (f *commitmentFactory) verifyRoot(proof CommitmentProof, key []byte, value []byte) bool {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	return smt.VerifyProof(proof.proof, proof.root, key, value, lo.PanicOnErr(blake2b.New256(nil)))
+}
+
+// ProofStateRoot returns the merkle proof for the outputID against the state root.
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) ProofStateRoot(ei epoch.Index, outID utxo.OutputID) (*CommitmentProof, error) {
+========
+func (f *commitmentFactory) ProofStateRoot(ei epoch.Index, outID utxo.OutputID) (*CommitmentProof, error) {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	key := outID.Bytes()
+	root, exists := f.commitmentTrees.Get(ei)
+	if !exists {
+		return nil, errors.Errorf("could not obtain commitment trees for epoch %d", ei)
+	}
+	tangleRoot := root.tangleTree.Root()
+	proof, err := f.stateRootTree.ProveForRoot(key, tangleRoot)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not generate the state root proof")
+	}
+	return &CommitmentProof{ei, proof, tangleRoot}, nil
+}
+
+// ProofStateMutationRoot returns the merkle proof for the transactionID against the state mutation root.
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) ProofStateMutationRoot(ei epoch.Index, txID utxo.TransactionID) (*CommitmentProof, error) {
+========
+func (f *commitmentFactory) ProofStateMutationRoot(ei epoch.Index, txID utxo.TransactionID) (*CommitmentProof, error) {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	committmentTrees, err := f.getCommitmentTrees(ei)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot get commitment trees for epoch %d", ei)
+	}
+
+	key := txID.Bytes()
+	root := committmentTrees.stateMutationTree.Root()
+	proof, err := committmentTrees.stateMutationTree.ProveForRoot(key, root)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not generate the state mutation root proof")
+	}
+	return &CommitmentProof{ei, proof, root}, nil
+}
+
+// ProofTangleRoot returns the merkle proof for the blockID against the tangle root.
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) ProofTangleRoot(ei epoch.Index, blockID models.BlockID) (*CommitmentProof, error) {
+========
+func (f *commitmentFactory) ProofTangleRoot(ei epoch.Index, blockID models.BlockID) (*CommitmentProof, error) {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	committmentTrees, err := f.getCommitmentTrees(ei)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot get commitment trees for epoch %d", ei)
+	}
+
+	key, _ := blockID.Bytes()
+	root := committmentTrees.tangleTree.Root()
+	proof, err := committmentTrees.tangleTree.ProveForRoot(key, root)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not generate the tangle root proof")
+	}
+	return &CommitmentProof{ei, proof, root}, nil
+}
+
+// VerifyTangleRoot verify the provided merkle proof against the tangle root.
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) VerifyTangleRoot(proof CommitmentProof, blockID models.BlockID) bool {
+========
+func (f *commitmentFactory) VerifyTangleRoot(proof CommitmentProof, blockID models.BlockID) bool {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	key, _ := blockID.Bytes()
+	return f.verifyRoot(proof, key, key)
+}
+
+// VerifyStateMutationRoot verify the provided merkle proof against the state mutation root.
+<<<<<<<< HEAD:packages/protocol/engine/notarization/proofs.go
+func (f *CommitmentFactory) VerifyStateMutationRoot(proof CommitmentProof, transactionID utxo.TransactionID) bool {
+========
+func (f *commitmentFactory) VerifyStateMutationRoot(proof CommitmentProof, transactionID utxo.TransactionID) bool {
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/proofs.go
+	key := transactionID.Bytes()
+	return f.verifyRoot(proof, key, key)
+}
+
+// endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+*/
---
+++ packages/protocol/engine/notarization/proofs/testutils.go
@@ -0,0 +1,97 @@
+package proofs
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/iotaledger/hive.go/core/generics/event"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+<<<<<<<< HEAD:packages/protocol/engine/notarization/testutils.go
+	"github.com/iotaledger/goshimmer/packages/protocol/engine/mana"
+========
+	"github.com/iotaledger/goshimmer/packages/core/commitment"
+	"github.com/iotaledger/goshimmer/packages/protocol/engine/notarization"
+>>>>>>>> develop:packages/protocol/engine/notarization/proofs/testutils.go
+)
+
+const (
+	testingAcceptanceThreshold = 0.4
+)
+
+// EventMock acts as a container for event mocks.
+type EventMock struct {
+	mock.Mock
+	expectedEvents uint64
+	calledEvents   uint64
+	test           *testing.T
+
+	attached []struct {
+		*event.Event[*commitment.Commitment]
+		*event.Closure[*commitment.Commitment]
+	}
+}
+
+// NewEventMock creates a new EventMock.
+func NewEventMock(t *testing.T, notarizationManager *notarization.Manager) *EventMock {
+	e := &EventMock{
+		test: t,
+	}
+
+	// attach all events
+	notarizationManager.Events.EpochCommitted.Hook(event.NewClosure(e.EpochCommittable))
+	// notarizationManager.Events.ConsensusWeightsUpdated.Hook(event.NewClosure(e.ManaVectorUpdate))
+
+	return e
+}
+
+// DetachAll detaches all event handlers.
+func (e *EventMock) DetachAll() {
+	for _, a := range e.attached {
+		a.Event.Detach(a.Closure)
+	}
+}
+
+// Expect is a proxy for Mock.On() but keeping track of num of calls.
+func (e *EventMock) Expect(eventName string, arguments ...interface{}) {
+	event.Loop.WaitUntilAllTasksProcessed()
+	e.On(eventName, arguments...)
+	atomic.AddUint64(&e.expectedEvents, 1)
+}
+
+// AssertExpectations asserts expectations.
+func (e *EventMock) AssertExpectations(t mock.TestingT) bool {
+	var calledEvents, expectedEvents uint64
+	event.Loop.WaitUntilAllTasksProcessed()
+
+	assert.Eventuallyf(t, func() bool {
+		calledEvents = atomic.LoadUint64(&e.calledEvents)
+		expectedEvents = atomic.LoadUint64(&e.expectedEvents)
+		return calledEvents == expectedEvents
+	}, 5*time.Second, 1*time.Millisecond, "number of called (%d) events is not equal to number of expected events (%d)", calledEvents, expectedEvents)
+
+	defer func() {
+		e.Calls = make([]mock.Call, 0)
+		e.ExpectedCalls = make([]*mock.Call, 0)
+		e.expectedEvents = 0
+		e.calledEvents = 0
+	}()
+
+	return e.Mock.AssertExpectations(t)
+}
+
+// EpochCommittable is the mocked EpochCommittable event.
+func (e *EventMock) EpochCommittable(commitment *commitment.Commitment) {
+	e.Called(commitment.Index())
+	atomic.AddUint64(&e.calledEvents, 1)
+}
+
+/*
+// ManaVectorUpdate is the mocked ManaVectorUpdate event.
+func (e *EventMock) ManaVectorUpdate(event *mana.ManaVectorUpdateEvent) {
+	e.Called(event.EI)
+	atomic.AddUint64(&e.calledEvents, 1)
+}
+*/
