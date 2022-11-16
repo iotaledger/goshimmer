ad6c1718768489f26b989518272be7a7220000cd
Alexander Sporn <github@alexsporn.de>
2022-11-16 10:44:29 +0100

    Format debug log better

--- packages/protocol/tipmanager/tipmanager.go
+++ packages/protocol/tipmanager/tipmanager.go
@@ -167,8 +167,16 @@ func (t *TipManager) selectTips(count int) (parents models.BlockIDs) {
 			if t.isPastConeTimestampCorrect(tip.Block.Block) {
 				parents.Add(tip.ID())
 			} else {
-				fmt.Printf("cannot select tip due to TSC condition tip issuing time (%s), time (%s), min supported time (%s), block id (%s), tip pool size %d\n", tip.IssuingTime(), t.engine.Clock.AcceptedTime(), t.engine.Clock.AcceptedTime().Add(-t.optsTimeSinceConfirmationThreshold), tip.ID().Base58(), t.tips.Size())
-				fmt.Println(tip.IsScheduled(), "Orphaned", tip.IsOrphaned(), "accepted", t.engine.Consensus.BlockGadget.IsBlockAccepted(tip.ID()))
+				fmt.Printf("cannot select tip due to TSC condition tip issuing time (%s), time (%s), min supported time (%s), block id (%s), tip pool size (%d), scheduled: (%t), orphaned: (%t), accepted: (%t)\n",
+					tip.IssuingTime(),
+					t.engine.Clock.AcceptedTime(),
+					t.engine.Clock.AcceptedTime().Add(-t.optsTimeSinceConfirmationThreshold),
+					tip.ID().Base58(),
+					t.tips.Size(),
+					tip.IsScheduled(),
+					tip.IsOrphaned(),
+					t.engine.Consensus.BlockGadget.IsBlockAccepted(tip.ID()),
+				)
 				//tip.ForEachParent(func(parent models.Parent) {
 				//	fmt.Println("parent block id", parent.ID.Base58())
 				//	if parentBlock, exists := t.engine.(parent.ID); exists {
