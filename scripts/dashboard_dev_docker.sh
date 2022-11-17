75accfed19fc9bb5dcf45f5d30153d6133976d85
jkrvivian <jkrvivian@gmail.com>
2022-11-17 16:58:19 +0800

    Clear build/ before building frontend

--- scripts/dashboard_dev_docker.sh
+++ scripts/dashboard_dev_docker.sh
@@ -3,12 +3,14 @@
 # Installs required dependencies and runs node's dashboard.
 
 echo "::: Running /plugins/dashboard/frontend :::"
+rm -rf plugins/dashboard/frontend/build
+
 docker run -it --rm \
-    -p 9090:9090 -u $(id -u ${USER}):$(id -g ${USER}) \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
     --volume="/etc/group:/etc/group:ro" \
     --volume="/etc/passwd:/etc/passwd:ro" \
     --volume="/etc/shadow:/etc/shadow:ro" \
     -v $(pwd):/tmp/mnt \
     -e YARN_CACHE_FOLDER=/tmp/ \
     -e HOME=/tmp/ \
-    -w /tmp/mnt/plugins/dashboard/frontend node:12.16 bash -c "yarn install && yarn start"
+    -w /tmp/mnt/plugins/dashboard/frontend node:12.16 bash -c "yarn install && yarn build"
