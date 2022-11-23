a2673c8d31017f9586c761efd5dee257cb9085db
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-11-23 23:52:21 +0100

    Refactor: renamed parameter

--- packages/core/snapshot/read.go
+++ packages/core/snapshot/read.go
@@ -15,38 +15,38 @@ import (
 	"github.com/iotaledger/goshimmer/packages/protocol/models"
 )
 
-func ReadSnapshot(fileHandle *os.File, engine *engine.Engine) {
+func ReadSnapshot(fileHandle *os.File, engineInstance *engine.Engine) {
 	// Settings
 	{
 		var settingsSize uint32
 		binary.Read(fileHandle, binary.LittleEndian, &settingsSize)
 		settingsBytes := make([]byte, settingsSize)
 		binary.Read(fileHandle, binary.LittleEndian, settingsBytes)
-		engine.Storage.Settings.FromBytes(settingsBytes)
+		engineInstance.Storage.Settings.FromBytes(settingsBytes)
 	}
 
 	// Committments
 	{
 		ProcessChunks(NewChunkedReader[commitment.Commitment](fileHandle), func(chunk []*commitment.Commitment) {
 			for _, commitment := range chunk {
-				if err := engine.Storage.Commitments.Store(commitment.Index(), commitment); err != nil {
+				if err := engineInstance.Storage.Commitments.Store(commitment.Index(), commitment); err != nil {
 					panic(err)
 				}
 			}
 		})
 	}
 
-	if err := engine.Storage.Settings.SetChainID(engine.Storage.Settings.LatestCommitment().ID()); err != nil {
+	if err := engineInstance.Storage.Settings.SetChainID(engineInstance.Storage.Settings.LatestCommitment().ID()); err != nil {
 		panic(err)
 	}
 
 	// Ledgerstate
 	{
 		ProcessChunks(NewChunkedReader[ledgerstate.OutputWithMetadata](fileHandle),
-			engine.Ledger.ImportOutputs,
-			engine.ManaTracker.ImportOutputs,
+			engineInstance.Ledger.ImportOutputs,
+			engineInstance.ManaTracker.ImportOutputs,
 			// This will import into all the consumers too: sybilprotection and ledgerState.unspentOutputIDs
-			engine.LedgerState.ImportOutputs,
+			engineInstance.LedgerState.ImportOutputs,
 		)
 	}
 
@@ -54,7 +54,7 @@ func ReadSnapshot(fileHandle *os.File, engine *engine.Engine) {
 	{
 		ProcessChunks(NewChunkedReader[models.BlockID](fileHandle), func(chunk []*models.BlockID) {
 			for _, blockID := range chunk {
-				if err := engine.Storage.RootBlocks.Store(*blockID); err != nil {
+				if err := engineInstance.Storage.RootBlocks.Store(*blockID); err != nil {
 					panic(err)
 				}
 			}
@@ -65,7 +65,7 @@ func ReadSnapshot(fileHandle *os.File, engine *engine.Engine) {
 	{
 		ProcessChunks(NewChunkedReader[identity.ID](fileHandle), func(chunk []*identity.ID) {
 			for _, id := range chunk {
-				if err := engine.Storage.Attestors.Store(engine.Storage.Settings.LatestCommitment().Index(), *id); err != nil {
+				if err := engineInstance.Storage.Attestors.Store(engineInstance.Storage.Settings.LatestCommitment().Index(), *id); err != nil {
 					panic(err)
 				}
 			}
@@ -83,27 +83,27 @@ func ReadSnapshot(fileHandle *os.File, engine *engine.Engine) {
 
 			// Created
 			ProcessChunks(NewChunkedReader[ledgerstate.OutputWithMetadata](fileHandle),
-				engine.Ledger.ApplySpentDiff,
+				engineInstance.Ledger.ApplySpentDiff,
 				func(createdChunk []*ledgerstate.OutputWithMetadata) {
-					engine.ManaTracker.RollbackOutputs(createdChunk, true)
+					engineInstance.ManaTracker.RollbackOutputs(createdChunk, true)
 					for _, createdOutput := range createdChunk {
-						engine.LedgerState.StateDiffs.StoreSpentOutput(createdOutput)
+						engineInstance.LedgerState.StateDiffs.StoreSpentOutput(createdOutput)
 					}
 				},
 			)
 
 			// Spent
 			ProcessChunks(NewChunkedReader[ledgerstate.OutputWithMetadata](fileHandle),
-				engine.Ledger.ApplyCreatedDiff,
+				engineInstance.Ledger.ApplyCreatedDiff,
 				func(spentChunk []*ledgerstate.OutputWithMetadata) {
-					engine.ManaTracker.RollbackOutputs(spentChunk, false)
+					engineInstance.ManaTracker.RollbackOutputs(spentChunk, false)
 					for _, createdOutput := range spentChunk {
-						engine.LedgerState.StateDiffs.StoreCreatedOutput(createdOutput)
+						engineInstance.LedgerState.StateDiffs.StoreCreatedOutput(createdOutput)
 					}
 				},
 			)
 
-			engine.LedgerState.ApplyStateDiff(epochIndex)
+			engineInstance.LedgerState.ApplyStateDiff(epochIndex)
 		}
 	}
 }
