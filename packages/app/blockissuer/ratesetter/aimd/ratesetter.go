1fa947246ad0d2d61c18ad04710472743b9c761e
Andrew Cullen <45826600+cyberphysic4l@users.noreply.github.com>
2022-12-01 12:31:48 +0000

    Remove issuerqueue from AIMD ratesetter

--- packages/app/blockissuer/ratesetter/aimd/ratesetter.go
+++ packages/app/blockissuer/ratesetter/aimd/ratesetter.go
@@ -22,7 +22,7 @@ const (
 	RateSettingIncrease = 0.075
 	// RateSettingDecrease global multiplicative decrease parameter (larger than 1).
 	// In the Access Control for Distributed Ledgers in the Internet of Things: A Networking Approach paper this parameter is denoted as "β".
-	RateSettingDecrease = 1.5
+	RateSettingDecrease = 0.7
 	// WMax is the maximum inbox threshold for the node. This value denotes when maximum active mana holder backs off its rate.
 	WMax = 10
 	// WMin is the min inbox threshold for the node. This value denotes when nodes with the least mana back off their rate.
@@ -39,8 +39,8 @@ type RateSetter struct {
 	totalManaRetrieveFunc func() int64
 	manaRetrieveFunc      func() map[identity.ID]int64
 
-	issuingQueue        *utils.IssuerQueue
-	issueChan           chan *models.Block
+	rateUpdateChan      chan *models.Block
+	creditUpdateChan    chan *models.Block
 	ownRate             *atomic.Float64
 	pauseUpdates        uint
 	initialPauseUpdates uint
@@ -48,6 +48,9 @@ type RateSetter struct {
 	shutdownSignal      chan struct{}
 	shutdownOnce        sync.Once
 	initOnce            sync.Once
+	issueCredits        float64
+	creditsMutex        sync.RWMutex
+	maxCredits          float64
 
 	optsInitialRate   float64
 	optsPause         time.Duration
@@ -63,15 +66,17 @@ func New(protocol *protocol.Protocol, selfIdentity identity.ID, opts ...options.
 		self:                  selfIdentity,
 		manaRetrieveFunc:      protocol.Engine().ManaTracker.ManaByIDs,
 		totalManaRetrieveFunc: protocol.Engine().ManaTracker.TotalMana,
-		issuingQueue:          utils.NewIssuerQueue(),
-		issueChan:             make(chan *models.Block),
+		rateUpdateChan:        make(chan *models.Block),
+		creditUpdateChan:      make(chan *models.Block),
 		ownRate:               atomic.NewFloat64(0),
+		issueCredits:          0.0,
+		maxCredits:            models.MaxBlockWork,
 		shutdownSignal:        make(chan struct{}),
 	}, opts, func(r *RateSetter) {
 		r.initialPauseUpdates = uint(r.optsPause / r.optsSchedulerRate)
 		r.maxRate = float64(time.Second / r.optsSchedulerRate)
 	}, func(r *RateSetter) {
-		go r.issuerLoop()
+		go r.rateSetting()
 	}, (*RateSetter).setupEvents)
 
 }
@@ -83,34 +88,23 @@ func (r *RateSetter) Events() *utils.Events {
 
 // Setup sets up the behavior of the component by making it attach to the relevant events of the other components.
 func (r *RateSetter) setupEvents() {
-	r.protocol.Events.CongestionControl.Scheduler.BlockScheduled.Attach(event.NewClosure(func(_ *scheduler.Block) {
+	r.protocol.Events.CongestionControl.Scheduler.BlockScheduled.Attach(event.NewClosure(func(block *scheduler.Block) {
 		if r.pauseUpdates > 0 {
 			r.pauseUpdates--
 			return
 		}
-		if r.issuingQueue.Size()+r.protocol.CongestionControl.Scheduler().IssuerQueueSize(r.self) > 0 {
-			r.rateSetting()
+		// update credits every time a block is scheduled.
+		r.creditUpdateChan <- block.ModelsBlock
+		// only update rate when the issuer is active
+		if r.protocol.CongestionControl.Scheduler().IssuerQueueSize(r.self) > 0 {
+			r.rateUpdateChan <- block.ModelsBlock
 		}
 	}))
-}
-
-// SubmitBlock submits a block to the local issuing queue.
-func (r *RateSetter) SubmitBlock(block *models.Block) error {
-
-	if identity.NewID(block.IssuerPublicKey()) != r.self {
-		return utils.ErrInvalidIssuer
-	}
-	r.initOnce.Do(func() {
-		// initialize mana vectors in cache here when mana vectors are already loaded
-		r.initializeRate()
-	})
-
-	select {
-	case r.issueChan <- block:
-		return nil
-	case <-r.shutdownSignal:
-		return utils.ErrStopped
-	}
+	r.protocol.Events.CongestionControl.Scheduler.BlockSubmitted.Attach(event.NewClosure(func(block *scheduler.Block) {
+		if block.IssuerID() == r.self {
+			r.updateIssueCredits(float64(-block.Work()))
+		}
+	}))
 }
 
 // Shutdown shuts down the RateSetter.
@@ -125,11 +119,6 @@ func (r *RateSetter) Rate() float64 {
 	return r.ownRate.Load()
 }
 
-// Size returns the size of the issuing queue.
-func (r *RateSetter) Size() int {
-	return r.issuingQueue.Size()
-}
-
 // Estimate estimates the issuing time of new block.
 func (r *RateSetter) Estimate() time.Duration {
 	r.initOnce.Do(func() {
@@ -141,89 +130,57 @@ func (r *RateSetter) Estimate() time.Duration {
 	if r.pauseUpdates > 0 {
 		pauseUpdate = time.Duration(float64(r.pauseUpdates)) * r.optsSchedulerRate
 	}
-	// dummy estimate
-	return lo.Max(time.Duration(math.Ceil(float64(r.Size())/r.ownRate.Load()*float64(time.Second))), pauseUpdate)
+	// dummy estimate of work, replace with estimated work as argument.
+	estimatedWork := float64(models.MaxBlockWork)
+	return lo.Max(time.Duration(0), pauseUpdate+time.Duration((r.getIssueCredits()-estimatedWork)/r.ownRate.Load()))
 }
 
-// rateSetting updates the rate ownRate at which blocks can be issued by the node.
 func (r *RateSetter) rateSetting() {
-	// Return access mana or MinMana to allow zero mana nodes issue.
-	ownMana := float64(lo.Max(r.manaRetrieveFunc()[r.self], scheduler.MinMana))
-	maxManaValue := float64(lo.Max(append(lo.Values(r.manaRetrieveFunc()), scheduler.MinMana)...))
-	totalMana := float64(r.totalManaRetrieveFunc())
 
-	ownRate := r.ownRate.Load()
-
-	// TODO: make sure not to issue or scheduled blocks older than TSC
-	if float64(r.protocol.CongestionControl.Scheduler().IssuerQueueSize(r.self)) > math.Max(WMin, WMax*ownMana/maxManaValue) {
-		ownRate /= RateSettingDecrease
-		r.pauseUpdates = r.initialPauseUpdates
-	} else {
-		ownRate += math.Max(RateSettingIncrease*ownMana/totalMana, 0.01)
-	}
-	r.ownRate.Store(math.Min(r.maxRate, ownRate))
-}
-
-func (r *RateSetter) issuerLoop() {
-	var (
-		issueTimer    = time.NewTimer(r.optsSchedulerRate) // setting this to 0 will cause a trigger right away
-		timerStopped  = false
-		lastIssueTime = time.Now()
-	)
-	defer issueTimer.Stop()
+	var lastCreditUpdate = time.Now()
 
 loop:
 	for {
 		select {
-		// a new block can be submitted to the scheduler
-		case <-issueTimer.C:
-			timerStopped = true
-			if r.issuingQueue.Front() == nil {
-				continue
-			}
 
-			block := r.issuingQueue.PopFront()
-			r.events.BlockIssued.Trigger(block)
-			lastIssueTime = time.Now()
+		case <-r.rateUpdateChan:
+			ownMana := float64(lo.Max(r.manaRetrieveFunc()[r.self], scheduler.MinMana))
+			maxManaValue := float64(lo.Max(append(lo.Values(r.manaRetrieveFunc()), scheduler.MinMana)...))
+			totalMana := float64(r.totalManaRetrieveFunc())
+			ownRate := r.ownRate.Load()
+			// TODO: make sure not to issue or scheduled blocks older than TSC
+			if float64(r.protocol.CongestionControl.Scheduler().IssuerQueueSize(r.self)) > math.Max(WMin, WMax*ownMana/maxManaValue) {
+				ownRate *= RateSettingDecrease
+				r.pauseUpdates = r.initialPauseUpdates
+			} else {
+				ownRate += math.Max(RateSettingIncrease*ownMana/totalMana, 0.01)
+			}
+			r.ownRate.Store(math.Min(r.maxRate, ownRate))
 
-			if next := r.issuingQueue.Front(); next != nil {
-				issueTimer.Reset(time.Until(lastIssueTime.Add(r.issueInterval())))
-				timerStopped = false
-			}
-
-		// add a new block to the local issuer queue
-		case block := <-r.issueChan:
-			if r.issuingQueue.Size()+1 > utils.MaxLocalQueueSize {
-				r.events.BlockDiscarded.Trigger(block)
-				continue
-			}
-			// add to queue
-			r.issuingQueue.Enqueue(block)
-
-			// set a new timer if needed
-			// if a timer is already running it is not updated, even if the ownRate has changed
-			if !timerStopped {
-				break
-			}
-			if next := r.issuingQueue.Front(); next != nil {
-				issueTimer.Reset(time.Until(lastIssueTime.Add(r.issueInterval())))
-			}
+		case <-r.creditUpdateChan:
+			ownRate := r.ownRate.Load()
+			r.updateIssueCredits(ownRate * float64(time.Since(lastCreditUpdate)))
+			lastCreditUpdate = time.Now()
 
 		// on close, exit the loop
 		case <-r.shutdownSignal:
 			break loop
 		}
 	}
-
-	// discard all remaining blocks at shutdown
-	for r.issuingQueue.Size() > 0 {
-		r.events.BlockDiscarded.Trigger(r.issuingQueue.PopFront())
-	}
 }
 
-func (r *RateSetter) issueInterval() time.Duration {
-	wait := time.Duration(math.Ceil(float64(1) / r.ownRate.Load() * float64(time.Second)))
-	return wait
+func (r *RateSetter) updateIssueCredits(credits float64) {
+	r.creditsMutex.Lock()
+	defer r.creditsMutex.Unlock()
+
+	r.issueCredits += credits
+}
+
+func (r *RateSetter) getIssueCredits() float64 {
+	r.creditsMutex.Lock()
+	defer r.creditsMutex.Unlock()
+
+	return r.issueCredits
 }
 
 func (r *RateSetter) initializeRate() {
