933115406a084bb8a457de35ded7d0d42223051e
Piotr Macek <piotr.macek@iota.org>
2022-12-08 12:13:54 +0100

    Prune retainer db

--- plugins/retainer/parameters.go
+++ plugins/retainer/parameters.go
@@ -4,7 +4,8 @@ import "github.com/iotaledger/goshimmer/plugins/config"
 
 // ParametersDefinition contains the definition of the parameters used by the remotelog plugin.
 type ParametersDefinition struct {
-	DBPath string `default:"retainer" usage:"path where retainer database is stored"`
+	DBPath         string `default:"retainer" usage:"path where retainer database is stored"`
+	DBPruningDelay uint32 `default:"8640" usage:"how many confirmed epochs should be retained"`
 }
 
 // Parameters contains the configuration used by the remotelog plugin.
