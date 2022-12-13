52dbc37465551416a12596632a3670df44dc2073
Alexander Sporn <github@alexsporn.de>
2022-12-13 11:45:25 +0100

    Fix snapshot creation when genesis amount is zero

--- packages/core/snapshot/creator/snapshotcreator.go
+++ packages/core/snapshot/creator/snapshotcreator.go
@@ -78,18 +78,20 @@ func CreateSnapshotForIntegrationTest(s *storage.Storage, snapshotFileName strin
 
 	outputsWithMetadata := make([]*models.OutputWithMetadata, 0)
 
-	// This is the same seed used to derive the faucet ID.
-	genesisPledgeID := identity.New(ed25519.PrivateKeyFromSeed(genesisNodePledge).Public()).ID()
-	output, outputMetadata := createOutput(seed.NewSeed(genesisSeedBytes).Address(0).Address(), genesisTokenAmount, genesisPledgeID, now)
-	outputsWithMetadata = append(outputsWithMetadata, models.NewOutputWithMetadata(0, output.ID(), output, outputMetadata.ConsensusManaPledgeID(), outputMetadata.AccessManaPledgeID()))
+	if genesisTokenAmount > 0 {
+		// This is the same seed used to derive the faucet ID.
+		genesisPledgeID := identity.New(ed25519.PrivateKeyFromSeed(genesisNodePledge).Public()).ID()
+		output, outputMetadata := createOutput(seed.NewSeed(genesisSeedBytes).Address(0).Address(), genesisTokenAmount, genesisPledgeID, now)
+		outputsWithMetadata = append(outputsWithMetadata, models.NewOutputWithMetadata(0, output.ID(), output, outputMetadata.ConsensusManaPledgeID(), outputMetadata.AccessManaPledgeID()))
 
-	if err := s.Attestors.Store(0, genesisPledgeID); err != nil {
-		panic(err)
+		if err := s.Attestors.Store(0, genesisPledgeID); err != nil {
+			panic(err)
+		}
 	}
 
 	for nodeSeedBytes, value := range nodesToPledge {
 		nodeID := identity.New(ed25519.PrivateKeyFromSeed(nodeSeedBytes[:]).Public()).ID()
-		output, outputMetadata = createOutput(seed.NewSeed(nodeSeedBytes[:]).Address(0).Address(), value, nodeID, now)
+		output, outputMetadata := createOutput(seed.NewSeed(nodeSeedBytes[:]).Address(0).Address(), value, nodeID, now)
 		outputsWithMetadata = append(outputsWithMetadata, models.NewOutputWithMetadata(0, output.ID(), output, outputMetadata.ConsensusManaPledgeID(), outputMetadata.AccessManaPledgeID()))
 		if err := s.Attestors.Store(0, nodeID); err != nil {
 			panic(err)
