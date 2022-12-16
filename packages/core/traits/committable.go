cd477db3b1252035f226e1ce69b2ea509ea4cb56
Hans Moog <3293976+hmoog@users.noreply.github.com>
2022-12-16 15:12:31 +0100

    Feat: simplified Committable trait

--- packages/core/traits/committable.go
+++ packages/core/traits/committable.go
@@ -1,13 +1,10 @@
 package traits
 
 import (
-	"sync"
-
-	"github.com/cockroachdb/errors"
-	"github.com/iotaledger/hive.go/core/generics/lo"
 	"github.com/iotaledger/hive.go/core/kvstore"
 
 	"github.com/iotaledger/goshimmer/packages/core/epoch"
+	"github.com/iotaledger/goshimmer/packages/storage/typedkey"
 )
 
 // Committable is a trait that stores information about the latest commitment.
@@ -21,80 +18,24 @@ type Committable interface {
 
 // NewCommittable creates a new Committable trait.
 func NewCommittable(store kvstore.KVStore, keyBytes ...byte) (newCommittable Committable) {
-	if len(keyBytes) == 0 {
-		panic("keyBytes must not be empty")
+	return &committable{
+		lastCommittedEpoch: typedkey.NewGenericType[epoch.Index](store, keyBytes...),
 	}
-
-	return (&committable{
-		store:    store,
-		keyBytes: keyBytes,
-	}).init()
 }
 
 // committable is the implementation of the Committable trait.
 type committable struct {
-	store kvstore.KVStore
-
-	keyBytes []byte
-
-	// lastCommittedEpoch is the last committed epoch.
-	lastCommittedEpoch epoch.Index
-
-	// mutex is used to synchronize access to lastCommittedEpoch.
-	mutex sync.RWMutex
+	lastCommittedEpoch *typedkey.GenericType[epoch.Index]
 }
 
 // SetLastCommittedEpoch sets the last committed epoch.
 func (c *committable) SetLastCommittedEpoch(index epoch.Index) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-
-	if c.lastCommittedEpoch != index {
-		c.lastCommittedEpoch = index
-
-		if err := c.storeLastCommittedEpoch(index); err != nil {
-			panic(err)
-		}
+	if c.lastCommittedEpoch.Get() != index {
+		c.lastCommittedEpoch.Set(index)
 	}
 }
 
 // LastCommittedEpoch returns the last committed epoch.
 func (c *committable) LastCommittedEpoch() epoch.Index {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-
-	return c.lastCommittedEpoch
-}
-
-// init initializes the committable trait.
-func (c *committable) init() (self *committable) {
-	lastCommittedEpoch, err := c.loadLastCommittedEpoch()
-	if err != nil {
-		panic(err)
-	}
-
-	c.lastCommittedEpoch = lastCommittedEpoch
-
-	return c
-}
-
-func (c *committable) loadLastCommittedEpoch() (lastCommittedEpoch epoch.Index, err error) {
-	lastCommittedEpochBytes, err := c.store.Get(c.keyBytes)
-	if err != nil {
-		return 0, lo.Cond(errors.Is(err, kvstore.ErrKeyNotFound), nil, errors.Errorf("failed to load last committed epoch: %w", err))
-	}
-
-	if lastCommittedEpoch, _, err = epoch.IndexFromBytes(lastCommittedEpochBytes); err != nil {
-		return 0, errors.Errorf("failed to parse last committed epoch: %w", err)
-	}
-
-	return
-}
-
-func (c *committable) storeLastCommittedEpoch(index epoch.Index) (err error) {
-	if err = c.store.Set(c.keyBytes, index.Bytes()); err != nil {
-		return errors.Errorf("failed to store weight of attestations for epoch %d: %w", index, err)
-	}
-
-	return
+	return c.lastCommittedEpoch.Get()
 }
