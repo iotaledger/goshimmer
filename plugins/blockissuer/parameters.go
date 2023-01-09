580c8787d337aaa1482cd79cee3f1dfc8e3b49db
Andrew Cullen <45826600+cyberphysic4l@users.noreply.github.com>
2023-01-09 19:12:48 +0000

    Update parameters.go

--- plugins/blockissuer/parameters.go
+++ plugins/blockissuer/parameters.go
@@ -11,7 +11,7 @@ type ParametersDefinition struct {
 	// RateSetter contains the definition of the parameters used by the Rate Setter.
 	RateSetter struct {
 		// Mode determines the type of the rate setting mechanism.
-		Mode string `default:"deficit" usage:"define the type of rate setter to use"`
+		Mode string `default:"deficit" usage:"define the type of rate setter to use. Possible options are deficit, aimd or disabled."`
 		// Initial defines the initial rate of rate setting.
 		Initial float64 `default:"1" usage:"the initial rate of AIMD rate setting (if in AIMD mode). Set 0 to automatically estimate the value based on access mana."`
 		// Pause defines for how long to pause updates after decrease of rate.
