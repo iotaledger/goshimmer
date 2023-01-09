b5e6bff3a29f591192d21d35e4a19015fbdcd8d5
Andrew Cullen <45826600+cyberphysic4l@users.noreply.github.com>
2023-01-09 17:37:44 +0000

    remove function from ratesetter tests.

--- packages/app/blockissuer/ratesetter/ratesetter_test.go
+++ packages/app/blockissuer/ratesetter/ratesetter_test.go
@@ -16,25 +16,23 @@ func TestRateSetter_IssueBlockAndAwaitSchedule(t *testing.T) {
 	allModes := []ModeType{AIMDMode, DeficitMode, DisabledMode}
 
 	for _, mode := range allModes {
-		func() {
-			tf := NewTestFramework(t, WithRateSetterOptions(WithMode(mode)))
+		tf := NewTestFramework(t, WithRateSetterOptions(WithMode(mode)))
 
-			defer tf.RateSetter[0].Shutdown()
+		defer tf.RateSetter[0].Shutdown()
 
-			blockScheduled := make(chan *models.Block, 1)
-			tf.Protocol.CongestionControl.Scheduler().Events.BlockScheduled.Attach(event.NewClosure(func(block *scheduler.Block) { blockScheduled <- block.ModelsBlock }))
-			blk := tf.CreateBlock(0)
+		blockScheduled := make(chan *models.Block, 1)
+		tf.Protocol.CongestionControl.Scheduler().Events.BlockScheduled.Attach(event.NewClosure(func(block *scheduler.Block) { blockScheduled <- block.ModelsBlock }))
+		blk := tf.CreateBlock(0)
 
-			assert.NoError(t, tf.IssueBlock(blk, 0))
-			assert.Eventually(t, func() bool {
-				select {
-				case blk1 := <-blockScheduled:
-					return assert.Equal(t, blk, blk1)
-				default:
-					return false
-				}
-			}, 1*time.Second, 10*time.Millisecond)
-		}()
+		assert.NoError(t, tf.IssueBlock(blk, 0))
+		assert.Eventually(t, func() bool {
+			select {
+			case blk1 := <-blockScheduled:
+				return assert.Equal(t, blk, blk1)
+			default:
+				return false
+			}
+		}, 1*time.Second, 10*time.Millisecond)
 	}
 }
 
@@ -47,28 +45,26 @@ func TestRateSetter_IssueBlocksAndAwaitScheduleMultipleIssuers(t *testing.T) {
 	allBlocks := make(map[models.BlockID]*models.Block)
 
 	for _, mode := range allModes {
-		func() {
-			tf := NewTestFramework(t, WithRateSetterOptions(WithMode(mode)), WithSchedulerOptions(scheduler.WithRate(time.Duration(schedulerRate))), WithNumIssuers(numIssuers))
-			blockScheduled := make(chan *models.Block, 1)
-			tf.Protocol.CongestionControl.Scheduler().Events.BlockScheduled.Attach(event.NewClosure(func(block *scheduler.Block) { blockScheduled <- block.ModelsBlock }))
+		tf := NewTestFramework(t, WithRateSetterOptions(WithMode(mode)), WithSchedulerOptions(scheduler.WithRate(time.Duration(schedulerRate))), WithNumIssuers(numIssuers))
+		blockScheduled := make(chan *models.Block, 1)
+		tf.Protocol.CongestionControl.Scheduler().Events.BlockScheduled.Attach(event.NewClosure(func(block *scheduler.Block) { blockScheduled <- block.ModelsBlock }))
 
-			for i := 0; i < numIssuers; i++ {
-				defer tf.RateSetter[i].Shutdown()
-				blocks := tf.IssueBlocks(numBlocksPerIssuer, i)
-				for blkID, blk := range blocks {
-					allBlocks[blkID] = blk
-				}
+		for i := 0; i < numIssuers; i++ {
+			defer tf.RateSetter[i].Shutdown()
+			blocks := tf.IssueBlocks(numBlocksPerIssuer, i)
+			for blkID, blk := range blocks {
+				allBlocks[blkID] = blk
 			}
+		}
 
-			assert.Eventually(t, func() bool {
-			        select {
-				case blk := <-blockScheduled:
-					delete(allBlocks, blk.ID())
-					return len(allBlocks) == 0
-				default:
-					return false
-				}
-			}, 1*time.Second, 10*time.Millisecond)
-		}()
+		assert.Eventually(t, func() bool {
+			select {
+			case blk := <-blockScheduled:
+				delete(allBlocks, blk.ID())
+				return len(allBlocks) == 0
+			default:
+				return false
+			}
+		}, 1*time.Second, 10*time.Millisecond)
 	}
 }
