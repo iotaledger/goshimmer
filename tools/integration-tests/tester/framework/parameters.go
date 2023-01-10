0ec630572dbc64ef06257300ca58f51a071b90a6
jkrvivian <jkrvivian@gmail.com>
2023-01-10 23:06:06 +0800

    Disable remotelog plugin in integration test

--- tools/integration-tests/tester/framework/parameters.go
+++ tools/integration-tests/tester/framework/parameters.go
@@ -60,7 +60,7 @@ func PeerConfig() config.GoShimmer {
 
 	c.Image = "iotaledger/goshimmer"
 
-	c.DisabledPlugins = []string{"portcheck", "analysisClient", "profiling", "remotelogmetrics", "remotemetrics", "WebAPIEpochEndpoint", "ManaInitializer", "Warpsync"}
+	c.DisabledPlugins = []string{"portcheck", "analysisClient", "profiling", "remotelog", "remotelogmetrics", "remotemetrics", "WebAPIEpochEndpoint", "ManaInitializer", "Warpsync"}
 
 	c.Network.Enabled = true
 
