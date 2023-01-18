0b196b1724a2547309a0fa0ee1c2854201ce7a26
Piotr Macek <piotr.macek@iota.org>
2023-01-18 15:31:36 +0100

    Temporarily disable TSA commitment check

--- packages/protocol/tipmanager/tipmanager.go
+++ packages/protocol/tipmanager/tipmanager.go
@@ -310,9 +310,10 @@ func (t *TipManager) addFutureTip(block *scheduler.Block) (added bool) {
 }
 
 func (t *TipManager) isValidTip(tip *scheduler.Block) (err error) {
-	if !t.isRecentCommitment(tip) {
-		return errors.Errorf("cannot select tip due to commitment not being recent (%d), current commitment (%d)", tip.Commitment().Index(), t.engine.Storage.Settings.LatestCommitment().Index())
-	}
+	// TODO: fix this check when node is bootstrapping and commits a bunch of epochs at once.
+	//if !t.isRecentCommitment(tip) {
+	//	return errors.Errorf("cannot select tip due to commitment not being recent (%s - %d), current commitment (%d)", tip.ID().String(), tip.Commitment().Index(), t.engine.Storage.Settings.LatestCommitment().Index())
+	//}
 
 	if !t.isPastConeTimestampCorrect(tip.Block.Block) {
 		return errors.Errorf("cannot select tip due to TSC condition tip issuing time (%s), time (%s), min supported time (%s), block id (%s), tip pool size (%d), scheduled: (%t), orphaned: (%t), accepted: (%t)",
@@ -330,7 +331,7 @@ func (t *TipManager) isValidTip(tip *scheduler.Block) (err error) {
 	return nil
 }
 
-// isRecentCommitment returns true if the commitment of the given block is not in the future and it is not older than TSC threshold
+// isRecentCommitment returns true if the commitment of the given block is not in the future, and it is not older than TSC threshold
 // epoch with respect to our latest commitment.
 func (t *TipManager) isRecentCommitment(block *scheduler.Block) (isFresh bool) {
 	return block.Commitment().Index() >= (t.engine.Storage.Settings.LatestCommitment().Index() - t.commitmentRecentBoundary).Max(0)
