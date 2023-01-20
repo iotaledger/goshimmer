003183c3be34774e1d9f914aea25b0578107d893
Piotr Macek <piotr.macek@iota.org>
2023-01-20 15:17:56 +0100

    Revert "Temporarily modify evil wallet to just send 1 conflict"
    This reverts commit 377260eee25b427e018fb51e304940baba2ee020.

--- client/evilspammer/spammer.go
+++ client/evilspammer/spammer.go
@@ -145,6 +145,7 @@ func (s *Spammer) Spam() {
 	s.log.Infof("Start spamming transactions with %d rate", s.SpamDetails.Rate)
 
 	s.State.spamStartTime = time.Now()
+	timeExceeded := time.After(s.SpamDetails.MaxDuration)
 
 	go func() {
 		goroutineCount := atomic.NewInt32(0)
@@ -152,6 +153,10 @@ func (s *Spammer) Spam() {
 			select {
 			case <-s.State.logTicker.C:
 				s.log.Infof("Blocks issued so far: %d, errors encountered: %d", s.State.txSent.Load(), s.ErrCounter.GetTotalErrorCount())
+			case <-timeExceeded:
+				s.log.Infof("Maximum spam duration exceeded, stopping spammer....")
+				s.StopSpamming()
+				return
 			case <-s.done:
 				s.StopSpamming()
 				return
@@ -159,10 +164,11 @@ func (s *Spammer) Spam() {
 				if goroutineCount.Load() > 100 {
 					break
 				}
-
-				s.spamFunc(s)
-				s.StopSpamming()
-				return
+				go func() {
+					goroutineCount.Inc()
+					defer goroutineCount.Dec()
+					s.spamFunc(s)
+				}()
 			}
 		}
 	}()
