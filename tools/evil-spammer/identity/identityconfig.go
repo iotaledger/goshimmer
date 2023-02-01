f31436e585f20268a08ea511adb77724f0ab4508
Daria Dziubałtowska <daria305@gmail.com>
2023-02-01 14:27:52 +0100

    Fix load config

--- tools/evil-spammer/identity/identityconfig.go
+++ tools/evil-spammer/identity/identityconfig.go
@@ -2,35 +2,27 @@ package identity
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 )
 
-// Params matches alias names and corresponding private key strings in Base58 encoding.
-type Params struct {
-	Network map[string]Identities `json:"network"`
-}
-
 type Identities map[string]string
+type Params map[string]Identities
 
-var Config Params
+var Config = Params{}
 
 var identityConfigJSON = `{
-  "network": [
-    "docker": [
+	"docker": {
 	  "peer_master": "8q491c3YWjbPwLmF2WD95YmCgh61j2kenCKHfGfByoWi",
 	  "peer_master2": "4ata8GcTRMJ5sSv2jQJWmSYYTHct748p3tXmCFYm7wjA",
 	  "faucet": "3YX6e7AL28hHihZewKdq6CMkEYVsTJBLgRiprUNiNq5E"
-    ]
-  ]
+	}
 }`
 
 func LoadIdentity(networkName, alias string) string {
-	if identities, ok := Config.Network[networkName]; ok {
-		if privateKey, exists := identities[alias]; exists {
-			return privateKey
-		}
-	}
+	//fmt.Println("Loading identity", alias, "for network", networkName)
+	//if privateKey, exists := Config[alias]; exists {
+	//	return privateKey
+	//}
 	return ""
 }
 
@@ -42,18 +34,19 @@ func LoadConfig() Params {
 		if !os.IsNotExist(err) {
 			panic(err)
 		}
-		if err = os.WriteFile(fname, []byte(identityConfigJSON), 0o644); err != nil {
-			panic(err)
-		}
-		if file, err = os.Open(fname); err != nil {
+		if err = os.WriteFile(fname, []byte(identityConfigJSON), 0o600); err != nil {
 			panic(err)
 		}
 	}
 	defer file.Close()
 	// decode config file
-	if err = json.NewDecoder(file).Decode(&Config); err != nil {
+	fbytes, err := os.ReadFile(fname)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Loaded config:", Config)
+	if err = json.Unmarshal(fbytes, &Config); err != nil {
+		panic(err)
+	}
+	//fmt.Println("Loaded config:", Config)
 	return Config
 }
