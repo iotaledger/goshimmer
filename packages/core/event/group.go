3929e4a7a927fb362040c9e180e07e8f798a8ea7
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-02-09 13:37:41 +0100

    Refactor: renamed type params

--- packages/core/event/group.go
+++ packages/core/event/group.go
@@ -8,20 +8,20 @@ import (
 )
 
 // Group is a trait that can be embedded into a struct to make the contained events linkable.
-type Group[A any, B ptrGroupType[A, B]] struct {
-	linkUpdated *Event1[B]
+type Group[GroupType any, GroupPtrType ptrGroupType[GroupType, GroupPtrType]] struct {
+	linkUpdated *Event1[GroupPtrType]
 	sync.Once
 }
 
 // NewGroup returns the linkable constructor for the given type.
-func NewGroup[A any, B ptrGroupType[A, B]](newFunc func() B) (constructor func(...B) B) {
-	return func(optLinkTargets ...B) (self B) {
+func NewGroup[GroupType any, GroupPtrType ptrGroupType[GroupType, GroupPtrType]](newFunc func() GroupPtrType) (constructor func(...GroupPtrType) GroupPtrType) {
+	return func(optLinkTargets ...GroupPtrType) (self GroupPtrType) {
 		self = newFunc()
 
 		selfValue := reflect.ValueOf(self).Elem()
-		self.onLinkUpdated(func(linkTarget B) {
+		self.onLinkUpdated(func(linkTarget GroupPtrType) {
 			if linkTarget == nil {
-				linkTarget = new(A)
+				linkTarget = new(GroupType)
 			}
 
 			linkTargetValue := reflect.ValueOf(linkTarget).Elem()
@@ -44,28 +44,28 @@ func NewGroup[A any, B ptrGroupType[A, B]](newFunc func() B) (constructor func(.
 }
 
 // LinkTo links the group to another group of the same type (nil unlinks).
-func (l *Group[A, B]) LinkTo(target B) {
-	l.linkUpdatedEvent().Trigger(target)
+func (g *Group[GroupType, GroupPtrType]) LinkTo(target GroupPtrType) {
+	g.linkUpdatedEvent().Trigger(target)
 }
 
 // onLinkUpdated registers a callback to be called when the link to the referenced Group is set or updated.
-func (l *Group[A, B]) onLinkUpdated(callback func(linkTarget B)) {
-	l.linkUpdatedEvent().Hook(callback)
+func (g *Group[GroupType, GroupPtrType]) onLinkUpdated(callback func(linkTarget GroupPtrType)) {
+	g.linkUpdatedEvent().Hook(callback)
 }
 
 // linkUpdatedEvent returns the linkUpdated Event.
-func (l *Group[A, B]) linkUpdatedEvent() (linkUpdatedEvent *Event1[B]) {
-	l.Do(func() {
-		l.linkUpdated = New1[B]()
+func (g *Group[GroupType, GroupPtrType]) linkUpdatedEvent() *Event1[GroupPtrType] {
+	g.Do(func() {
+		g.linkUpdated = New1[GroupPtrType]()
 	})
 
-	return l.linkUpdated
+	return g.linkUpdated
 }
 
-// ptrGroupType is a helper type to create a pointer to a linkableCollectionType.
-type ptrGroupType[A any, B constraints.Ptr[A]] interface {
-	*A
+// ptrGroupType is a helper type to create a pointer to a Group type.
+type ptrGroupType[GroupType any, GroupPtrType constraints.Ptr[GroupType]] interface {
+	*GroupType
 
-	onLinkUpdated(callback func(B))
-	LinkTo(target B)
+	onLinkUpdated(callback func(GroupPtrType))
+	LinkTo(target GroupPtrType)
 }
