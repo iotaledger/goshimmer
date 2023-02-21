2a206f8fd0812c3dccceea7981187376d2bef942
jonastheis <4181434+jonastheis@users.noreply.github.com>
2023-02-21 13:13:45 -0600

    Remove attachment orphanage

--- packages/protocol/engine/tangle/booker/attachments.go
+++ packages/protocol/engine/tangle/booker/attachments.go
@@ -1,7 +1,6 @@
 package booker
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/iotaledger/goshimmer/packages/core/epoch"
@@ -19,18 +18,13 @@ type attachments struct {
 	attachments *shrinkingmap.ShrinkingMap[utxo.TransactionID, *shrinkingmap.ShrinkingMap[epoch.Index, *shrinkingmap.ShrinkingMap[models.BlockID, *virtualvoting.Block]]]
 	evictionMap *shrinkingmap.ShrinkingMap[epoch.Index, set.Set[utxo.TransactionID]]
 
-	// nonOrphanedCounter is used to count all non-orphaned attachment of a transaction,
-	// so that it's not necessary to iterate through all the attachments to check if the transaction is orphaned.
-	nonOrphanedCounter *shrinkingmap.ShrinkingMap[utxo.TransactionID, uint32]
-
 	mutex *syncutils.DAGMutex[utxo.TransactionID]
 }
 
 func newAttachments() (newAttachments *attachments) {
 	return &attachments{
-		attachments:        shrinkingmap.New[utxo.TransactionID, *shrinkingmap.ShrinkingMap[epoch.Index, *shrinkingmap.ShrinkingMap[models.BlockID, *virtualvoting.Block]]](),
-		evictionMap:        shrinkingmap.New[epoch.Index, set.Set[utxo.TransactionID]](),
-		nonOrphanedCounter: shrinkingmap.New[utxo.TransactionID, uint32](),
+		attachments: shrinkingmap.New[utxo.TransactionID, *shrinkingmap.ShrinkingMap[epoch.Index, *shrinkingmap.ShrinkingMap[models.BlockID, *virtualvoting.Block]]](),
+		evictionMap: shrinkingmap.New[epoch.Index, set.Set[utxo.TransactionID]](),
 
 		mutex: syncutils.NewDAGMutex[utxo.TransactionID](),
 	}
@@ -44,44 +38,9 @@ func (a *attachments) Store(txID utxo.TransactionID, block *virtualvoting.Block)
 		return false
 	}
 
-	prevValue, _ := a.nonOrphanedCounter.GetOrCreate(txID, func() uint32 { return 0 })
-	a.nonOrphanedCounter.Set(txID, prevValue+1)
-
-	a.updateEvictionMap(block.ID().EpochIndex, txID)
+	a.updateEvictionMap(block.ID().Index(), txID)
 
 	return true
-}
-
-func (a *attachments) AttachmentOrphaned(txID utxo.TransactionID, block *virtualvoting.Block) (attachmentBlock *virtualvoting.Block, attachmentOrphaned, lastAttachmentOrphaned bool) {
-	a.mutex.Lock(txID)
-	defer a.mutex.Unlock(txID)
-
-	storage := a.storage(txID, false)
-	if storage == nil {
-		// zero attachments registered
-		return nil, false, false
-	}
-
-	attachmentsOfEpoch, exists := storage.Get(block.ID().Index())
-	if !exists {
-		// there is no attachments of the transaction in the epoch, so no need to continue
-		return nil, false, false
-	}
-
-	attachmentBlock, attachmentExists := attachmentsOfEpoch.Get(block.ID())
-	if !attachmentExists {
-		return nil, false, false
-	}
-
-	prevValue, counterExists := a.nonOrphanedCounter.Get(txID)
-	if !counterExists {
-		panic(fmt.Sprintf("non orphaned attachment counter does not exist for TxID(%s)", txID.String()))
-	}
-
-	newValue := prevValue - 1
-	a.nonOrphanedCounter.Set(txID, newValue)
-
-	return attachmentBlock, true, newValue == 0
 }
 
 func (a *attachments) Get(txID utxo.TransactionID) (attachments []*virtualvoting.Block) {
