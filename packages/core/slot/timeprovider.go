6b6c52508fbedddc9c121a8dc3fdcd53b40011b1
Alexander Sporn <github@alexsporn.de>
2023-02-27 14:43:51 +0100

    Rename the epoch TimeProvider to slot TimeProvider

--- packages/core/slot/timeprovider.go
+++ packages/core/slot/timeprovider.go
@@ -6,7 +6,7 @@ import (
 	"github.com/iotaledger/hive.go/runtime/options"
 )
 
-// TimeProvider defines the genesis time of epoch 0 and allows to convert index to and from time.
+// TimeProvider defines the genesis time of slot 0 and allows to convert index to and from time.
 type TimeProvider struct {
 	// genesisUnixTime is the time (Unix in seconds) of the genesis.
 	genesisUnixTime int64
@@ -24,42 +24,42 @@ func NewTimeProvider(opts ...options.Option[TimeProvider]) *TimeProvider {
 }
 
 // GenesisUnixTime is the time (Unix in seconds) of the genesis.
-func (e *TimeProvider) GenesisUnixTime() int64 {
-	return e.genesisUnixTime
+func (t *TimeProvider) GenesisUnixTime() int64 {
+	return t.genesisUnixTime
 }
 
 // GenesisTime is the time  of the genesis.
-func (e *TimeProvider) GenesisTime() time.Time {
-	return time.Unix(e.genesisUnixTime, 0)
+func (t *TimeProvider) GenesisTime() time.Time {
+	return time.Unix(t.genesisUnixTime, 0)
 }
 
 // Duration is the epoch duration in seconds.
-func (e *TimeProvider) Duration() int64 {
-	return e.duration
+func (t *TimeProvider) Duration() int64 {
+	return t.duration
 }
 
 // IndexFromTime calculates the Index from the given time.
 //
-// Note: Epochs are counted starting from 1 because 0 is reserved for the genesis which has to be addressable as its own
-// epoch as part of the commitment chains.
-func (e *TimeProvider) IndexFromTime(t time.Time) Index {
-	elapsedSeconds := t.Unix() - e.genesisUnixTime
+// Note: slots are counted starting from 1 because 0 is reserved for the genesis which has to be addressable as its own
+// slot as part of the commitment chains.
+func (t *TimeProvider) IndexFromTime(time time.Time) Index {
+	elapsedSeconds := time.Unix() - t.genesisUnixTime
 	if elapsedSeconds < 0 {
 		return 0
 	}
 
-	return Index(elapsedSeconds/e.duration + 1)
+	return Index(elapsedSeconds/t.duration + 1)
 }
 
-// StartTime calculates the start time of the given epoch.
-func (e *TimeProvider) StartTime(i Index) time.Time {
-	startUnix := e.genesisUnixTime + int64(i-1)*e.duration
+// StartTime calculates the start time of the given slot.
+func (t *TimeProvider) StartTime(i Index) time.Time {
+	startUnix := t.genesisUnixTime + int64(i-1)*t.duration
 	return time.Unix(startUnix, 0)
 }
 
-// EndTime returns the latest possible timestamp for an Epoch. Anything with higher timestamp will belong to the next epoch.
-func (e *TimeProvider) EndTime(i Index) time.Time {
-	endUnix := e.genesisUnixTime + int64(i)*e.duration
+// EndTime returns the latest possible timestamp for a slot. Anything with higher timestamp will belong to the next slot.
+func (t *TimeProvider) EndTime(i Index) time.Time {
+	endUnix := t.genesisUnixTime + int64(i)*t.duration
 	// we subtract 1 nanosecond from the next epoch to get the latest possible timestamp for epoch i
 	return time.Unix(endUnix, 0).Add(-1)
 }
@@ -71,8 +71,8 @@ func WithGenesisUnixTime(genesisTime int64) options.Option[TimeProvider] {
 	}
 }
 
-// WithEpochDuration allows to set the duration in seconds of each epoch. Defaults to 10.
-func WithEpochDuration(duration int64) options.Option[TimeProvider] {
+// WithSlotDuration allows to set the duration in seconds of each slot. Defaults to 10.
+func WithSlotDuration(duration int64) options.Option[TimeProvider] {
 	return func(e *TimeProvider) {
 		e.duration = duration
 	}
