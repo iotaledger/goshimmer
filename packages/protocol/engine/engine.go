985dc2fd0a5026f6932becac043b00b0a29b847c
Alexander Sporn <github@alexsporn.de>
2023-03-03 12:57:49 +0100

    Linter fix

--- packages/protocol/engine/engine.go
+++ packages/protocol/engine/engine.go
@@ -212,7 +212,7 @@ func (e *Engine) SlotTimeProvider() *slot.TimeProvider {
 
 func (e *Engine) Initialize(snapshot ...string) (err error) {
 	if !e.Storage.Settings.SnapshotImported() {
-		if len(snapshot) == 0 || len(snapshot[0]) == 0 {
+		if len(snapshot) == 0 || snapshot[0] == "" {
 			panic("no snapshot path specified")
 		}
 		if err = e.readSnapshot(snapshot[0]); err != nil {
