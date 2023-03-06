eac477c0a6ae3f76801145b6b4f2179bc6710ded
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-03-06 22:49:00 +0100

    Feat: confirmed overrides accepted

--- packages/protocol/engine/clock/blocktime/clock.go
+++ packages/protocol/engine/clock/blocktime/clock.go
@@ -13,11 +13,11 @@ import (
 
 // Clock implements the clock.Clock interface that sources its notion of time from accepted and confirmed blocks.
 type Clock struct {
-	// accepted contains a notion of time that is anchored to the latest accepted block.
-	accepted *RelativeTime
+	// acceptedTime contains a notion of time that is anchored to the latest accepted block.
+	acceptedTime *RelativeTime
 
-	// confirmed contains a notion of time that is anchored to the latest confirmed block.
-	confirmed *RelativeTime
+	// confirmedTime contains a notion of time that is anchored to the latest confirmed block.
+	confirmedTime *RelativeTime
 
 	// Module embeds the required methods of the module.Interface.
 	module.Module
@@ -27,32 +27,34 @@ type Clock struct {
 func NewProvider(opts ...options.Option[Clock]) module.Provider[*engine.Engine, clock.Clock] {
 	return module.Provide(func(e *engine.Engine) clock.Clock {
 		return options.Apply(&Clock{
-			accepted:  NewRelativeTime(),
-			confirmed: NewRelativeTime(),
+			acceptedTime:  NewRelativeTime(),
+			confirmedTime: NewRelativeTime(),
 		}, opts, func(c *Clock) {
 			e.HookConstructed(func() {
 				e.LedgerState.HookInitialized(func() {
-					c.accepted.Set(e.SlotTimeProvider().EndTime(e.Storage.Settings.LatestCommitment().Index()))
-					c.confirmed.Set(e.SlotTimeProvider().EndTime(e.Storage.Settings.LatestCommitment().Index()))
+					c.acceptedTime.Set(e.SlotTimeProvider().EndTime(e.Storage.Settings.LatestCommitment().Index()))
+					c.confirmedTime.Set(e.SlotTimeProvider().EndTime(e.Storage.Settings.LatestCommitment().Index()))
 
 					c.TriggerInitialized()
 				})
 
-				e.Events.Clock.AcceptedTimeUpdated.LinkTo(c.accepted.OnUpdated)
-				e.Events.Clock.ConfirmedTimeUpdated.LinkTo(c.confirmed.OnUpdated)
+				e.Events.Clock.AcceptedTimeUpdated.LinkTo(c.acceptedTime.OnUpdated)
+				e.Events.Clock.ConfirmedTimeUpdated.LinkTo(c.confirmedTime.OnUpdated)
 
 				async := event.WithWorkerPool(e.Workers.CreatePool("Clock", 1))
 				c.HookStopped(lo.Batch(
 					e.Events.Consensus.BlockGadget.BlockAccepted.Hook(func(block *blockgadget.Block) {
-						c.accepted.Advance(block.IssuingTime())
+						c.acceptedTime.Advance(block.IssuingTime())
 					}, async).Unhook,
 
 					e.Events.Consensus.BlockGadget.BlockConfirmed.Hook(func(block *blockgadget.Block) {
-						c.confirmed.Advance(block.IssuingTime())
+						c.acceptedTime.Advance(block.IssuingTime())
+						c.confirmedTime.Advance(block.IssuingTime())
 					}, async).Unhook,
 
 					e.Events.Consensus.SlotGadget.SlotConfirmed.Hook(func(index slot.Index) {
-						c.confirmed.Advance(e.SlotTimeProvider().EndTime(index))
+						c.acceptedTime.Advance(e.SlotTimeProvider().EndTime(index))
+						c.confirmedTime.Advance(e.SlotTimeProvider().EndTime(index))
 					}, async).Unhook,
 				))
 			})
@@ -64,10 +66,10 @@ func NewProvider(opts ...options.Option[Clock]) module.Provider[*engine.Engine, 
 
 // Accepted returns a notion of time that is anchored to the latest accepted block.
 func (c *Clock) Accepted() clock.RelativeTime {
-	return c.accepted
+	return c.acceptedTime
 }
 
 // Confirmed returns a notion of time that is anchored to the latest confirmed block.
 func (c *Clock) Confirmed() clock.RelativeTime {
-	return c.confirmed
+	return c.confirmedTime
 }
