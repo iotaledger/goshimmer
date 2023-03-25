13a3b23c4d5e8781bcd29d7f93ed59588c5a487c
Hans Moog <3293976+hmoog@users.noreply.github.com>
2023-03-25 23:41:01 +0100

    Feat: started tying things together in the ConflictDAG

--- packages/protocol/engine/ledger/mempool/newconflictdag/conflictdag.go
+++ packages/protocol/engine/ledger/mempool/newconflictdag/conflictdag.go
@@ -1,46 +1,82 @@
 package newconflictdag
 
-//
-// import (
-// 	"github.com/iotaledger/hive.go/ds/advancedset"
-// 	"github.com/iotaledger/hive.go/ds/shrinkingmap"
-// 	"github.com/iotaledger/hive.go/runtime/options"
-// 	"github.com/iotaledger/hive.go/runtime/syncutils"
-// )
-//
-// type ConflictDAG[ConflictIDType, ResourceIDType comparable] struct {
-// 	conflicts    *shrinkingmap.ShrinkingMap[ConflictIDType, *Conflict[ConflictIDType, ResourceIDType]]
-// 	conflictSets *shrinkingmap.ShrinkingMap[ResourceIDType, *ConflictSet[ConflictIDType, ResourceIDType]]
-//
-// 	mutex *syncutils.StarvingMutex
-//
-// 	optsMergeToMaster bool
-// }
-//
-// func New[ConflictIDType comparable, ResourceIDType comparable](opts ...options.Option[ConflictDAG[ConflictIDType, ResourceIDType]]) *ConflictDAG[ConflictIDType, ResourceIDType] {
-// 	return options.Apply(&ConflictDAG[ConflictIDType, ResourceIDType]{
-// 		conflicts:    shrinkingmap.New[ConflictIDType, *Conflict[ConflictIDType, ResourceIDType]](),
-// 		conflictSets: shrinkingmap.New[ResourceIDType, *ConflictSet[ConflictIDType, ResourceIDType]](),
-// 		mutex:        syncutils.NewStarvingMutex(),
-// 	}, opts, func(instance *ConflictDAG[ConflictIDType, ResourceIDType]) {
-//
-// 	})
-// }
-//
-// func (c *ConflictDAG[ConflictIDType, ResourceIDType]) CreateConflict(id ConflictIDType, parentIDs *advancedset.AdvancedSet[ConflictIDType], conflictingResourceIDs *advancedset.AdvancedSet[ResourceIDType], initialWeight Weight) (created bool) {
-// 	c.mutex.Lock()
-// 	defer c.mutex.Unlock()
-//
-// 	conflictParents := advancedset.New[*Conflict[ConflictIDType, ResourceIDType]]()
-// 	for it := parentIDs.Iterator(); it.HasNext(); {
-// 		parentID := it.Next()
-// 		parent, exists := c.conflicts.Get(parentID)
-// 		if !exists {
-// 			// if the parent does not exist it means that it has been evicted already. We can ignore it.
-// 			continue
-// 		}
-// 		conflictParents.Add(parent)
-// 	}
-//
-// 	return true
-// }
+import (
+	"sync"
+
+	"github.com/iotaledger/goshimmer/packages/protocol/engine/ledger/mempool/newconflictdag/conflict"
+	"github.com/iotaledger/hive.go/ds/advancedset"
+	"github.com/iotaledger/hive.go/ds/shrinkingmap"
+	"github.com/iotaledger/hive.go/runtime/event"
+	"github.com/iotaledger/hive.go/runtime/syncutils"
+)
+
+// ConflictDAG represents a data structure that tracks causal relationships between Conflicts and that allows to
+// efficiently manage these Conflicts (and vote on their fate).
+type ConflictDAG[ConflictID, ResourceID conflict.IDType] struct {
+	// ConflictCreated is triggered when a new Conflict is created.
+	ConflictCreated *event.Event1[*conflict.Conflict[ConflictID, ResourceID]]
+
+	// conflictsByID is a mapping of ConflictIDs to Conflicts.
+	conflictsByID *shrinkingmap.ShrinkingMap[ConflictID, *conflict.Conflict[ConflictID, ResourceID]]
+
+	// conflictSetsByID is a mapping of ResourceIDs to ConflictSets.
+	conflictSetsByID *shrinkingmap.ShrinkingMap[ResourceID, *conflict.Set[ConflictID, ResourceID]]
+
+	// pendingTasks is a counter that keeps track of the number of pending tasks.
+	pendingTasks *syncutils.Counter
+
+	// mutex is used to synchronize access to the ConflictDAG.
+	mutex sync.RWMutex
+}
+
+// New creates a new ConflictDAG.
+func New[ConflictID, ResourceID conflict.IDType]() *ConflictDAG[ConflictID, ResourceID] {
+	return &ConflictDAG[ConflictID, ResourceID]{
+		ConflictCreated:  event.New1[*conflict.Conflict[ConflictID, ResourceID]](),
+		conflictsByID:    shrinkingmap.New[ConflictID, *conflict.Conflict[ConflictID, ResourceID]](),
+		conflictSetsByID: shrinkingmap.New[ResourceID, *conflict.Set[ConflictID, ResourceID]](),
+		pendingTasks:     syncutils.NewCounter(),
+	}
+}
+
+// CreateConflict creates a new Conflict that is conflicting over the given ResourceIDs and that has the given parents.
+func (c *ConflictDAG[ConflictID, ResourceID]) CreateConflict(id ConflictID, parentIDs []ConflictID, resourceIDs []ResourceID) bool {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+
+	newConflict, newConflictCreated := c.conflictsByID.GetOrCreate(id, func() *conflict.Conflict[ConflictID, ResourceID] {
+		return conflict.New[ConflictID, ResourceID](id, c.Conflicts(parentIDs...), c.ConflictSets(resourceIDs...), nil, c.pendingTasks)
+	})
+
+	if newConflictCreated {
+		c.ConflictCreated.Trigger(newConflict)
+	}
+
+	return newConflictCreated
+}
+
+// Conflicts returns the Conflicts that are associated with the given ConflictIDs.
+func (c *ConflictDAG[ConflictID, ResourceID]) Conflicts(ids ...ConflictID) *advancedset.AdvancedSet[*conflict.Conflict[ConflictID, ResourceID]] {
+	conflicts := advancedset.New[*conflict.Conflict[ConflictID, ResourceID]]()
+	for _, id := range ids {
+		// TODO: check if it's okay to ignore non-existing conflicts
+		if existingConflict, exists := c.conflictsByID.Get(id); exists {
+			conflicts.Add(existingConflict)
+		}
+	}
+
+	return conflicts
+}
+
+// ConflictSets returns the ConflictSets that are associated with the given ResourceIDs.
+func (c *ConflictDAG[ConflictID, ResourceID]) ConflictSets(ids ...ResourceID) *advancedset.AdvancedSet[*conflict.Set[ConflictID, ResourceID]] {
+	conflictSets := advancedset.New[*conflict.Set[ConflictID, ResourceID]]()
+	for _, id := range ids {
+		// TODO: check if it's okay to ignore non-existing conflictSets
+		if existingConflictSet, exists := c.conflictSetsByID.Get(id); exists {
+			conflictSets.Add(existingConflictSet)
+		}
+	}
+
+	return conflictSets
+}
