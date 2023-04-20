d745f0aebe91cd1461e489c2af96a68e5e68d28f
Piotr Macek <4007944+piotrm50@users.noreply.github.com>
2023-04-20 09:29:19 +0200

    Remove some unused metrics that caused problems

--- plugins/dashboardmetrics/conflict.go
+++ plugins/dashboardmetrics/conflict.go
@@ -5,21 +5,11 @@ import (
 
 	"go.uber.org/atomic"
 
-	"github.com/iotaledger/goshimmer/packages/protocol/engine/ledger/mempool/conflictdag"
 	"github.com/iotaledger/goshimmer/packages/protocol/engine/ledger/utxo"
 	"github.com/iotaledger/hive.go/ds/types"
 )
 
 var (
-	// total number of conflicts in the database at startup.
-	initialConflictTotalCountDB uint64
-
-	// total number of finalized conflicts in the database at startup.
-	initialFinalizedConflictCountDB uint64
-
-	// total number of confirmed conflicts in the database at startup.
-	initialConfirmedConflictCountDB uint64
-
 	// number of conflicts created since the node started.
 	conflictTotalCountDB atomic.Uint64
 
@@ -45,17 +35,17 @@ func ConflictConfirmationTotalTime() uint64 {
 
 // ConfirmedConflictCount returns the number of confirmed conflicts.
 func ConfirmedConflictCount() uint64 {
-	return initialConfirmedConflictCountDB + confirmedConflictCount.Load()
+	return confirmedConflictCount.Load()
 }
 
 // TotalConflictCountDB returns the total number of conflicts.
 func TotalConflictCountDB() uint64 {
-	return initialConflictTotalCountDB + conflictTotalCountDB.Load()
+	return conflictTotalCountDB.Load()
 }
 
 // FinalizedConflictCountDB returns the number of non-confirmed conflicts.
 func FinalizedConflictCountDB() uint64 {
-	return initialFinalizedConflictCountDB + finalizedConflictCountDB.Load()
+	return finalizedConflictCountDB.Load()
 }
 
 func addActiveConflict(conflictID utxo.TransactionID) (added bool) {
@@ -80,45 +70,4 @@ func removeActiveConflict(conflictID utxo.TransactionID) (removed bool) {
 	}
 
 	return false
-}
-
-func measureInitialConflictStats() {
-	activeConflictsMutex.Lock()
-	defer activeConflictsMutex.Unlock()
-	activeConflicts = make(map[utxo.TransactionID]types.Empty)
-	conflictsToRemove := make([]utxo.TransactionID, 0)
-
-	deps.Protocol.Engine().Ledger.MemPool().ConflictDAG().ForEachConflict(func(conflict *conflictdag.Conflict[utxo.TransactionID, utxo.OutputID]) {
-		switch conflict.ID() {
-		case utxo.EmptyTransactionID:
-			return
-		default:
-			initialConflictTotalCountDB++
-			activeConflicts[conflict.ID()] = types.Void
-			if deps.Protocol.Engine().Ledger.MemPool().ConflictDAG().ConfirmationState(utxo.NewTransactionIDs(conflict.ID())).IsAccepted() {
-				conflict.ForEachConflictingConflict(func(conflictingConflict *conflictdag.Conflict[utxo.TransactionID, utxo.OutputID]) bool {
-					if conflictingConflict.ID() != conflict.ID() {
-						initialFinalizedConflictCountDB++
-					}
-					return true
-				})
-				initialFinalizedConflictCountDB++
-				initialConfirmedConflictCountDB++
-				conflictsToRemove = append(conflictsToRemove, conflict.ID())
-			}
-		}
-	})
-
-	// remove finalized conflicts from the map in separate loop when all conflicting conflicts are known
-	for _, conflictID := range conflictsToRemove {
-		if c, exists := deps.Protocol.Engine().Ledger.MemPool().ConflictDAG().Conflict(conflictID); exists {
-			c.ForEachConflictingConflict(func(conflictingConflict *conflictdag.Conflict[utxo.TransactionID, utxo.OutputID]) bool {
-				if conflictingConflict.ID() != conflictID {
-					delete(activeConflicts, conflictingConflict.ID())
-				}
-				return true
-			})
-		}
-		delete(activeConflicts, conflictID)
-	}
 }
